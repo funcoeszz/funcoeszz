59f564479347902beb2486ebfe0f571274c4ccdd
Aurelio Jargas <verde@aurelio.net>
2013-02-22 00:13:39 -0300

    zzipinternet: voltou a funcionar (URL e filtro atualizados)

--- zz/zzipinternet.sh
+++ zz/zzipinternet.sh
@@ -1,20 +1,20 @@
 # ----------------------------------------------------------------------------
-# http://www.whatismyip.com
+# http://www.getip.com
 # Mostra o seu número IP (externo) na Internet.
 # Uso: zzipinternet
 # Ex.: zzipinternet
 #
 # Autor: Thobias Salazar Trevisan, www.thobias.org
 # Desde: 2005-09-01
-# Versão: 3
+# Versão: 4
 # Licença: GPL
 # ----------------------------------------------------------------------------
 zzipinternet ()
 {
 	zzzz -h ipinternet "$1" && return
 
-	local url='http://automation.whatismyip.com/n09230945.asp'
+	local url='http://www.getip.com'
 
 	# O resultado já vem pronto!
-	$ZZWWWDUMP "$url" | sed -n '/\./ s/^  *//p'
+	$ZZWWWDUMP "$url" | sed -n 's/^Current IP: //p'
 }
