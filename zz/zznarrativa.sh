363f7ddf614bb638480e5f5bd5d8cd1cea6c1fa9
Aurelio Jargas <verde@aurelio.net>
2013-02-23 21:47:17 -0300

    zznarrativa: funciona no Mac, descrição melhorada

--- zz/zznarrativa.sh
+++ zz/zznarrativa.sh
@@ -1,15 +1,15 @@
 # ----------------------------------------------------------------------------
 # http://translate.google.com
-# Google Tradutor, para narrar frases.
+# Narra frases em português usando o Google Tradutor.
 #
 # Uso: zznarrativa palavras
 # Ex.: zznarrativa regex é legal
 #
 # Autor: Kl0nEz <kl0nez (a) wifi org br>
 # Desde: 2011-08-23
-# Versão: 1
+# Versão: 2
 # Licença: GPLv2
-# Requisitos: mpg123
+# Requisitos: mpg123 ou afplay
 # ----------------------------------------------------------------------------
 zznarrativa ()
 {
@@ -18,15 +18,20 @@ zznarrativa ()
 	[ "$1" ] || { zztool uso narrativa; return 1; }
 
 	# Variaveis locais
-	local padrao
+	local padrao play_cmd
 	local url='http://translate.google.com.br'
 	local charset_para='UTF-8'
 	local audio_file="/tmp/$$.WAV"
-	local play_cmd='mpg123 -q'
+
+	if test -f /usr/bin/afplay
+	then
+		play_cmd='afplay'     # mac
+	else
+		play_cmd='mpg123 -q'  # linux
+	fi
 
 	# Narrativa
 	padrao=$(echo "$*" | sed "$ZZSEDURL")
 	local audio="translate_tts?ie=$charset_para&q=$padrao&tl=pt&prev=input"
 	$ZZWWWHTML "$url/$audio" > $audio_file && $play_cmd $audio_file && rm -rf $audio_file
-
 }
