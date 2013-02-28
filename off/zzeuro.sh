df5a28b0ddc6ef31809886c1799969cf7e25609d
Aurelio Jargas <verde@aurelio.net>
2013-02-28 13:36:03 -0300

    DESATIVADA: zzeuro (veja issue #64)

--- off/zzeuro.sh
+++ off/zzeuro.sh
@@ -9,11 +9,13 @@
 # Versão: 1
 # Licença: GPL
 # ----------------------------------------------------------------------------
+# DESATIVADA: 2013-02-28 Parou de funcionar (issue #64)
 zzeuro ()
 {
 	zzzz -h euro "$1" && return
 
 	$ZZWWWDUMP 'http://cotacoes.agronegocios-e.com.br/investimentos/conteudoi.asp?option=dolar&title=%20Euro' |
+		tee /tmp/z |
 		sed '
 			s/^ *//
 			/Compra/,/Euro x D/!d
