8719ae2c74aad87212d3d7ac8d80576ce664edda
Aurelio Jargas <verde@aurelio.net>
2013-02-28 00:27:00 -0300

    zztweets: linha em branco entre os tweets
    Para melhorar a legibilidade. O comando G sozinho é quem insere estas
    linhas.

--- zz/zztweets.sh
+++ zz/zztweets.sh
@@ -5,7 +5,7 @@
 #
 # Autor: Eri Ramos Bastos <bastos.eri (a) gmail.com>
 # Desde: 2009-07-30
-# Versão: 4
+# Versão: 5
 # Licença: GPL
 # ----------------------------------------------------------------------------
 zztweets ()
@@ -18,9 +18,10 @@ zztweets ()
 	local name=$(echo $1 | tr -d "@")
 
 	$ZZWWWDUMP $url/$name |
-	sed '1,50 d' |
-	sed -n '/ .*[0-9]\{1,2\}\./{n;p;}' |
-	sed 's/\[DEL: \(.\) :DEL\] /\1/g;s/^ */ /g'
+		sed '1,50 d' |
+		sed -n '/ .*[0-9]\{1,2\}\./{n;p;}' |
+		sed 's/\[DEL: \(.\) :DEL\] /\1/g; s/^ *//g' |
+		sed G
 
 	# Apagando as 50 primeiras linhas usando apenas números,
 	# pois o sed do BSD capota se tentar ler o conteúdo destas
