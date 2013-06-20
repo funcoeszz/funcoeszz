ea4f0145ad25cf8fd225969359b93ff09cf27c66
itamarnet <itamarnet@yahoo.com.br>
2013-06-19 22:22:43 -0300

    zzvdp: Incluindo a possibilidade de escolher a data da tirinha a ser exibida.

--- zz/zzvdp.sh
+++ zz/zzvdp.sh
@@ -1,21 +1,36 @@
 # ----------------------------------------------------------------------------
 # http://vidadeprogramador.com.br
 # Mostra o texto das últimas tirinhas de Vida de Programador.
+# Se fornecer uma data, mostra a tirinha do dia escolhido.
+# Você pode informar a data dd/mm/aaaa ou usar palavras: hoje, (ante)ontem.
+# Usando a mesma sintaxe do zzdata
 #
-# Uso: zzvdp
+# Uso: zzvdp [data [+|- data|número<d|m|a>]]
 # Ex.: zzvdp
+#      zzvdp anteontem
 #
 # Autor: Itamar <itamarnet (a) yahoo com br>
 # Desde: 2013-03-25
-# Versão: 1
+# Versão: 2
 # Licença: GPL
-# Requisitos: zzunescape
+# Requisitos: zzunescape zzdatafmt
 # ----------------------------------------------------------------------------
 zzvdp ()
 {
 	zzzz -h vdp "$1" && return
 
-	$ZZWWWHTML "http://vidadeprogramador.com.br" |
+	local url="http://vidadeprogramador.com.br"
+	local ano mes dia
+
+	if [ "$1" ] && zztool testa_data $(zzdatafmt "$1")
+	then
+		ano=$(zzdatafmt "$1" | awk -F'/' '{print $3}')
+		mes=$(zzdatafmt "$1" | awk -F'/' '{print $2}')
+		dia=$(zzdatafmt "$1" | awk -F'/' '{print $1}')
+		url="${url}/${ano}/${mes}/${dia}"
+	fi
+
+	$ZZWWWHTML $url |
 	sed -n '/title="Link permanente para /,/title="Comentário para /p' |
 	sed 's/<[^>]*>//g;/[0-9]\{1,\} resposta/d;/Tweet/d;s/^[[:blank:]]*/ /g' |
 	sed 's/^ *Camiseta: .*/&\n----------------------------------------------------------------------------/g' |
