150e42849228114b54605df522e6ccd8cfc25d16
Mauricio Calligaris <mauriciocalligaris@gmail.com>
2013-06-21 01:59:24 -0300

    zzoperadora: Alteração no sed, comentarios
    Atualizado sed, e removido o comentário sobre o awk.

--- zz/zzoperadora.sh
+++ zz/zzoperadora.sh
@@ -32,10 +32,9 @@ zzoperadora ()
 	# Faz a consulta no site e retira as informações da tag <title>
 	# do html da página.
 	resultado=$(curl -s --data-urlencode $post $url |
-		grep \<title\>Resultado |
-		sed 's/<[^>]*>//g;s/-\{0,1\}\( \)\{0,1\}[[:upper:]][a-z]*: /\1/g');
+		grep '<title>Resultado' |
+		sed 's/<[^>]*>//g;s/-\{0,1\} \{0,1\}[[:upper:]][a-z]*: //g');
 
-	# Usa o awk pra limpar os dados
 	if test -n "$resultado"
 	then
 		echo $resultado;
