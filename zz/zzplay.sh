5edf171fa09ff5e5ffe97aa865834334d5e9bb20
itamarnet <itamarnet@yahoo.com.br>
2013-07-19 14:12:02 -0300

    zzplay: Retirando opção de múltiplos arquivos de áudio. Envio de sinal de interrupção apenas pula para próximo arquivo de áudio.

--- zz/zzplay.sh
+++ zz/zzplay.sh
@@ -6,7 +6,7 @@
 #
 # Autor: Itamar <itamarnet (a) yahoo com br>
 # Desde: 2013-03-13
-# Versão: 1
+# Versão: 2
 # Licença: GPL
 # Requisitos: zzextensao zzminusculas
 # ----------------------------------------------------------------------------
@@ -19,56 +19,51 @@ zzplay ()
 	# Verificação dos parâmetros
 	[ "$1" ] || { zztool uso play; return 1; }
 
-	while [ "$1" ]
-	do
-		tipo=$(zzextensao "$1" | zzminusculas)
+	tipo=$(zzextensao "$1" | zzminusculas)
 
-		# Para cada tipo de arquivo de audio, seleciona o player disponivel
-		case $tipo in
-		wav | au | aiff )
-			for play_cmd in play mplayer cvlc
-			do
-				if type $play_cmd >/dev/null 2>&1
-				then
-					player="$play_cmd"
-					break
-				fi
-			done
-		;;
-		mp2 | mp3 )
-			for play_cmd in mpg321 mpg123 mplayer cvlc
-			do
-				if type $play_cmd >/dev/null 2>&1
-				then
-					player="$play_cmd"
-					break
-				fi
-			done
-		;;
-		ogg )
-			for play_cmd in ogg123 mplayer cvlc
-			do
-				if type $play_cmd >/dev/null 2>&1
-				then
-					player="$play_cmd"
-					break
-				fi
-			done
-		;;
-		aac | wma | mka )
-			for play_cmd in mplayer cvlc
-			do
-				if type $play_cmd >/dev/null 2>&1
-				then
-					player="$play_cmd"
-					break
-				fi
-			done
-		;;
-		esac
+	# Para cada tipo de arquivo de audio, seleciona o player disponivel
+	case $tipo in
+	wav | au | aiff )
+		for play_cmd in play mplayer cvlc
+		do
+			if type $play_cmd >/dev/null 2>&1
+			then
+				player="$play_cmd"
+				break
+			fi
+		done
+	;;
+	mp2 | mp3 )
+		for play_cmd in mpg321 mpg123 mplayer cvlc
+		do
+			if type $play_cmd >/dev/null 2>&1
+			then
+				player="$play_cmd"
+				break
+			fi
+		done
+	;;
+	ogg )
+		for play_cmd in ogg123 mplayer cvlc
+		do
+			if type $play_cmd >/dev/null 2>&1
+			then
+				player="$play_cmd"
+				break
+			fi
+		done
+	;;
+	aac | wma | mka )
+		for play_cmd in mplayer cvlc
+		do
+			if type $play_cmd >/dev/null 2>&1
+			then
+				player="$play_cmd"
+				break
+			fi
+		done
+	;;
+	esac
 
-		[ "$player" ] && $player "$1" >/dev/null 2>&1
-
-		shift
-	done
+	[ "$player" ] && $player "$1" >/dev/null 2>&1
 }
