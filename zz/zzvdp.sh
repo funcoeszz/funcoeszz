13c85c507891aa4ceff06c1b92d55cf9d522d4db
Aurelio Jargas <verde@aurelio.net>
2013-07-23 08:38:06 -0300

    zzvdp: portabilidade: \n no sed não é portável

--- zz/zzvdp.sh
+++ zz/zzvdp.sh
@@ -11,7 +11,7 @@
 #
 # Autor: Itamar <itamarnet (a) yahoo com br>
 # Desde: 2013-03-25
-# Versão: 3
+# Versão: 4
 # Licença: GPL
 # Requisitos: zzunescape zzdatafmt
 # ----------------------------------------------------------------------------
@@ -29,6 +29,7 @@ zzvdp ()
 	$ZZWWWHTML $url |
 	sed -n '/title="Link permanente para /,/title="Comentário para /p' |
 	sed 's/<[^>]*>//g;/[0-9]\{1,\} resposta/d;/Tweet/d;s/^[[:blank:]]*/ /g' |
-	sed 's/^ *Camiseta: .*/&\n----------------------------------------------------------------------------/g' |
+	sed '/^ *Camiseta: .*/ a \
+----------------------------------------------------------------------------' |
 	zzunescape --html | uniq
 }
