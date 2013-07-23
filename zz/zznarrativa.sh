725bb7e994b5c5d0223537fcfa17778167e42f88
Aurelio Jargas <verde@aurelio.net>
2013-07-23 15:36:59 -0300

    zznarrativa: Agora usando o comando genérico zzplay

--- zz/zznarrativa.sh
+++ zz/zznarrativa.sh
@@ -7,9 +7,9 @@
 #
 # Autor: Kl0nEz <kl0nez (a) wifi org br>
 # Desde: 2011-08-23
-# Versão: 2
+# Versão: 3
 # Licença: GPLv2
-# Requisitos: mpg123 ou afplay
+# Requisitos: zzplay
 # ----------------------------------------------------------------------------
 zznarrativa ()
 {
@@ -18,20 +18,13 @@ zznarrativa ()
 	[ "$1" ] || { zztool uso narrativa; return 1; }
 
 	# Variaveis locais
-	local padrao play_cmd
+	local padrao
 	local url='http://translate.google.com.br'
 	local charset_para='UTF-8'
 	local audio_file="/tmp/$$.WAV"
 
-	if test -f /usr/bin/afplay
-	then
-		play_cmd='afplay'     # mac
-	else
-		play_cmd='mpg123 -q'  # linux
-	fi
-
 	# Narrativa
 	padrao=$(echo "$*" | sed "$ZZSEDURL")
 	local audio="translate_tts?ie=$charset_para&q=$padrao&tl=pt&prev=input"
-	$ZZWWWHTML "$url/$audio" > $audio_file && $play_cmd $audio_file && rm -rf $audio_file
+	$ZZWWWHTML "$url/$audio" > $audio_file && zzplay $audio_file &&	rm -rf $audio_file
 }
