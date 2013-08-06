0f5eec7d65ec92346284026f165f4a04d1653238
Aurelio Jargas <verde@aurelio.net>
2013-08-06 08:37:53 -0300

    testador NOVO: zzhorariodeverao.sh convertida

--- testador/zzhorariodeverao.sh
+++ testador/zzhorariodeverao.sh
@@ -1,23 +1,28 @@
-#!/usr/bin/env bash
-debug=0
-values=1
-tests=(
-1995	t	"Antes de 2008 não havia regra fixa para o horário de verão"
-2007	t	"Antes de 2008 não havia regra fixa para o horário de verão"
-2008	t	"19/10/2008\n15/02/2009"
-2009	t	"18/10/2009\n21/02/2010"
-2010	t	"17/10/2010\n20/02/2011"
-
 # Erros
-01/01/1970	t	"Ano inválido '01/01/1970'"
--2000 		t	"Ano inválido '-2000'"
-0 		t	"Ano inválido '0'"
-foo 		t	"Ano inválido 'foo'"
+
+$ zzhorariodeverao 01/01/1970	#→ Ano inválido '01/01/1970'
+$ zzhorariodeverao -2000 	#→ Ano inválido '-2000'
+$ zzhorariodeverao 0 		#→ Ano inválido '0'
+$ zzhorariodeverao foo 		#→ Ano inválido 'foo'
 
 # Epoch
-1		t	"Antes de 2008 não havia regra fixa para o horário de verão"
-10 		t	"Antes de 2008 não havia regra fixa para o horário de verão"
-100 		t	"Antes de 2008 não havia regra fixa para o horário de verão"
-1969 		t	"Antes de 2008 não havia regra fixa para o horário de verão"
-)
-. _lib
+
+$ zzhorariodeverao 1		#→ Antes de 2008 não havia regra fixa para o horário de verão
+$ zzhorariodeverao 10 		#→ Antes de 2008 não havia regra fixa para o horário de verão
+$ zzhorariodeverao 100 		#→ Antes de 2008 não havia regra fixa para o horário de verão
+$ zzhorariodeverao 1969 	#→ Antes de 2008 não havia regra fixa para o horário de verão
+$ zzhorariodeverao 1995		#→ Antes de 2008 não havia regra fixa para o horário de verão
+$ zzhorariodeverao 2007		#→ Antes de 2008 não havia regra fixa para o horário de verão
+
+# Uso normal
+
+$ zzhorariodeverao 2008
+19/10/2008
+15/02/2009
+$ zzhorariodeverao 2009
+18/10/2009
+21/02/2010
+$ zzhorariodeverao 2010
+17/10/2010
+20/02/2011
+$
