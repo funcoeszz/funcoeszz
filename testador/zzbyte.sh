0cfa7389700b6af73e0f5adc7ef9932ec0ce3510
Aurelio Jargas <verde@aurelio.net>
2013-08-06 07:41:32 -0300

    testador NOVO: zzbyte.sh convertida

--- testador/zzbyte.sh
+++ testador/zzbyte.sh
@@ -1,91 +1,86 @@
-#!/usr/bin/env bash
-debug=0
-values=3
-tests=(
-''	''	''	r	^Uso:.*
-a	''	''	t	"Número inválido 'a'"
--1	''	''	t	"Número inválido '-1'"
-1.0	''	''	t	"Número inválido '1.0'"
-1,0	''	''	t	"Número inválido '1,0'"
-# ' '	''	''	t	"Número inválido ' '"
+$ zzbyte				#→ --regex ^Uso:
+$ zzbyte	a			#→ Número inválido 'a'
+$ zzbyte	-1			#→ Número inválido '-1'
+$ zzbyte	1.0			#→ Número inválido '1.0'
+$ zzbyte	1,0			#→ Número inválido '1,0'
+# $ zzbyte  ' '				#→ Número inválido ' '
 
-0	x	''	r	"Unidade inv.lida '.'"
-0	B	x	r	"Unidade inv.lida '.'"
+$ zzbyte	0	x		#→ Unidade inválida 'X'
+$ zzbyte	0	B	x	#→ Unidade inválida 'X'
 
-1073741824	Z	''	t	1048576Y # unidade maxima
+# unidade máxima
+$ zzbyte	1073741824	Z	#→ 1048576Y
 
-# o zero fica na unidade escolhda
-0	''	''	t	0B
-0	g	''	t	0G
-0	g	k	t	0K
-0	g	p	t	0P
+# o zero fica na unidade escolhida
+$ zzbyte	0			#→ 0B
+$ zzbyte	0	g		#→ 0G
+$ zzbyte	0	g	k	#→ 0K
+$ zzbyte	0	g	p	#→ 0P
 
 # aumento de grandezas
-1	''	''	t	1B
-10	''	''	t	10B
-100	''	''	t	100B
-1000	''	''	t	1000B
+$ zzbyte	1			#→ 1B
+$ zzbyte	10			#→ 10B
+$ zzbyte	100			#→ 100B
+$ zzbyte	1000			#→ 1000B
 
-# ate 1023 nao vira
-1023	''	''	t	1023B
-1023	b	''	t	1023B
-1023	k	''	t	1023K
+# até 1023 não vira
+$ zzbyte	1023			#→ 1023B
+$ zzbyte	1023	b		#→ 1023B
+$ zzbyte	1023	k		#→ 1023K
 
-# 1024 eh o ponto de virada
-1024	''	''	t	1K
-1024	b	''	t	1K
-1024	k	''	t	1M
-1024	b	k	t	1K
-1024	k	b	t	1048576B
+# 1024 é o ponto de virada
+$ zzbyte	1024			#→ 1K
+$ zzbyte	1024	b		#→ 1K
+$ zzbyte	1024	k		#→ 1M
+$ zzbyte	1024	b	k	#→ 1K
+$ zzbyte	1024	k	b	#→ 1048576B
 
-# decresce ate B
-1	m	b	t	1048576B
-1	g	b	t	1073741824B
+# decresce até B
+$ zzbyte	1	m	b	#→ 1048576B
+$ zzbyte	1	g	b	#→ 1073741824B
 
 # cresce desde B
-1048576	b	k	t	1024K
-1048576	b	m	t	1M
-1048576	b	''	t	1M
-1048576	''	''	t	1M
+$ zzbyte	1048576	b	k	#→ 1024K
+$ zzbyte	1048576	b	m	#→ 1M
+$ zzbyte	1048576	b		#→ 1M
+$ zzbyte	1048576			#→ 1M
 
 # aumento de grandezas em M
-5	m	g	t	0.004G
-50	m	g	t	0.048G
-500	m	g	t	0.488G
-5000	m	g	t	4G
+$ zzbyte	5	m	g	#→ 0.004G
+$ zzbyte	50	m	g	#→ 0.048G
+$ zzbyte	500	m	g	#→ 0.488G
+$ zzbyte	5000	m	g	#→ 4G
 
-# unidades de entrada e saida iguais
-5	g	g	t	5G
+# unidades de entrada e saída iguais
+$ zzbyte	5	g	g	#→ 5G
 
 # 1 T em todas as unidades
-1	t	b	t	1099511627776B
-1	t	k	t	1073741824K
-1	t	m	t	1048576M
-1	t	g	t	1024G
-1	t	t	t	1T
-1	t	p	t	0P
-1	t	e	t	0E
-1	t	z	t	0Z
-1	t	y	t	0Y
+$ zzbyte	1	t	b	#→ 1099511627776B
+$ zzbyte	1	t	k	#→ 1073741824K
+$ zzbyte	1	t	m	#→ 1048576M
+$ zzbyte	1	t	g	#→ 1024G
+$ zzbyte	1	t	t	#→ 1T
+$ zzbyte	1	t	p	#→ 0P
+$ zzbyte	1	t	e	#→ 0E
+$ zzbyte	1	t	z	#→ 0Z
+$ zzbyte	1	t	y	#→ 0Y
 
 # 2 T em todas as unidades
-2	t	b	t	2199023255552B
-2	t	k	t	2147483648K
-2	t	m	t	2097152M
-2	t	g	t	2048G
-2	t	t	t	2T
-2	t	p	t	0.001P
-2	t	e	r	0[.,]000001E
-2	t	z	r	0[.,]000000001Z
-2	t	y	r	0[.,]000000000001Y
+$ zzbyte	2	t	b	#→ 2199023255552B
+$ zzbyte	2	t	k	#→ 2147483648K
+$ zzbyte	2	t	m	#→ 2097152M
+$ zzbyte	2	t	g	#→ 2048G
+$ zzbyte	2	t	t	#→ 2T
+$ zzbyte	2	t	p	#→ 0.001P
+$ zzbyte	2	t	e	#→ --regex 0[.,]000001E
+$ zzbyte	2	t	z	#→ --regex 0[.,]000000001Z
+$ zzbyte	2	t	y	#→ --regex 0[.,]000000000001Y
 
 # Limites dos poucos B
-1	b	k	t	0K
-1	b	y	t	0Y
-2	b	k	t	0.001K
-2	b	y	r	0[.,]000000000000000000000001Y
+$ zzbyte	1	b	k	#→ 0K
+$ zzbyte	1	b	y	#→ 0Y
+$ zzbyte	2	b	k	#→ 0.001K
+$ zzbyte	2	b	y	#→ --regex 0[.,]000000000000000000000001Y
 
 # Limite de descida de 1Y sem estourar a pilha do Bash
-1	y	m	t	1152921504606846976M
-)
-. _lib
+$ zzbyte	1	y	m	#→ 1152921504606846976M
