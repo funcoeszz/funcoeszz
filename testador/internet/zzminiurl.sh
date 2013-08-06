8063f6075d6bd884d8bf7bf85c3d0dbac031f898
Aurelio Jargas <verde@aurelio.net>
2013-08-06 10:24:50 -0300

    testador NOVO: zzminiurl.sh convertida

--- testador/internet/zzminiurl.sh
+++ testador/internet/zzminiurl.sh
@@ -1,9 +1,3 @@
-#!/usr/bin/env bash
-
-values=1
-tests=(
-''			r	^Uso:.*
-funcoeszz.net		r	"^http://migre.me/[A-Za-z0-9][A-Za-z0-9]*$"
-http://funcoeszz.net	r	"^http://migre.me/[A-Za-z0-9][A-Za-z0-9]*$"
-)
-. _lib
+$ zzminiurl                       #→ --regex ^Uso:
+$ zzminiurl funcoeszz.net         #→ --regex ^http://migre.me/[A-Za-z0-9]+$
+$ zzminiurl http://funcoeszz.net  #→ --regex ^http://migre.me/[A-Za-z0-9]+$
