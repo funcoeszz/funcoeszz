45edd2f852e2ce5e99a667dc620afad636a56444
Aurelio Jargas <verde@aurelio.net>
2013-08-06 08:41:41 -0300

    testador NOVO: zzjuntalinhas.sh convertida

--- testador/zzjuntalinhas.sh
+++ testador/zzjuntalinhas.sh
@@ -1,48 +1,143 @@
-#!/usr/bin/env bash
-debug=0
-values=5
+$ cat _numeros.txt
+1
+2
+3
+4
+5
+$
 
-tests=(
 # sem opções
-'_numeros'	''	''	''	''	t	"1\t2\t3\t4\t5"
+
+$ zzjuntalinhas	_numeros.txt
+1	2	3	4	5
+$
 
 # -d
-# -d	''	'_numeros'	''	''	t	"12345"  # cant test
--d	@	'_numeros'	''	''	t	"1@2@3@4@5"
--d	::	'_numeros'	''	''	t	"1::2::3::4::5"
-# -d	'{ }'	'_numeros'	''	''	t	"1{ }2{ }3{ }4{ }5"
 
-# números e $
-# -d	@	-i	0	_numeros	t	"1@2@3@4@5"  # depende do sed, o GNU entende endereço zero, BSD não
--d	@	-i	1	_numeros	t	"1@2@3@4@5"
--d	@	-i	3	_numeros	t	"1\n2\n3@4@5"
--d	@	-i	$	_numeros	t	"1\n2\n3\n4\n5"
--d	@	-f	1	_numeros	t	"1\n2\n3\n4\n5"
--d	@	-f	3	_numeros	t	"1@2@3\n4\n5"
--d	@	-f	$	_numeros	t	"1@2@3@4@5"
--i	2	-f	4	_numeros	t	"1\n2\t3\t4\n5"
-# -i	4	-f	2	_numeros	t	"1\n2\n3\n4\t5"  # bug: got '1 2 3 5'
--i	4	-f	$	_numeros	t	"1\n2\n3\n4\t5"
--i	1	-f	$	_numeros	t	"1\t2\t3\t4\t5"
--i	4	-f	4	_numeros	t	"1\n2\n3\n4\n5"
--i	99	-f	$	_numeros	t	"1\n2\n3\n4\n5"
--i	4	-f	99	_numeros	t	"1\n2\n3\n4\n5"
+$ zzjuntalinhas	-d	''	_numeros.txt	#→ 12345
+$ zzjuntalinhas	-d	@	_numeros.txt	#→ 1@2@3@4@5
+$ zzjuntalinhas	-d	::	_numeros.txt	#→ 1::2::3::4::5
+$ zzjuntalinhas	 -d	'{ }'	_numeros.txt	#→ 1{ }2{ }3{ }4{ }5
 
-# regex
--d	@	-i	^1$	_numeros	t	"1@2@3@4@5"
--d	@	-i	^3$	_numeros	t	"1\n2\n3@4@5"
--d	@	-f	^1$	_numeros	t	"1\n2\n3\n4\n5"
--d	@	-f	^3$	_numeros	t	"1@2@3\n4\n5"
--i	^2$	-f	^4$	_numeros	t	"1\n2\t3\t4\n5"
+# Números e $
 
-# escape da /
--d	@	-f	a/b	_numeros	t	"1\n2\n3\n4\n5"
--d	@	-i	a/b	_numeros	t	"1\n2\n3\n4\n5"
--d	/	-i	1	_numeros	t	"1/2/3/4/5"
--i	a/b	-f	a/b	_numeros	t	"1\n2\n3\n4\n5"
+# depende do sed, o GNU entende endereço zero, BSD não
+# $ zzjuntalinhas	-d	@	-i	0	_numeros.txt
+# 1@2@3@4@5
+$ zzjuntalinhas	-d	@	-i	1	_numeros.txt
+1@2@3@4@5
+$ zzjuntalinhas	-d	@	-i	3	_numeros.txt
+1
+2
+3@4@5
+$ zzjuntalinhas	-d	@	-i	$	_numeros.txt
+1
+2
+3
+4
+5
+$ zzjuntalinhas	-d	@	-f	1	_numeros.txt
+1
+2
+3
+4
+5
+$ zzjuntalinhas	-d	@	-f	3	_numeros.txt
+1@2@3
+4
+5
+$ zzjuntalinhas	-d	@	-f	$	_numeros.txt
+1@2@3@4@5
+$ zzjuntalinhas	-i	2	-f	4	_numeros.txt
+1
+2	3	4
+5
+$ zzjuntalinhas	-i	4	-f	2	_numeros.txt
+1
+2
+3
+4	5
+$ zzjuntalinhas	-i	4	-f	$	_numeros.txt
+1
+2
+3
+4	5
+$ zzjuntalinhas	-i	1	-f	$	_numeros.txt
+1	2	3	4	5
+$ zzjuntalinhas	-i	4	-f	4	_numeros.txt
+1
+2
+3
+4
+5
+$ zzjuntalinhas	-i	99	-f	$	_numeros.txt
+1
+2
+3
+4
+5
+$ zzjuntalinhas	-i	4	-f	99	_numeros.txt
+1
+2
+3
+4
+5
+$
 
-# início e fim na mesma linha
--i	^2	-f	:	_dados		t	"1:um:one\n2:dois:two\n3:tres:three\n4:quatro:four\n5:cinco:five"
+# Regex
 
-)
-. _lib
+$ zzjuntalinhas	-d	@	-i	^1$	_numeros.txt
+1@2@3@4@5
+$ zzjuntalinhas	-d	@	-i	^3$	_numeros.txt
+1
+2
+3@4@5
+$ zzjuntalinhas	-d	@	-f	^1$	_numeros.txt
+1
+2
+3
+4
+5
+$ zzjuntalinhas	-d	@	-f	^3$	_numeros.txt
+1@2@3
+4
+5
+$ zzjuntalinhas	-i	^2$	-f	^4$	_numeros.txt
+1
+2	3	4
+5
+$
+
+# Escape da /
+
+$ zzjuntalinhas	-d	@	-f	a/b	_numeros.txt
+1
+2
+3
+4
+5
+$ zzjuntalinhas	-d	@	-i	a/b	_numeros.txt
+1
+2
+3
+4
+5
+$ zzjuntalinhas	-d	/	-i	1	_numeros.txt
+1/2/3/4/5
+$ zzjuntalinhas	-i	a/b	-f	a/b	_numeros.txt
+1
+2
+3
+4
+5
+$
+
+# Início e fim na mesma linha
+
+$ zzjuntalinhas	-i	^2	-f	:	_dados.txt
+1:um:one
+2:dois:two
+3:tres:three
+4:quatro:four
+5:cinco:five
+$
