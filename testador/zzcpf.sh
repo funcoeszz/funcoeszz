2c9a2547982e8a4c457d1e683d4e4caa126f908f
Aurelio Jargas <verde@aurelio.net>
2013-08-06 07:57:42 -0300

    testador NOVO: zzcpf.sh convertida

--- testador/zzcpf.sh
+++ testador/zzcpf.sh
@@ -1,14 +1,17 @@
-#!/usr/bin/env bash
-debug=0
-values=1
-tests=(
-123456789	r "CPF inv.lido \(deve ter 11 d.gitos\)"
-123456789012	r "CPF inv.lido \(deve ter 11 d.gitos\)"
-12345678900	r "CPF inv.lido \(deveria terminar em 09\)"
-12345678909	r "CPF v.lido"
-123.456.789-09	r "CPF v.lido"
-'123 456 789 09'	r "CPF v.lido"
-'z123	4_5=6*7@8+9?0a9'	r "CPF v.lido"
-''		r "[0-9]{3}\.[0-9]{3}\.[0-9]{3}-[0-9]{2}"
-)
-. _lib
+# Número incorreto de dígitos
+$ zzcpf 123456789			#→ CPF inválido (deve ter 11 dígitos)
+$ zzcpf 123456789012			#→ CPF inválido (deve ter 11 dígitos)
+
+# Dígito verificador incorreto
+$ zzcpf 12345678900			#→ CPF inválido (deveria terminar em 09)
+
+# Números válidos, com ou sem pontuação
+$ zzcpf 12345678909			#→ CPF válido
+$ zzcpf 123.456.789-09			#→ CPF válido
+
+# Qualquer caractere não numérico é removido
+$ zzcpf '123 456 789 09'		#→ CPF válido
+$ zzcpf 'z123	4_5=6*7@8+9?0a9'	#→ CPF válido
+
+# Sem argumentos, gera um CPF aleatório
+$ zzcpf				 	#→ --regex \d{3}\.\d{3}\.\d{3}-\d{2}
