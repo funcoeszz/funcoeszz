045200380a991e0e1b04fcd535461c8d5356d24e
Aurelio Jargas <verde@aurelio.net>
2013-08-06 08:35:55 -0300

    testador NOVO: zzhexa2str.sh convertida

--- testador/zzhexa2str.sh
+++ testador/zzhexa2str.sh
@@ -1,13 +1,6 @@
-#!/usr/bin/env bash
-debug=0
-values=1
-tests=(
-
 # a-f
-"62 69 6e 61 72 69 6f"	t	binario
+$ zzhexa2str	"62 69 6e 61 72 69 6f"			#→ binario
 # A-F
-"62 69 6E 61 72 69 6F"	t	binario
+$ zzhexa2str	"62 69 6E 61 72 69 6F"			#→ binario
 # A-F, prefixo 0x
-"0x62 0x69 0x6e 0x61 0x72 0x69 0x6F"	t	binario
-)
-. _lib
+$ zzhexa2str	"0x62 0x69 0x6e 0x61 0x72 0x69 0x6F"	#→ binario
