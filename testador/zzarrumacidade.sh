b6b5d428572c965bb551144ced2c806793b8a926
Aurelio Jargas <verde@aurelio.net>
2013-08-06 07:33:29 -0300

    testador NOVO: zzarrumacidade.sh convertida

--- testador/zzarrumacidade.sh
+++ testador/zzarrumacidade.sh
@@ -1,95 +1,85 @@
-#!/usr/bin/env bash
-
-values=1
-tests=(
-
 # Volta algumas iniciais para minúsculas
-"foo e bar"		t	"Foo e Bar"
-"foo de bar"		t	"Foo de Bar"
-"foo da bar"		t	"Foo da Bar"
-"foo do bar"		t	"Foo do Bar"
-"foo das bar"		t	"Foo das Bar"
-"foo dos bar"		t	"Foo dos Bar"
+$ zzarrumacidade "foo e bar"                      #→ Foo e Bar
+$ zzarrumacidade "foo de bar"                     #→ Foo de Bar
+$ zzarrumacidade "foo da bar"                     #→ Foo da Bar
+$ zzarrumacidade "foo do bar"                     #→ Foo do Bar
+$ zzarrumacidade "foo das bar"                    #→ Foo das Bar
+$ zzarrumacidade "foo dos bar"                    #→ Foo dos Bar
 
 # abreviações
-"SP"			t	"São Paulo"
-"RJ"			t	"Rio de Janeiro"
-"BH"			t	"Belo Horizonte"
-"BSB"			t	"Brasília"
-"RIO"			t	"Rio de Janeiro"
-"SAMPA"			t	"São Paulo"
-"FLORIPA"		t	"Florianópolis"
+$ zzarrumacidade "SP"                             #→ São Paulo
+$ zzarrumacidade "RJ"                             #→ Rio de Janeiro
+$ zzarrumacidade "BH"                             #→ Belo Horizonte
+$ zzarrumacidade "BSB"                            #→ Brasília
+$ zzarrumacidade "RIO"                            #→ Rio de Janeiro
+$ zzarrumacidade "SAMPA"                          #→ São Paulo
+$ zzarrumacidade "FLORIPA"                        #→ Florianópolis
 
 # Restaura acentuação às capitais
-"BELEM"			t	"Belém"
-"BRASILIA"		t	"Brasília"
-"CUIABA"		t	"Cuiabá"
-"FLORIANOPOLIS"		t	"Florianópolis"
-"GOIANIA"		t	"Goiânia"
-"JOAO PESSOA"		t	"João Pessoa"
-"MACAPA"		t	"Macapá"
-"MACEIO"		t	"Maceió"
-"SAO LUIS"		t	"São Luís"
-"VITORIA"		t	"Vitória"
+$ zzarrumacidade "BELEM"                          #→ Belém
+$ zzarrumacidade "BRASILIA"                       #→ Brasília
+$ zzarrumacidade "CUIABA"                         #→ Cuiabá
+$ zzarrumacidade "FLORIANOPOLIS"                  #→ Florianópolis
+$ zzarrumacidade "GOIANIA"                        #→ Goiânia
+$ zzarrumacidade "JOAO PESSOA"                    #→ João Pessoa
+$ zzarrumacidade "MACAPA"                         #→ Macapá
+$ zzarrumacidade "MACEIO"                         #→ Maceió
+$ zzarrumacidade "SAO LUIS"                       #→ São Luís
+$ zzarrumacidade "VITORIA"                        #→ Vitória
 
 # Corrige capital
-"são luis"		t	"São Luís"
-"sao luís"		t	"São Luís"
-"são luís"		t	"São Luís"
-"sao luiz"		t	"São Luís"
-"são luiz"		t	"São Luís"
-"sao luíz"		t	"São Luís"
-"são luíz"		t	"São Luís"
+$ zzarrumacidade "são luis"                       #→ São Luís
+$ zzarrumacidade "sao luís"                       #→ São Luís
+$ zzarrumacidade "são luís"                       #→ São Luís
+$ zzarrumacidade "sao luiz"                       #→ São Luís
+$ zzarrumacidade "são luiz"                       #→ São Luís
+$ zzarrumacidade "sao luíz"                       #→ São Luís
+$ zzarrumacidade "são luíz"                       #→ São Luís
 
 # Muitas cidades emprestam o nome do estado
-"foo do amapa"		t	"Foo do Amapá"
-"foo do ceara"		t	"Foo do Ceará"
-"foo do goias"		t	"Foo do Goiás"
-"foo do maranhao"	t	"Foo do Maranhão"
-"foo do para"		t	"Foo do Pará"
-"foo do paraiba"	t	"Foo do Paraíba"
-"foo do parana"		t	"Foo do Paraná"
-"foo do piaui"		t	"Foo do Piauí"
-"foo do rondonia"	t	"Foo do Rondônia"
+$ zzarrumacidade "foo do amapa"                   #→ Foo do Amapá
+$ zzarrumacidade "foo do ceara"                   #→ Foo do Ceará
+$ zzarrumacidade "foo do goias"                   #→ Foo do Goiás
+$ zzarrumacidade "foo do maranhao"                #→ Foo do Maranhão
+$ zzarrumacidade "foo do para"                    #→ Foo do Pará
+$ zzarrumacidade "foo do paraiba"                 #→ Foo do Paraíba
+$ zzarrumacidade "foo do parana"                  #→ Foo do Paraná
+$ zzarrumacidade "foo do piaui"                   #→ Foo do Piauí
+$ zzarrumacidade "foo do rondonia"                #→ Foo do Rondônia
 
 # O nome de alguns estados pode aparecer no início/meio
-"amapa do sul"		t	"Amapá do Sul"
-"espirito do sul"	t	"Espírito do Sul"
-"para do sul"		t	"Pará do Sul"
-"paraiba do sul"	t	"Paraíba do Sul"
+$ zzarrumacidade "amapa do sul"                   #→ Amapá do Sul
+$ zzarrumacidade "espirito do sul"                #→ Espírito do Sul
+$ zzarrumacidade "para do sul"                    #→ Pará do Sul
+$ zzarrumacidade "paraiba do sul"                 #→ Paraíba do Sul
 
 # Restaura acentuação de maneira genérica
-"uberlandia"			t	"Uberlândia"
-"rolandia"			t	"Rolândia"
-"florianopolis"			t	"Florianópolis"
-"virginopolis"			t	"Virginópolis"
-"alto caparao"			t	"Alto Caparaó"    # exceção
-"caparao"			t	"Caparaó"
-"sao Francisco"			t	"São Francisco"
-"sao joao do capao ribeirao"	t	"São João do Capão Ribeirão"    # ão várias vezes
+$ zzarrumacidade "uberlandia"                     #→ Uberlândia
+$ zzarrumacidade "rolandia"                       #→ Rolândia
+$ zzarrumacidade "florianopolis"                  #→ Florianópolis
+$ zzarrumacidade "virginopolis"                   #→ Virginópolis
+$ zzarrumacidade "caparao"                        #→ Caparaó
+$ zzarrumacidade "sao Francisco"                  #→ São Francisco
+# exceção
+$ zzarrumacidade "alto caparao"                   #→ Alto Caparaó
+# ão várias vezes
+$ zzarrumacidade "sao joao do capao ribeirao"     #→ São João do Capão Ribeirão
 
 # Exceções pontuais
-"morro cabeça no tempo"		t	"Morro Cabeça no Tempo"
-"passa-e-fica"			t	"Passa-e-Fica"
-"são joão del-rei"		t	"São João del-Rei"
-"xangri-lá"			t	"Xangri-Lá"
-"pedro ii"			t	"Pedro II"
-"pio ix"			t	"Pio IX"
-"pio xii"			t	"Pio XII"
-"estrela d'oeste"		t	"Estrela d'Oeste"
-"sítio d'abadia"		t	"Sítio d'Abadia"
-"dias d'ávila"			t	"Dias d'Ávila"
-"são joão do pau-d'alho"	t	"São João do Pau-d'Alho"
-"olhos-d'água"			t	"Olhos-d'Água"
-"pau-d'arco"			t	"Pau-d'Arco"
-)
-. _lib
-
+$ zzarrumacidade "morro cabeça no tempo"          #→ Morro Cabeça no Tempo
+$ zzarrumacidade "passa-e-fica"                   #→ Passa-e-Fica
+$ zzarrumacidade "são joão del-rei"               #→ São João del-Rei
+$ zzarrumacidade "xangri-lá"                      #→ Xangri-Lá
+$ zzarrumacidade "pedro ii"                       #→ Pedro II
+$ zzarrumacidade "pio ix"                         #→ Pio IX
+$ zzarrumacidade "pio xii"                        #→ Pio XII
+$ zzarrumacidade "estrela d'oeste"                #→ Estrela d'Oeste
+$ zzarrumacidade "sítio d'abadia"                 #→ Sítio d'Abadia
+$ zzarrumacidade "dias d'ávila"                   #→ Dias d'Ávila
+$ zzarrumacidade "são joão do pau-d'alho"         #→ São João do Pau-d'Alho
+$ zzarrumacidade "olhos-d'água"                   #→ Olhos-d'Água
+$ zzarrumacidade "pau-d'arco"                     #→ Pau-d'Arco
 
 # Testar cada palavra como um argumento diferente
-values=5
-tests=(
-rio	de	janeiro	''	''	t	"Rio de Janeiro"
-vargem	grande	do	rio	pardo	t	"Vargem Grande do Rio Pardo"
-)
-. _lib
+$ zzarrumacidade rio de janeiro                   #→ Rio de Janeiro
+$ zzarrumacidade vargem grande do rio pardo       #→ Vargem Grande do Rio Pardo
