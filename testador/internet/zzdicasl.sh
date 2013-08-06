6c4b90f13aadc178a9f5dfc1d627bf338e3cedcd
Aurelio Jargas <verde@aurelio.net>
2013-08-06 18:42:13 -0300

    testador NOVO: zzdicasl.sh criada

--- testador/internet/zzdicasl.sh
+++ testador/internet/zzdicasl.sh
@@ -1,3 +1,4 @@
+$ zzdicasl procmail
 http://www.dicas-l.com.br/arquivo/
 backup_de_e-mail_com_postfix_e_procmailrc.php
 bloqueando_e-mails_com_links_para_executaveis_com_procmail.php
@@ -7,3 +8,6 @@ dicas_procmail.php
 configuracao_do_programa_procmail.php
 evitando_cross-posting_com_procmail.php
 procmail_reducao_do_tamanho_de_cabecalhos_de_mensagens.php
+$ zzdicasl
+Uso: zzdicasl [opção-grep] palavra(s)
+$
