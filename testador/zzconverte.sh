3eb57875cba2b22f820a7fd9a2d34864c32e12b5
Aurelio Jargas <verde@aurelio.net>
2013-08-06 07:55:17 -0300

    testador NOVO: zzconverte.sh convertida

--- testador/zzconverte.sh
+++ testador/zzconverte.sh
@@ -1,57 +1,57 @@
-#!/usr/bin/env bash
-debug=0
-values=2
-tests=(
-cf	0	t	'0 C = 32.00 F'
-fc	0	t	'0 F = -17.77 C'
-ck	0	t	'0 C = 273.15 K'
-kc	0	t	'0 K = -273.15 C'
-kf	0	t	'0 K = -459.67 F'
-fk	0	t	'0 F = 255.37 K'
-km	0	t	'0 km = 0 milhas'
-mk	0	t	'0 milhas = 0 km'
-db	0	t	'0'
-bd	0	t	'0'
-cd	0	t	'48'
-dc	0	t	''
+$ zzconverte	cf	0	#→ 0 C = 32.00 F
+$ zzconverte	fc	0	#→ 0 F = -17.77 C
+$ zzconverte	ck	0	#→ 0 C = 273.15 K
+$ zzconverte	kc	0	#→ 0 K = -273.15 C
+$ zzconverte	kf	0	#→ 0 K = -459.67 F
+$ zzconverte	fk	0	#→ 0 F = 255.37 K
+$ zzconverte	km	0	#→ 0 km = 0 milhas
+$ zzconverte	mk	0	#→ 0 milhas = 0 km
+$ zzconverte	db	0	#→ 0
+$ zzconverte	bd	0	#→ 0
+$ zzconverte	cd	0	#→ 48
+$ zzconverte	dc	0	#→ 
 
-cf	-0	t	'-0 C = 32.00 F'
-fc	-0	t	'-0 F = -17.77 C'
-ck	-0	t	'-0 C = 273.15 K'
-kc	-0	t	'-0 K = -273.15 C'
-kf	-0	t	'-0 K = -459.67 F'
-fk	-0	t	'-0 F = 255.37 K'
-km	-0	t	'-0 km = 0 milhas'
-mk	-0	t	'-0 milhas = 0 km'
-db	-0	t	'0'
-bd	-0	t	'0'
-cd	-0	t	'45'
-dc	-0	t	''
+$ zzconverte	cf	-0	#→ -0 C = 32.00 F
+$ zzconverte	fc	-0	#→ -0 F = -17.77 C
+$ zzconverte	ck	-0	#→ -0 C = 273.15 K
+$ zzconverte	kc	-0	#→ -0 K = -273.15 C
+$ zzconverte	kf	-0	#→ -0 K = -459.67 F
+$ zzconverte	fk	-0	#→ -0 F = 255.37 K
+$ zzconverte	km	-0	#→ -0 km = 0 milhas
+$ zzconverte	mk	-0	#→ -0 milhas = 0 km
+$ zzconverte	db	-0	#→ 0
+$ zzconverte	bd	-0	#→ 0
+$ zzconverte	cd	-0	#→ 45
+$ zzconverte	dc	-0	#→ 
 
-cf	100	t	'100 C = 212.00 F'
-fc	100	t	'100 F = 37.77 C'
-ck	100	t	'100 C = 373.15 K'
-kc	100	t	'100 K = -173.15 C'
-kf	100	t	'100 K = -279.67 F'
-fk	100	t	'100 F = 310.92 K'
-km	100	t	'100 km = 62.1400 milhas'
-mk	100	t	'100 milhas = 160.900 km'
-db	100	t	'1100100'
-bd	100	t	'4'
-cd	100	t	'49'
-dc	100	t	'd'
+$ zzconverte	cf	100	#→ 100 C = 212.00 F
+$ zzconverte	fc	100	#→ 100 F = 37.77 C
+$ zzconverte	ck	100	#→ 100 C = 373.15 K
+$ zzconverte	kc	100	#→ 100 K = -173.15 C
+$ zzconverte	kf	100	#→ 100 K = -279.67 F
+$ zzconverte	fk	100	#→ 100 F = 310.92 K
+$ zzconverte	km	100	#→ 100 km = 62.1400 milhas
+$ zzconverte	mk	100	#→ 100 milhas = 160.900 km
+$ zzconverte	db	100	#→ 1100100
+$ zzconverte	bd	100	#→ 4
+$ zzconverte	cd	100	#→ 49
+$ zzconverte	dc	100	#→ d
 
-cf	-100	t	'-100 C = -148.00 F'
-fc	-100	t	'-100 F = -73.33 C'
-ck	-100	t	'-100 C = 173.15 K'
-kc	-100	t	'-100 K = -373.15 C'
-kf	-100	t	'-100 K = -639.67 F'
-fk	-100	t	'-100 F = 199.81 K'
-km	-100	t	'-100 km = -62.1400 milhas'
-mk	-100	t	'-100 milhas = -160.900 km'
-db	-100	t	'-1100100'
-bd	-100	t	'-100'
-cd	-100	t	'45'
-# dc	-100	t	"?" # !
-)
-. _lib
+$ zzconverte	cf	-100	#→ -100 C = -148.00 F
+$ zzconverte	fc	-100	#→ -100 F = -73.33 C
+$ zzconverte	ck	-100	#→ -100 C = 173.15 K
+$ zzconverte	kc	-100	#→ -100 K = -373.15 C
+$ zzconverte	kf	-100	#→ -100 K = -639.67 F
+$ zzconverte	fk	-100	#→ -100 F = 199.81 K
+$ zzconverte	km	-100	#→ -100 km = -62.1400 milhas
+$ zzconverte	mk	-100	#→ -100 milhas = -160.900 km
+$ zzconverte	db	-100	#→ -1100100
+$ zzconverte	bd	-100	#→ -100
+$ zzconverte	cd	-100	#→ 45
+
+# $ zzconverte	 dc	-100	#→ "?" # !
+#
+# Aqui no Mac este teste dá um resultado bizarro:
+# $ zzconverte  dc -100
+# ?ffffffffffff9c
+# $
