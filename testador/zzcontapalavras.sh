f8104ddd2fb80c0c849978850d8425023f3ea310
Aurelio Jargas <verde@aurelio.net>
2013-08-06 07:54:40 -0300

    testador NOVO: zzcontapalavras.sh convertida
    Chega de minhocas com \n e \t! Agora é só colar a saída normal e pronto.

--- testador/zzcontapalavras.sh
+++ testador/zzcontapalavras.sh
@@ -1,32 +1,29 @@
-#!/usr/bin/env bash
-debug=0
-values=4
+$ echo "um! dois, dois? três; quatro: três.TrÊs quatro - quatro QUATRO." > _tmp1
+$
 
-echo "um! dois, dois? três; quatro: três.TrÊs quatro - quatro QUATRO." > _tmp1
+# Normal
 
-# normal:
-# 3	quatro
-# 2	três
-# 2	dois
-# 1	um
-# 1	TrÊs
-# 1	QUATRO
-
-# -i:
-# 4	quatro
-# 3	três
-# 2	dois
-# 1	um
-
-tests=(
-
-# normal
-_tmp1	''	''	''	t	'3\tquatro\n2\ttrês\n2\tdois\n1\tum\n1\tTrÊs\n1\tQUATRO'
--n	3	_tmp1	''	t	'3\tquatro\n2\ttrês\n2\tdois'
+$ zzcontapalavras _tmp1
+3	quatro
+2	três
+2	dois
+1	um
+1	TrÊs
+1	QUATRO
+$ zzcontapalavras -n 3 _tmp1
+3	quatro
+2	três
+2	dois
+$
 
 # -i
--i	_tmp1	''	''	t	'4\tquatro\n3\ttrês\n2\tdois\n1\tum'
--i	-n	2	_tmp1	t	'4\tquatro\n3\ttrês'
 
-)
-. _lib
+$ zzcontapalavras -i _tmp1
+4	quatro
+3	três
+2	dois
+1	um
+$ zzcontapalavras -i -n 2 _tmp1
+4	quatro
+3	três
+$
