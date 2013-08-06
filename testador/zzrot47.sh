fb4bf05b1c3eb1d387b2c3855a455866554b9f44
Aurelio Jargas <verde@aurelio.net>
2013-08-06 09:22:05 -0300

    testador NOVO: zzrot47.sh convertida

--- testador/zzrot47.sh
+++ testador/zzrot47.sh
@@ -1,27 +1,23 @@
-#!/usr/bin/env bash
-debug=0
-values=1
-tests=(
+# Mapa completo (ida)
 
-# mapa completo (ida)
-'!'"\"#$%&'()*+,-./"		t	'PQRSTUVWXYZ[\]^'
-0123456789			t	'_`abcdefgh'
-':;<=>?@'			t	ijklmno
-ABCDEFGHIJKLMNOPQRSTUVWXYZ	t	"pqrstuvwxyz{|}~!\"#$%&'()*+"
-'[\]^_`'			t	,-./01
-abcdefghijklmnopqrstuvwxyz	t	'23456789:;<=>?@ABCDEFGHIJK'
-'{|}~'				t	LMNO
+$ zzrot47 '!"'"#$%&'()*+,-./"		#→ PQRSTUVWXYZ[\]^
+$ zzrot47 0123456789			#→ _`abcdefgh
+$ zzrot47 ':;<=>?@'			#→ ijklmno
+$ zzrot47 ABCDEFGHIJKLMNOPQRSTUVWXYZ	#→ pqrstuvwxyz{|}~!"#$%&'()*+
+$ zzrot47 '[\]^_`'			#→ ,-./01
+$ zzrot47 abcdefghijklmnopqrstuvwxyz	#→ 23456789:;<=>?@ABCDEFGHIJK
+$ zzrot47 '{|}~'			#→ LMNO
 
-# mapa completo (volta)
-'PQRSTUVWXYZ[\]^'		t	'!'"\"#$%&'()*+,-./"	
-'_`abcdefgh'			t	0123456789		
-ijklmno				t	':;<=>?@'		
-"pqrstuvwxyz{|}~!\"#$%&'()*+"	t	ABCDEFGHIJKLMNOPQRSTUVWXYZ
-,-./01				t	'[\]^_`'		
-'23456789:;<=>?@ABCDEFGHIJK'	t	abcdefghijklmnopqrstuvwxyz
-LMNO				t	'{|}~'			
+# Mapa completo (volta)
 
-# e os acentos?
-AÁaá	t	pÁ2á
-)
-. _lib
+$ zzrot47 'PQRSTUVWXYZ[\]^'		#→ !"#$%&'()*+,-./
+$ zzrot47 '_`abcdefgh'			#→ 0123456789
+$ zzrot47 ijklmno			#→ :;<=>?@
+$ zzrot47 "pqrstuvwxyz{|}~!\"#$%&'()*+"	#→ ABCDEFGHIJKLMNOPQRSTUVWXYZ
+$ zzrot47 ,-./01			#→ [\]^_`
+$ zzrot47 '23456789:;<=>?@ABCDEFGHIJK'	#→ abcdefghijklmnopqrstuvwxyz
+$ zzrot47 LMNO				#→ {|}~
+
+# Letras acentuadas não são alteradas
+
+$ zzrot47 AÁaá				#→ pÁ2á
