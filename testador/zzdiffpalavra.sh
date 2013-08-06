5a1c905a36c84808e6aaaa37663331e93c7c475d
Aurelio Jargas <verde@aurelio.net>
2013-08-06 08:06:59 -0300

    testador NOVO: zzdiffpalavra.sh convertida

--- testador/zzdiffpalavra.sh
+++ testador/zzdiffpalavra.sh
@@ -1,43 +1,57 @@
-#!/usr/bin/env bash
-debug=0
-values=2
+# Preparativos
 
-cat _dados | tr : ' ' > _tmp1
-sed 's/um/UM/;s/ three//;s/cinco/& mil/' _tmp1 > _tmp2
-sed 's/five/FIVE/' _tmp1 > _tmp3
-sed 's/ five//' _tmp1 > _tmp4
+$ cat _dados.txt
+1:um:one
+2:dois:two
+3:tres:three
+4:quatro:four
+5:cinco:five
+$ cat _dados.txt | tr : ' ' > _tmp1
+$ sed 's/um/UM/;s/ three//;s/cinco/& mil/' _tmp1 > _tmp2
+$ sed 's/five/FIVE/' _tmp1 > _tmp3
+$ sed 's/ five//' _tmp1 > _tmp4
+$
 
-saida1=" 1
+# Uso incorreto
+
+$ zzdiffpalavra			#→ --regex ^Uso:
+$ zzdiffpalavra	_tmp1		#→ --regex ^Uso:
+
+# Arquivo não existente
+
+$ zzdiffpalavra	_fake_	_tmp2_	#→ Não consegui ler o arquivo _fake_
+$ zzdiffpalavra	_tmp1	_fake_	#→ Não consegui ler o arquivo _fake_
+
+# Nenhuma diferença
+
+$ zzdiffpalavra	_tmp1	_tmp1				# no diff
+$ zzdiffpalavra	_vazio.txt	_vazio.txt		# both empty
+$
+
+# Há diferenças
+
+$ zzdiffpalavra	_tmp1	_tmp2				# termina normal
+ 1
 -um
 +UM
  one 2 dois two 3 tres
 -three
  4 quatro four 5 cinco
 +mil
- five"
-saida2="-1 um one 2 dois two 3 tres three 4 quatro four 5 cinco five"
-saida3="+1 um one 2 dois two 3 tres three 4 quatro four 5 cinco five"
-saida4=" 1 um one 2 dois two 3 tres three 4 quatro four 5 cinco
+ five
+$ zzdiffpalavra	_tmp1	_vazio.txt			# só -antigo
+-1 um one 2 dois two 3 tres three 4 quatro four 5 cinco five
+$ zzdiffpalavra	_vazio.txt	_tmp1			# só +novo
++1 um one 2 dois two 3 tres three 4 quatro four 5 cinco five
+$ zzdiffpalavra	_tmp1	_tmp3				# termina +novo
+ 1 um one 2 dois two 3 tres three 4 quatro four 5 cinco
 -five
-+FIVE"
-saida5=" 1 um one 2 dois two 3 tres three 4 quatro four 5 cinco
--five"
++FIVE
+$ zzdiffpalavra	_tmp1	_tmp4				# termina -antigo
+ 1 um one 2 dois two 3 tres three 4 quatro four 5 cinco
+-five
+$
 
-tests=(
-''	''	r	^Uso:.*
-_tmp1	''	r	^Uso:.*
-
-_fake_	_tmp2_	r	'N.o consegui ler o arquivo _fake_'
-_tmp1	_fake_	r	'N.o consegui ler o arquivo _fake_'
-
-_tmp1	_tmp1	t	''	# no diff
-_empty	_empty	t	''	# both empty
-
-_tmp1	_tmp2	t	"$saida1" # termina normal
-_tmp1	_empty	t	"$saida2" # soh -antigo
-_empty	_tmp1	t	"$saida3" # soh +novo
-
-_tmp1	_tmp3	t	"$saida4" # termina +novo
-_tmp1	_tmp4	t	"$saida5" # termina -antigo
-)
-. _lib
+# Faxina
+$ rm -f _tmp[1-4]
+$
