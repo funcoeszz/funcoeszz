eb89fd6e7d48a2a248c4a6b34191aacc9d7236ff
Aurelio Jargas <verde@aurelio.net>
2013-08-06 09:33:19 -0300

    testador NOVO: zzsextapaixao.sh convertida

--- testador/zzsextapaixao.sh
+++ testador/zzsextapaixao.sh
@@ -1,35 +1,31 @@
-#!/usr/bin/env bash
-debug=0
-values=1
-tests=(
-1995	t	14/04/1995
-1996	t	05/04/1996
-1997	t	28/03/1997
-1998	t	10/04/1998
-1999	t	02/04/1999
-2000	t	21/04/2000
-2001	t	13/04/2001
-2002	t	29/03/2002
-2003	t	18/04/2003
-2004	t	09/04/2004
-2005	t	25/03/2005
-2006	t	14/04/2006
-2007	t	06/04/2007
-2008	t	21/03/2008
-2009	t	10/04/2009
-2010	t	02/04/2010
+$ zzsextapaixao	1995	#→ 14/04/1995
+$ zzsextapaixao	1996	#→ 05/04/1996
+$ zzsextapaixao	1997	#→ 28/03/1997
+$ zzsextapaixao	1998	#→ 10/04/1998
+$ zzsextapaixao	1999	#→ 02/04/1999
+$ zzsextapaixao	2000	#→ 21/04/2000
+$ zzsextapaixao	2001	#→ 13/04/2001
+$ zzsextapaixao	2002	#→ 29/03/2002
+$ zzsextapaixao	2003	#→ 18/04/2003
+$ zzsextapaixao	2004	#→ 09/04/2004
+$ zzsextapaixao	2005	#→ 25/03/2005
+$ zzsextapaixao	2006	#→ 14/04/2006
+$ zzsextapaixao	2007	#→ 06/04/2007
+$ zzsextapaixao	2008	#→ 21/03/2008
+$ zzsextapaixao	2009	#→ 10/04/2009
+$ zzsextapaixao	2010	#→ 02/04/2010
 
 # Erros
-01/01/1970	t	"Ano inválido '01/01/1970'"
--2000 		t	"Ano inválido '-2000'"
-0 		t	"Ano inválido '0'"
-foo 		t	"Ano inválido 'foo'"
+
+$ zzsextapaixao	01/01/1970	#→ Ano inválido '01/01/1970'
+$ zzsextapaixao	-2000 		#→ Ano inválido '-2000'
+$ zzsextapaixao	0 		#→ Ano inválido '0'
+$ zzsextapaixao	foo 		#→ Ano inválido 'foo'
 
 # Epoch
-1	t	25/03/1
-10 	t	18/04/10
-100 	t	10/04/100
-1000	t	29/03/1000
-1969 	t	04/04/1969
-)
-. _lib
+
+$ zzsextapaixao	1	#→ 25/03/1
+$ zzsextapaixao	10 	#→ 18/04/10
+$ zzsextapaixao	100 	#→ 10/04/100
+$ zzsextapaixao	1000	#→ 29/03/1000
+$ zzsextapaixao	1969 	#→ 04/04/1969
