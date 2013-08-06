6c97d3bc22db60017289d71a1c3e57e91119bd44
Aurelio Jargas <verde@aurelio.net>
2013-08-06 07:40:37 -0300

    testador NOVO: zzbissexto.sh convertida

--- testador/zzbissexto.sh
+++ testador/zzbissexto.sh
@@ -1,36 +1,30 @@
-#!/usr/bin/env bash
-debug=0
-values=1
-tests=(
-2000	t	"2000 é bissexto"
-2001	t	"2001 não é bissexto"
-2002	t	"2002 não é bissexto"
-2003	t	"2003 não é bissexto"
-2004	t	"2004 é bissexto"
-2005	t	"2005 não é bissexto"
-2006	t	"2006 não é bissexto"
-2007	t	"2007 não é bissexto"
-2008	t	"2008 é bissexto"
-2009	t	"2009 não é bissexto"
-2010	t	"2010 não é bissexto"
-2011	t	"2011 não é bissexto"
-2012	t	"2012 é bissexto"
+$ zzbissexto 2000		#→ 2000 é bissexto
+$ zzbissexto 2001		#→ 2001 não é bissexto
+$ zzbissexto 2002		#→ 2002 não é bissexto
+$ zzbissexto 2003		#→ 2003 não é bissexto
+$ zzbissexto 2004		#→ 2004 é bissexto
+$ zzbissexto 2005		#→ 2005 não é bissexto
+$ zzbissexto 2006		#→ 2006 não é bissexto
+$ zzbissexto 2007		#→ 2007 não é bissexto
+$ zzbissexto 2008		#→ 2008 é bissexto
+$ zzbissexto 2009		#→ 2009 não é bissexto
+$ zzbissexto 2010		#→ 2010 não é bissexto
+$ zzbissexto 2011		#→ 2011 não é bissexto
+$ zzbissexto 2012		#→ 2012 é bissexto
 
 # Epoch
-1968	t	"1968 é bissexto"
-1969	t	"1969 não é bissexto"
-1970	t	"1970 não é bissexto"
-1971	t	"1971 não é bissexto"
-1972	t	"1972 é bissexto"
+$ zzbissexto 1968		#→ 1968 é bissexto
+$ zzbissexto 1969		#→ 1969 não é bissexto
+$ zzbissexto 1970		#→ 1970 não é bissexto
+$ zzbissexto 1971		#→ 1971 não é bissexto
+$ zzbissexto 1972		#→ 1972 é bissexto
 
 # Divisível por 100 e não divisível por 4 não é
-1900	t	"1900 não é bissexto"
+$ zzbissexto 1900		#→ 1900 não é bissexto
 
 # Erros
-01/01/1970	t	"Ano inválido '01/01/1970'"
--2000 		t	"Ano inválido '-2000'"
-0 		t	"Ano inválido '0'"
-foo 		t	"Ano inválido 'foo'"
+$ zzbissexto 01/01/1970		#→ Ano inválido '01/01/1970'
+$ zzbissexto -2000 		#→ Ano inválido '-2000'
+$ zzbissexto 0 			#→ Ano inválido '0'
+$ zzbissexto foo 		#→ Ano inválido 'foo'
 
-)
-. _lib
