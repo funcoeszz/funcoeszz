eafeb733ee8612469cf02fa81a1bd90f13382b54
Aurelio Jargas <verde@aurelio.net>
2013-08-06 09:34:11 -0300

    testador NOVO: zzstr2hexa.sh convertida

--- testador/zzstr2hexa.sh
+++ testador/zzstr2hexa.sh
@@ -1,19 +1,11 @@
-#!/usr/bin/env bash
-debug=0
-values=2
-protected=1
-
-n="$(printf '.\n.')"
-t="$(printf '.\t.')"
-
-tests=(
-
-binario	''	t	"62 69 6e 61 72 69 6f"
-com	espaco	t	"63 6f 6d 20 65 73 70 61 63 6f"
-'. .'	''	t	"2e 20 2e"
-'\t'	''	t	"5c 74"
-'\n'	''	t	"5c 6e"
-"$t"	''	t	"2e 09 2e"
-"$n"	''	t	"2e 0a 2e"
-)
-. _lib
+$ n="$(printf '.\n.')"
+$ t="$(printf '.\t.')"
+$ zzstr2hexa	binario		#→ 62 69 6e 61 72 69 6f
+$ zzstr2hexa	com	espaco	#→ 63 6f 6d 20 65 73 70 61 63 6f
+$ zzstr2hexa	'. .'		#→ 2e 20 2e
+$ zzstr2hexa	'\t'		#→ 5c 74
+$ zzstr2hexa	'\n'		#→ 5c 6e
+$ zzstr2hexa	"$t"		#→ 2e 09 2e
+$ zzstr2hexa	"$n"		#→ 2e 0a 2e
+$ n=
+$ t=
