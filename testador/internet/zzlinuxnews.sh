75ea9605abf37f0814f5a44c3f6bdbd69c762536
Aurelio Jargas <verde@aurelio.net>
2013-08-08 18:19:46 -0300

    testador: zzlinuxnews melhorado

--- testador/internet/zzlinuxnews.sh
+++ testador/internet/zzlinuxnews.sh
@@ -1,5 +1,7 @@
-$ zzlinuxnews f  #→ --lines 7
-$ zzlinuxnews t  #→ --lines 7
-$ zzlinuxnews w  #→ --lines 7
-$ zzlinuxnews o  #→ --lines 7
-$ zzlinuxnews s  #→ --lines 7
+$ zzlinuxnews f  # FreshMeat           #→ --lines 7
+$ zzlinuxnews t  # Linux Today         #→ --lines 7
+$ zzlinuxnews w  # Linux Weekly News   #→ --lines 7
+$ zzlinuxnews o  # OS News             #→ --lines 7
+$ zzlinuxnews s  # SlashDot            #→ --lines 7
+$ zzlinuxnews foo
+$
