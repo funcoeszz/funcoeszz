33abe1a1b64935f67c51b4c0e1a0ad5b289271a8
Aurelio Jargas <verde@aurelio.net>
2013-08-08 18:26:32 -0300

    testador NOVO: zznoticiassec.sh criada

---
+++ testador/internet/zznoticiassec.sh
@@ -0,0 +1,7 @@
+$ zznoticiassec c  # CERT/CC                  #→ --lines 7
+$ zznoticiassec s  # Linux Security           #→ --lines 7
+$ zznoticiassec b  # Linux Security Brasil    #→ --lines 7
+$ zznoticiassec t  # Linux Today - Security   #→ --lines 7
+$ zznoticiassec f  # Security Focus           #→ --lines 7
+$ zznoticiassec z
+$
