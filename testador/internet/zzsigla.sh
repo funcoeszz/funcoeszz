42ea818f0b07179d73f868e4138dd68814f29aec
Aurelio Jargas <verde@aurelio.net>
2013-08-08 19:15:17 -0300

    testador NOVO: zzsigla.sh criada

--- testador/internet/zzsigla.sh
+++ testador/internet/zzsigla.sh
@@ -1,3 +1,4 @@
+$ zzsigla DVD
 DVD   Digital Video Disc (now Digital Versatile Disc)
 DVD   Digital Versatile Disc (formerly Digital Video Disc)
 DVD   Dick Van Dyke (actor)
@@ -7,3 +8,6 @@ DVD   Divers Droite (France, politics)
 DVD   Deutsche Vereinigung für Datenschutz
 DVD   Direct Vendor Delivery
 DVD   Developmental Verbal Dyspraxia
+$ zzsigla
+Uso: zzsigla sigla
+$
