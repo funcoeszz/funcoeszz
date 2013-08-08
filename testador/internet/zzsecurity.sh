9241a187fdc138abb06ae9fd0879b9256bf89f46
Aurelio Jargas <verde@aurelio.net>
2013-08-08 19:14:00 -0300

    testador NOVO: zzsecurity.sh criada

---
+++ testador/internet/zzsecurity.sh
@@ -0,0 +1,9 @@
+$ zzsecurity debian      #→ --lines 8
+$ zzsecurity freebsd     #→ --lines 8
+$ zzsecurity gentoo      #→ --lines 8
+$ zzsecurity mandriva    #→ --lines 8
+$ zzsecurity slackware   #→ --lines 8
+$ zzsecurity suse        #→ --lines 8
+$ zzsecurity ubuntu      #→ --lines 8
+$ zzsecurity foo
+$
