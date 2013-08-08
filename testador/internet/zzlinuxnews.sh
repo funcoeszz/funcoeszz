77c49b2b1274241e135cf347c1fe3934eaaabfa7
Aurelio Jargas <verde@aurelio.net>
2013-08-08 18:23:34 -0300

    testador: typo

--- testador/internet/zzlinuxnews.sh
+++ testador/internet/zzlinuxnews.sh
@@ -3,5 +3,5 @@ $ zzlinuxnews t  # Linux Today         #→ --lines 7
 $ zzlinuxnews w  # Linux Weekly News   #→ --lines 7
 $ zzlinuxnews o  # OS News             #→ --lines 7
 $ zzlinuxnews s  # SlashDot            #→ --lines 7
-$ zzlinuxnews foo
+$ zzlinuxnews z
 $
--- testador/internet/zznoticiaslinux.sh
+++ testador/internet/zznoticiaslinux.sh
@@ -3,5 +3,5 @@ $ zznoticiaslinux n  # Notícias Linux  #→ --lines 7
 $ zznoticiaslinux u  # Under Linux     #→ --lines 7
 $ zznoticiaslinux v  # Viva o Linux    #→ --lines 7
 $ zznoticiaslinux y  # Yahoo Linux     #→ --lines 7
-$ zznoticiaslinux foo
+$ zznoticiaslinux z
 $
