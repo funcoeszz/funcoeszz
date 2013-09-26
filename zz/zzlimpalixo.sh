5af99ec07a576d0f03e157f650b131b9141e6c2d
itamarnet <itamarnet@yahoo.com.br>
2013-09-25 21:40:57 -0300

    zzlimpalixo: Ampliando o leque de arquivos suportados.

--- zz/zzlimpalixo.sh
+++ zz/zzlimpalixo.sh
@@ -1,32 +1,61 @@
 # ----------------------------------------------------------------------------
 # Retira linhas em branco e comentários.
 # Para ver rapidamente quais opções estão ativas num arquivo de configuração.
-# Além do tradicional #, reconhece comentários de arquivos .vim.
+# Além do tradicional #, reconhece comentários de vários tipos de arquivos.
+#  vim, asp, asm, ada, sql, e, bat, tex, c, css, html, cc, d, js, php. scala.
+# E inclui os comentários multilinhas (/* ... */), usando opção --multi.
 # Obs.: Aceita dados vindos da entrada padrão (STDIN).
-# Uso: zzlimpalixo [arquivos]
+# Uso: zzlimpalixo [--multi] [arquivos]
 # Ex.: zzlimpalixo ~/.vimrc
 #      cat /etc/inittab | zzlimpalixo
 #
 # Autor: Aurelio Marinho Jargas, www.aurelio.net
 # Desde: 2000-04-24
-# Versão: 2
+# Versão: 3
 # Licença: GPL
+# Requisitos: zzjuntalinhas
 # ----------------------------------------------------------------------------
 zzlimpalixo ()
 {
 	zzzz -h limpalixo "$1" && return
 
 	local comentario='#'
+	local multi=0
+	local comentario_ini='\/\*'
+	local comentario_fim='\*\/'
+
+	# Para comentários multilinhas: /* ... */
+	if test "$1" = "--multi"
+	then
+		multi=1
+		shift
+	fi
 
 	# Reconhecimento de comentários do Vim
 	case "$1" in
-		*.vim | *.vimrc*)
-			comentario='"'
-		;;
+		*.vim | *.vimrc*)			comentario='"';;
+		*.asp)					comentario="'";;
+		*.asm)					comentario=';';;
+		*.ada | *.sql | *.e)			comentario='--';;
+		*.bat)					comentario='rem ';;
+		*.tex)					comentario='%';;
+		*.c | *.css)				multi=1;;
+		*.html | *.htm)				comentario_ini='<!--'; comentario_fim='-->'; multi=1;;
+		*.cc | *.d | *.js | *.php | *.scala)	comentario='\/\/';;
 	esac
 
 	# Arquivos via STDIN ou argumentos
 	zztool file_stdin "$@" |
+
+	# Junta os comentários multilinhas
+	if test $multi -eq 1
+	then
+		zzjuntalinhas -i "$comentario_ini" -f "$comentario_fim" |
+		sed "/^[[:blank:]]*${comentario_ini}/d"
+
+	else
+		cat -
+	fi |
 
 		# Remove comentários e linhas em branco
 		sed "
