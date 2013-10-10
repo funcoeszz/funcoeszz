8f11902c5a6e16f0b16b543402c80fa426bf1433
itamarnet <itamarnet@yahoo.com.br>
2013-10-10 12:10:16 -0300

    REATIVADA: zzcep. Alterado a url para consulta dos CEPs.

--- off/zzcep.sh
+++ off/zzcep.sh
@@ -1,37 +1,51 @@
 # ----------------------------------------------------------------------------
-# http://www.correios.com.br/servicos/cep
+# http://www.achecep.com.br
 # Busca o CEP de qualquer rua de qualquer cidade do país ou vice-versa.
-# Uso: zzcep estado cidade rua
-# Ex.: zzcep PR curitiba rio gran
-#      zzcep RJ 'Rio de Janeiro' Vinte de
+# Pode-se fornecer apenas o CEP, ou o estado com endereço.
+# Uso: zzcep <estado endereço | CEP>
+# Ex.: zzcep SP Rua Santa Ifigênia
+#      zzcep 01310-000
 #
 # Autor: Aurélio Marinho Jargas, www.aurelio.net
 # Desde: 2000-11-08
-# Versão: 1
+# Versão: 2
 # Licença: GPL
 # ----------------------------------------------------------------------------
-# DESATIVADA: 2008-03-01 Agora o site dos Correios usa AJAX :(
 zzcep ()
 {
 	zzzz -h cep $1 && return
 
-	local r c
-	local url='http://www.correios.com.br/servicos/cep'
-	local e="$1"
+	local r e query
+	local url='http://www.achecep.com.br'
 
 	# Verificação dos parâmetros
-	[ "$3" ] || { zztool uso cep; return; }
+	[ "$1" ] || { zztool uso cep; return; }
 
-	c=$(echo "$2"| sed "$ZZSEDURL")
-	shift
-	shift
-	r=$(echo "$*"| sed "$ZZSEDURL")
-	echo "UF=$e&Localidade=$c&Tipo=&Logradouro=$r" |
-		$ZZWWWPOST "$url" |
-		sed -n '
-			/^ *UF:/,/^$/ {
-				/Página Anter/d
-				s/.*óxima Pág.*/...CONTINUA/
-				p
-			}'
+	# Testando se parametro é o CEP
+	echo "$1" | tr -d '-' | grep -E '[0-9]{8}' > /dev/null
+	if test $? -eq 0
+	then
+		query=$(echo "q=$1" | tr -d '-')
+	fi
+
+	# Conferindo se é sigla do Estado e endereço
+	if test -z $query && test -n "$2"
+	then
+		echo $1 | grep -E '[a-zA-Z]{2}' > /dev/null
+		if test $? -eq 0
+		then
+			e="$1"
+			shift
+			r=$(echo "$*"| sed "$ZZSEDURL")
+			query="uf=${e}&q=$r"
+		else
+			zztool uso cep; return;
+		fi
+	fi
+
+	# Testando se formou a query string
+	test -n "$query" || { zztool uso cep; return; }
+
+	echo "$query" | $ZZWWWPOST "$url" | 
+	sed -n '/^[[:blank:]]*CEP/,/^[[:blank:]]*$/p'| sed 's/^ *//g;$d'
 }
