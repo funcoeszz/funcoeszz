3cdfca7ecf518a38f61fc12315e9cd9ecc0e9be2
Marcell S. Martini <marcellmartini@gmail.com>
2013-10-30 12:50:25 -0400

    zzdado: por compatiblidade foi retirado o 0. Também foi adicionada a opção -f ou --faces, que serve para setar uma quantidade de faces diferente de 6.

--- zz/zzdado.sh
+++ zz/zzdado.sh
@@ -1,20 +1,46 @@
 # ----------------------------------------------------------------------------
-# Dado virtual, exibe um número aleatório entre 1 e 6.
+# Dado virtual, caso não seja passado nenhum argumento, exibe um número 
+# aleatório entre 1 e 6. É possível "rolar" dados com outras quantidades de 
+# faces com o argumento -f ou --faces.
+# 
 # Uso: zzdado
 # Ex.: zzdado
+#      zzdado -f 20
+#      zzdado --faces 12
 #
 # Autor: Angelito M. Goulart, www.angelitomg.com
 # Desde: 2012-12-05
-# Versão: 1
+# Versão: 2
 # Licença: GPL
 # ----------------------------------------------------------------------------
 zzdado ()
 {
 
+	local n_faces=6
+
 	# Comando especial das funcoes ZZ
 	zzzz -h dado "$1" && return
 
-	# Gera e exibe um numero aleatorio entre 1 e 6
-	echo "$((($RANDOM % 6) + 1))"
+	while [ "${1#-}" != "$1" ]
+	do
+		case "$1" in
+			-f|--faces)
+				if zztool testa_numero $2; 
+				then
+					n_faces="$2"
+				else
+					echo "Numero inválido"
+					return 1
+				fi
+			;;
+			*)
+				echo "Opção inválida"
+				return 2
+			;;
+		esac
+		shift
+	done
 
+	# Gera e exibe um numero aleatorio entre 1 e 6
+	expr $(echo $RANDOM) % $n_faces + 1
 }
