096020d366be2a2cf2182ca786389120077c116a
itamarnet <itamarnet@yahoo.com.br>
2014-03-20 20:41:18 -0300

    zzgeoip: Nanny, alinhamento e otimização do código.

--- zz/zzgeoip.sh
+++ zz/zzgeoip.sh
@@ -6,66 +6,65 @@
 #
 # Autor: Alexandre Magno <alexandre.mbm (a) gmail com>
 # Desde: 2013-07-06
-# Versão: 1
+# Versão: 2
 # Licença: GPLv2
 # Requisitos: zzxml zzipinternet zzecho zzminiurl
 # ----------------------------------------------------------------------------
 zzgeoip ()
 {
 	zzzz -h geoip "$1" && return
-	
-	local ip pagina src latitude longintude cidade uf pais mapa
+
+	local ip pagina latitude longintude cidade uf pais mapa
 	local url='http://geoip.s12.com.br'
 
 	if [ $# -ge 2 ]
 	then
 		zztool uso geoip
 		return 1
-	elif [ $1 ]
+	elif [ "$1" ]
 	then
-		zztool -e testa_ip $1
+		zztool -e testa_ip "$1"
 		[ $? -ne 0 ] && zztool uso geoip && return 1
-		ip=$1
-    else
-        ip=$(zzipinternet)
-    fi	
+		ip="$1"
+	else
+		ip=$(zzipinternet)
+	fi
 
-    pagina=$($ZZWWWHTML "$url?ip=$ip")
+	pagina=$($ZZWWWHTML http://geoip.s12.com.br?ip=$ip |
+				zzxml --tidy --untag --tag td |
+				sed '/^[[:blank:]]*$/d;/&/d' |
+				awk '{if ($0 ~ /:/) { printf "\n%s",$0 } else printf $0}'
+			)
 
-	pais=$(echo -e "$pagina" |
-			zzxml --tag 'tr' |
+	cidade=$(echo "$pagina" |
+			grep 'Cidade:' |
+			cut -f2 -d:)
+
+	uf=$(echo "$pagina" |
+			grep 'Estado:' |
+			cut -f2 -d:)
+
+	pais=$(echo "$pagina" |
 			grep 'País:' |
-			zzxml --tag 'strong' --untag)
+			cut -f2 -d:)
 
-	src=$(echo -e "$pagina" |
-			zzxml --tag 'td' |
-			grep 'tx_verde_14' |
-			sed -e 's/^.* src=\"//' -e 's/\" width=.*$//' |
-			sed 's/\&amp\;/\&/g')
+	latitude=$(echo "$pagina" |
+			grep 'Latitude:' |
+			cut -f2 -d: |
+			tr ',' '.')
 
-	mapa=$(zzminiurl "$url/$src")
+	longitude=$(echo "$pagina" |
+			grep 'Longitude:' |
+			cut -f2 -d: |
+			tr ',' '.')
 
-    src=$(echo "$src" | sed -e 's/^.*lat=//g' -e 's/\&[a-z]*=/|/g')
+	mapa=$(zzminiurl "$url/mapa.asp?lat=$latitude&lon=$longitude")
 
-    # 50,9832992553711|7,13329982757568|Bergisch Gladbach|DF
-
-	latitude=$(echo "$src" | cut -d'|' -f1)
-	longitude=$(echo "$src" | cut -d'|' -f2)
-	cidade=$(echo "$src" | cut -d'|' -f3)
-	uf=$(echo "$src" | cut -d'|' -f4)
-
-	latitude=${latitude/,/.}
-	longitude=${longitude/,/.}
-
-	# mapa="http://maps.google.com/maps?q=$latitude,$longitude" (bug)
-
-	echo
-	zzecho -n '        IP: '; zzecho -l verde -N "$ip"
-	zzecho -n '    Cidade: '; zzecho -N "$cidade"
-	zzecho -n '    Estado: '; zzecho -N "$uf"
-	zzecho -n '      País: '; zzecho -N "$pais"
-	zzecho -n '  Latitude: '; zzecho -l amarelo -e "\\$latitude"
-	zzecho -n ' Longitude: '; zzecho -l amarelo -e "\\$longitude"
-	zzecho -n '      Mapa: '; zzecho -l azul "$mapa"
-	echo
+	zzecho -n '       IP: '; zzecho -l verde -N "${ip:- }"
+	zzecho -n '   Cidade: '; zzecho -N "${cidade:- }"
+	zzecho -n '   Estado: '; zzecho -N "${uf:- }"
+	zzecho -n '     País: '; zzecho -N "${pais:- }"
+	zzecho -n ' Latitude: '; zzecho -l amarelo "${latitude:- }"
+	zzecho -n 'Longitude: '; zzecho -l amarelo "${longitude:- }"
+	zzecho -n '     Mapa: '; zzecho -l azul "${mapa:- }"
 }
