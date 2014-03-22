c858287c60fec7e737642810b97697ca9dd322bd
itamarnet <itamarnet@yahoo.com.br>
2014-03-22 09:56:02 -0300

    zztabuada: Ampliando o range opcionalmente para a tabuada.

--- zz/zztabuada.sh
+++ zz/zztabuada.sh
@@ -1,29 +1,44 @@
 # ----------------------------------------------------------------------------
-# Imprime a tabuada de qualquer número inteiro positivo ou negativo de 1 a 10.
+# Exibe a tabela de tabuada de um número.
+# Com 1 argumento:
+#  Tabuada de qualquer número inteiro de 1 a 10.
+#
+# Com 2 argumentos:
+#  Tabuada de qualquer número inteiro de 1 ao segundo argumento.
+#  O segundo argumento só pode ser um número positivo de 1 até 99, inclusive.
+#
 # Se não for informado nenhum argumento será impressa a tabuada de 1 a 9.
 #
-# Uso: zztabuada [número]
+# Uso: zztabuada [número [número]]
 # Ex.: zztabuada
 #      zztabuada 2
 #      zztabuada -176
+#      zztabuada 5 15  # Tabuada do 5, mas multiplicado de 1 até o 15.
 #
 # Autor: Kl0nEz <kl0nez (a) wifi org br>
 # Desde: 2011-08-23
-# Versão: 5
+# Versão: 6
 # Licença: GPLv2
+# Requisitos: zzseq
 # ----------------------------------------------------------------------------
 zztabuada ()
 {
 	zzzz -h tabuada "$1" && return
 
 	local i j
+	local numeros='0 1 2 3 4 5 6 7 8 9 10'
 	local linha="+--------------+--------------+--------------+"
 
 	case "$#" in
-		1)
+		1 | 2)
 			if zztool testa_numero_sinal "$1"
 			then
-				for i in 0 1 2 3 4 5 6 7 8 9 10
+				if zztool testa_numero "$2" && test $2 -le 99
+				then
+					numeros=$(zzseq -f '%d ' $2)
+				fi
+
+				for i in $numeros
 				do
 					if test $i -eq 0 && ! zztool testa_numero "$1"
 					then
@@ -52,6 +67,10 @@ zztabuada ()
 				echo "$linha"
 				echo
 			done | sed '$d'
+		;;
+		*)
+			zztool uso tabuada
+			return 1
 		;;
 	esac
 }
