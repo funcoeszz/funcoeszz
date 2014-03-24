98c4d481af6f2c01cedd70cdfc46c4b80e4f7cb6
Tárcio Zemel <tarciozemel@gmail.com>
2014-03-24 14:21:02 -0300

    Adiciona função zzcriptomoeda
    Em #133 estava sendo discutida a ideia de se criar algumas funções
    referentes a criptomoedas e, conforme a sugestão e dicas do colega
    @itamarnet, esta é a primeira versão da coisa.
    
    Depois de breve análise, chegou-se à conclusão de que seria mais
    conveniente uma só função para tratar de várias criptomoedas por
    facilitar expansões/incrementos futuros (embora, no momento, estejam
    contempladas somente Bitcoin e Litecoin).

---
+++ zz/zzcriptomoeda.sh
@@ -0,0 +1,45 @@
+# ----------------------------------------------------------------------------
+# Retorna a cotação de criptomoedas em Reais.
+# Uso: zzcriptomoeda [btc|bitcoin|ltc|litecoin]
+# Ex.: zzcriptomoeda btc
+#      zzcriptomoeda litecoin
+#
+# Autor: Tárcio Zemel <tarciozemel (a) gmail com>
+# Versão: 1
+# Licença: GPL
+# Requisitos: zzminusculas zzsemacento
+# ----------------------------------------------------------------------------
+zzcriptomoeda ()
+{
+    zzzz -h criptomoeda "$1" && return
+
+    # Variáveis gerais
+    local moedas_validas=(btc bitcoin ltc litecoin)
+    local moeda_informada=$(echo "$1" | zzminusculas | zzsemacento)
+
+    # Se passou moeda válida, atribui valor da query string;
+    # senão, termina.
+    if [[ ${moedas_validas[*]} =~ "$moeda_informada" ]]
+    then
+        local query_string=''
+
+        case "$moeda_informada" in
+            btc|bitcoin  ) query_string='BTC';;
+            ltc|litecoin ) query_string='LTC';;
+        esac
+    else
+        return
+    fi
+
+    # Monta URL a ser consultada
+    local url="https://www.bitinvest.com.br/?SDCC=$query_string"
+
+    # Retorno
+    $ZZWWWDUMP "$url" |
+    sed -n '5{s/^ *//;s/Último //;s/: R\$/: R$ /g ;s/ [BL]TC//;p;}' |
+    sed 's/[0-9] /&\
+    /g' |
+    sed -n 1p |
+    sed -e 's/Preço: //g' |
+    sed 's/ //g'
+}
