5e2e49deb9be0ee56ec958a7ff0e6228364fbb52
itamarnet <itamarnet@yahoo.com.br>
2014-04-06 15:04:16 -0300

    zzloteria2: Ajustes na faixa de premiação na loteca nos concursos abaixo de 89.

--- zz/zzloteria2.sh
+++ zz/zzloteria2.sh
@@ -205,6 +205,10 @@ zzloteria2 ()
 					}
 					{ print }
 				')
+				case $(echo "$num_con" | sed 's/.*=//;s/ *//g') in
+					[1-9] | [1-8][0-9]) faixa=$(zzseq -f '\t%d\n' 14 12);;
+					*) faixa=$(zzseq -f '\t%d\n' 14 13);;
+				esac
 				faixa=$(zzseq -f '\t%d\n' 14 13)
 				printf '%b\n' "$faixa" > "${cache}"
 				echo "$dump" | cut -d '|' -f 5 | sed 's/ [12].\{1,2\} (1[34] acertos)/\
