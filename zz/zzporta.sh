80420471919dec451cdeae23a6fb2d990b0dc745
itamarnet <itamarnet@yahoo.com.br>
2014-11-15 17:11:38 -0200

    Função nova: zzporta - Lista das portas de protocolos da internet.

---
+++ zz/zzporta.sh
@@ -0,0 +1,37 @@
+# ----------------------------------------------------------------------------
+# Mostra uma lista das portas de protocolos usados na internet.
+# Se houver um número como argumento, a listagem é filtrada pelo mesmo.
+#
+# Uso: zzporta [porta]
+# Ex.: zzporta
+#      zzporta 513
+#
+# Autor: Itamar <itamarnet (a) yahoo com br>
+# Desde: 2014-11-15
+# Versão: 1
+# Licença: GPL
+# Requisitos: zzjuntalinhas
+# ----------------------------------------------------------------------------
+zzporta ()
+{
+	zzzz -h porta "$1" && return
+
+	local url="http://pt.wikipedia.org/wiki/Anexo:Lista_de_portas_de_protocolos"
+	local port=$1
+	zztool testa_numero $port || port='.'
+
+	$ZZWWWHTML "$url" |
+	awk '/"wikitable"/,/<\/table>/ { sub (/ bgcolor.*>/,">"); print }' |
+	zzjuntalinhas -d '' -i '<tr>' -f '</tr>' |
+	awk -F '</?t[^>]+>' 'BEGIN {OFS="\t"}{ print $3, $5 }' |
+	expand -t 18 |
+	sed '
+		1d
+		s/<[^>]*>//g # Retira os links
+		3,${
+			/^Porta/d
+			/^[[:blank:]]*$/d
+			/\/IP /d
+		}' |
+	awk 'NR==1;NR>1 && /'$port'/'
+}
