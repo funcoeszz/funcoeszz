79be9e31378162a8e9ebb4f97f0c5e663589919b
Aurelio Jargas <verde@aurelio.net>
2014-12-18 06:39:01 -0200

    zzchavepgp: testes atualizados

--- testador/zzchavepgp.sh
+++ testador/zzchavepgp.sh
@@ -1,7 +1,7 @@
 $ zzchavepgp
 Uso: zzchavepgp nome|e-mail
 $ zzchavepgp XXNOTFOUNDXX
-   Error handling request: No keys found
+   No results found: No keys found
 $ zzchavepgp Guido van Rossum
 Type bits/keyID     Date       User ID
 pub  1024D/6F023F0B 2002-10-16 Guido van Rossum <guido@python.org>
