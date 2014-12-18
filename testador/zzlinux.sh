5723a7f64b4d4a47dbf3a0546a968d3e8952f6a6
Aurelio Jargas <verde@aurelio.net>
2014-12-18 19:51:33 -0200

    zzlinux: testes atualizados

--- testador/zzlinux.sh
+++ testador/zzlinux.sh
@@ -1,11 +1,12 @@
 $ zzlinux | sed 's/\(is: *\)[^ ].*/\1VERSION/'
 The latest mainline 3 version of the Linux kernel is:         VERSION
-The latest stable 3.10 version of the Linux kernel is:        VERSION
-The latest stable 3.9 version of the Linux kernel is:         VERSION
+The latest stable 3.18 version of the Linux kernel is:        VERSION
+The latest stable 3.17 version of the Linux kernel is:        VERSION
+The latest longterm 3.14 version of the Linux kernel is:      VERSION
+The latest longterm 3.12 version of the Linux kernel is:      VERSION
+The latest longterm 3.10 version of the Linux kernel is:      VERSION
 The latest longterm 3.4 version of the Linux kernel is:       VERSION
 The latest longterm 3.2 version of the Linux kernel is:       VERSION
-The latest longterm 3.0 version of the Linux kernel is:       VERSION
-The latest longterm 2.6.34 version of the Linux kernel is:    VERSION
 The latest longterm 2.6.32 version of the Linux kernel is:    VERSION
 The latest linux-next version of the Linux kernel is:         VERSION
 $
