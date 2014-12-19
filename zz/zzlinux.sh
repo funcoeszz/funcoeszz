508c393214520451b8ab1478fb341f324d869325
Aurelio Jargas <verde@aurelio.net>
2014-12-18 22:10:11 -0200

    zzlinux: removida linha em branco do resultado

--- zz/zzlinux.sh
+++ zz/zzlinux.sh
@@ -6,12 +6,12 @@
 #
 # Autor: Diogo Gullit <guuuuuuuuuullit (a) yahoo com br>
 # Desde: 2008-05-01
-# Versão: 1
+# Versão: 2
 # Licença: GPL
 # ----------------------------------------------------------------------------
 zzlinux ()
 {
 	zzzz -h linux "$1" && return
 
-	$ZZWWWDUMP http://www.kernel.org/kdist/finger_banner
+	$ZZWWWDUMP http://www.kernel.org/kdist/finger_banner | grep -v '^$'
 }
