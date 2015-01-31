ff5e6d71c8d0fe982652d726c98a6624c1f415ee
Paulo Vital <pvital@gmail.com>
2015-01-31 23:16:50 +0100

    zzsenha: adição de testes para opção -u/--uniq
    Signed-off-by: Paulo Vital <pvital@gmail.com>

--- testador/zzsenha.sh
+++ testador/zzsenha.sh
@@ -10,6 +10,10 @@ $ zzsenha	--num	-8	#→ Número inválido '-8'
 $ zzsenha	--pro	FOO	#→ Número inválido 'FOO'
 $ zzsenha	--pro	-8	#→ Número inválido '-8'
 
+$ zzsenha	--uniq	FOO	#→ Número inválido 'FOO'
+$ zzsenha	--uniq	-8	#→ Número inválido '-8'
+$ zzsenha	--uniq	63	#→ O tamanho máximo desse tipo de senha é 62
+
 # Normal
 
 $ zzsenha		0
@@ -32,3 +36,11 @@ $ zzsenha	--pro	1	#→ --regex ^[A-Za-z0-9/:;()$&@.,?!-]$
 $ zzsenha	--pro	10	#→ --regex ^[A-Za-z0-9/:;()$&@.,?!-]{10}$
 $ zzsenha	--pro	75	#→ --regex ^[A-Za-z0-9/:;()$&@.,?!-]{75}$
 $ zzsenha	--pro		#→ --regex ^[A-Za-z0-9/:;()$&@.,?!-]{8}$
+
+# --uniq
+
+$ zzsenha	--uniq	0
+$ zzsenha	--uniq	1	#→ --regex ^[A-Za-z0-9]$
+$ zzsenha	--uniq	10	#→ --regex ^[A-Za-z0-9]{10}$
+$ zzsenha	--uniq	62	#→ --regex ^[A-Za-z0-9]{62}$
+$ zzsenha	--uniq		#→ --regex ^[A-Za-z0-9]{8}$
