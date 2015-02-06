a4e0b67959e6488c442d3800ac908c834a2c35fe
Paulo Vital <pvital@gmail.com>
2015-02-07 00:27:25 +0100

    zzsigla: Incremento de resultados para test case.
    Signed-off-by: Paulo Vital <pvital@gmail.com>

--- testador/zzsigla.sh
+++ testador/zzsigla.sh
@@ -8,6 +8,19 @@ DVD   Divers Droite (France, politics)
 DVD   Deutsche Vereinigung für Datenschutz
 DVD   Direct Vendor Delivery
 DVD   Developmental Verbal Dyspraxia
+DVD   Death Valley Driver (pro wrestling)
+DVD   D-Von Dudley (wrestler)
+DVD   Dissociated Vertical Deviation (ophthalmology)
+DVD   Digital Video Drive
+DVD   Dynamic Voltage Drop (electronics)
+DvD   Depp Vom Dienst (German: Idiot of Service)
+DVD   Double-Vessel Disease
+DVD   Digital Virtual Disk
+DVD   Drivers Vigilance Device (UK)
+DVD   Driver and Vehicle Data Online Access
+DVD   Diploma in Venerology & Dermatology (India)
+DVD   DC Video Deluxe (skateboarding)
+DVD   Definitie Van Dopeheid (Dutch: Definition of Permissible Dope)
 $ zzsigla
 Uso: zzsigla sigla
 $
