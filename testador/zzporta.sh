42da3d17a6600f62142ac81ea58bd01b375af53e
Aurelio Jargas <verde@aurelio.net>
2015-02-12 10:47:42 -0200

    zzporta: adicionados testes

---
+++ testador/zzporta.sh
@@ -0,0 +1,8 @@
+$ zzporta 29920
+Porta             Descrição
+29920             Nintendo Wi-Fi Connection
+$ zzporta 513
+Porta             Descrição
+513/TCP           Login
+513/UDP           Who
+513/UDP           Router
