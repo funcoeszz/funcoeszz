1d8d104a103ec810e2ac8930050a8f1684091e65
itamarnet <itamarnet@yahoo.com.br>
2015-02-16 21:25:24 -0200

    zzascii: Usando zzcolunar como requisito, e pequenas alterações de código.

--- zz/zzascii.sh
+++ zz/zzascii.sh
@@ -9,15 +9,15 @@
 #
 # Autor: Aurelio Marinho Jargas, www.aurelio.net
 # Desde: 2002-12-06
-# Versão: 2
+# Versão: 3
 # Licença: GPL
-# Requisitos: zzseq
+# Requisitos: zzseq zzcolunar
 # ----------------------------------------------------------------------------
 zzascii ()
 {
 	zzzz -h ascii "$1" && return
 
-	local referencias decimais decimal hexa octal caractere largura_col
+	local decimais decimal hexa octal caractere largura_col
 	local num_colunas="${1:-5}"
 	local largura="${2:-78}"
 	local max_colunas=20
@@ -48,42 +48,25 @@ zzascii ()
 	decimais=$(zzseq 32 126 ; zzseq 161 255)
 
 	# Cálculos das dimensões da tabela
-	local colunas=$(zzseq 0 $((num_colunas - 1)))
 	local largura_coluna=$((largura / num_colunas))
 	local num_caracteres=$(echo "$decimais" | sed -n '$=')
-	local num_linhas=$((num_caracteres / num_colunas + 1))
+	# Uma alternativa de cálculo de linhas com awk comentado, mas o bc é mais robustos e rápido
+	#local num_linhas=$(echo "$num_caracteres $num_colunas" | awk '{print int($1/$2.0) + (int($1/$2.0)==$1/$2.0?0:1)}')
+	local num_linhas=$(echo "$num_caracteres / $num_colunas + ($num_caracteres % $num_colunas != 0 || 1 && 0)" | bc)
 
 	# Mostra as dimensões
 	echo $num_caracteres caracteres, $num_colunas colunas, $num_linhas linhas, $largura de largura
 
-	# Linha a linha...
-	while [ $linha -lt $num_linhas ]
-	do
-		linha=$((linha+1))
-
-		# Extrai as referências (número da linha dentro do $decimais)
-		# para cada caractere que será mostrado nesta linha da tabela.
-		# É montado um comando Sed com eles: 5p; 10p; 13p;
-		referencias=''
-		for col in $colunas
-		do
-			referencias="$referencias $((num_linhas * col + linha))p;"
-		done
-
-		# Usando as referências coletadas, percorre cada decimal
-		# que será usado nesta linha da tabela
-		for decimal in $(echo "$decimais" | sed -n "$referencias")
+		for decimal in $decimais
 		do
 			hexa=$( printf '%X'   $decimal)
 			octal=$(printf '%03o' $decimal) # NNN
 			caractere=$(awk 'BEGIN { printf "%c", '$decimal' }')
 
 			# Na parte extendida da tabela ascii o tamanho do caractere precisa um espaço adicional.
-			[ $decimal -ge 161 ] && largura_col=$((largura_coluna+1)) || largura_col=$largura_coluna
+			test $decimal -ge 161 && largura_col=$((largura_coluna+1)) || largura_col=$largura_coluna
 
 			# Mostra a célula atual da tabela
-			printf "%${largura_col}s" "$decimal $hexa $octal $caractere"
-		done
-		echo
-	done
+			printf "%${largura_col}s\n" "$decimal $hexa $octal $caractere"
+		done | zzcolunar -r $num_colunas
 }
