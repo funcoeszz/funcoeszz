ec3f3ac07b74be8cb62ec87c5a262278a632d48d
itamarnet <itamarnet@yahoo.com.br>
2015-02-22 09:19:07 -0300

    zzoperadora: Site original usando javascript apenas, mudando então url e filtros.

--- zz/zzoperadora.sh
+++ zz/zzoperadora.sh
@@ -1,5 +1,5 @@
 # ----------------------------------------------------------------------------
-# http://www.qualoperadora.net
+# http://consultaoperadora.com.br
 # Consulta operadora de um número de telefone fixo/celular.
 # O formato utilizado é: <DDD><NÚMERO>
 # Não utilize espaços, (), -
@@ -8,7 +8,7 @@
 #
 # Autor: Mauricio Calligaris <mauriciocalligaris@gmail.com>
 # Desde: 2013-06-19
-# Versão: 2
+# Versão: 3
 # Licença: GPL
 # ----------------------------------------------------------------------------
 
@@ -16,21 +16,19 @@ zzoperadora ()
 {
 	zzzz -h operadora "$1" && return
 
-	local url="http://www.qualoperadora.net"
-	local post="telefone=$1"
+	local url="http://consultaoperadora.com.br"
+	local post="numero=$1"
 
 	# Verifica o paramentro
-	if (
-		! zztool testa_numero "$1" ||
-		test "$1" -eq 0)
+	if (! zztool testa_numero "$1" || test "$1" -eq 0)
 	then
 		zztool uso operadora
 		return 1
 	fi
 
-	# Faz a consulta no site e retira as informações da tag <title>
-	# do html da página.
-	curl -s --data-urlencode $post $url |
-	grep '<title>Resultado' |
-	sed 's/<[^>]*>//g;s/-\{0,1\} \{0,1\}[[:upper:]][a-z]*: //g;s/^ */ /'
+	# Faz a consulta no site
+	echo "${post}&tipo=consulta" |
+	$ZZWWWPOST "$url" |
+	sed -n '/Número:/p' |
+	awk '{print $1, $2; print $3, $4; for(i=6;i<=NF;i++) {printf  $i " "}; print ""}'
 }
