c0d9482d6adb93fafa68e0d9c30b36e361cc2d1a
Aurelio Jargas <verde@aurelio.net>
2015-02-23 12:29:58 -0300

    zzansi2html: testes atualizados

--- testador/zzansi2html.sh
+++ testador/zzansi2html.sh
@@ -1,14 +1,19 @@
 # preparativos
 $ printf '\033[32m verde \033[m\n' > _tmp1
-$ zzcores > _tmp2
+$ printf '\033[32m\033[K verde \033[m\033[K\r\r\n' > _tmp2
+$ zzcores > _tmp3
 $
 
 $ zzansi2html _tmp1
 <pre style="background:#000;color:#FFF"><div style="display:inline">
 <span style="color:#0F0;font-weight:normal;text-decoration:none"> verde </div><div style="display:inline">
 </pre>
-$ zzansi2html _tmp2  #→ --file zzansi2html.out.html
+$ zzansi2html _tmp1
+<pre style="background:#000;color:#FFF"><div style="display:inline">
+<span style="color:#0F0;font-weight:normal;text-decoration:none"> verde </div><div style="display:inline">
+</pre>
+$ zzansi2html _tmp3  #→ --file zzansi2html.out.html
 
 # faxina
-$ rm -f _tmp[12]
+$ rm -f _tmp[123]
 $
