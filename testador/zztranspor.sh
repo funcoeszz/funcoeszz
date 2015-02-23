a6f9ba980c1a3c3ec494b379afcf50a1135ac5c4
Aurelio Jargas <verde@aurelio.net>
2015-02-23 12:35:24 -0300

    zztranspor: arrumado teste sem prompt

--- testador/zztranspor.sh
+++ testador/zztranspor.sh
@@ -13,7 +13,7 @@ $ zztranspor _numeros.txt
 1 2 3 4 5
 $
 
-zztabuada 3 | zztranspor --ofs '\t'
+$ zztabuada 3 | zztranspor --ofs '\t'
 3	3	3	3	3	3	3	3	3	3	3
 x	x	x	x	x	x	x	x	x	x	x
 0	1	2	3	4	5	6	7	8	9	10
