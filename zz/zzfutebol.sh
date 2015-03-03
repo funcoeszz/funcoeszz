dbf33a2aca263df5604859e7f184c48837ca0166
itamarnet <itamarnet@yahoo.com.br>
2015-03-03 20:51:24 -0300

    zzfutebol: Refeito código, incluindo filtros de time, campeonato e horário, além da data.

--- zz/zzfutebol.sh
+++ zz/zzfutebol.sh
@@ -4,14 +4,24 @@
 # Além de mostrar os times que irão jogar, o script também mostra o dia,
 # o horário e por qual campeonato será o jogo.
 #
-# Uso: zzfutebol [ hoje | amanha | ontem | sabado | domingo ]
-# Ex.: zzfutebol
-#      zzfutebol hoje
-#      zzfutebol sabado
+# Suporta um argumento que pode ser:
+#
+# Um dos dias da semana, como:
+#  hoje, amanhã, segunda, terça, quarta, quinta, sexta, sábado, domingo.
+#
+# Um filtro como nome do campeonato ou nome do time.
+# Ou o horário de uma partida.
+#
+# Uso: zzfutebol [ argumento ]
+# Ex.: zzfutebol                 # Todas as partidas nos próximos dias.
+#      zzfutebol hoje            # Partidas que acontecem hoje.
+#      zzfutebol sabado          # Partidas que acontecem no sábado.
+#      zzfutebol libertadores    # Próximas partidas da Libertadores.
+#      zzfutebol 21h             # Partidas que começam entre 21 e 22h.
 #
 # Autor: Jefferson Fausto Vaz (www.faustovaz.com)
 # Desde: 2014-04-08
-# Versão: 2
+# Versão: 3
 # Licença: GPL
 # Requisitos: zzdata zzdatafmt
 # ----------------------------------------------------------------------------
@@ -19,40 +29,29 @@ zzfutebol ()
 {
 
 	zzzz -h futebol "$1" && return
+	local url="http://esporte.uol.com.br/futebol/agenda-de-jogos"
 
-	listajogos=$(
-		local url="http://esporte.uol.com.br/futebol/agenda-de-jogos"
-		$ZZWWWDUMP $url | awk ' {
-			gsub(/^[\t ]+/, "", $0)
-			gsub(/[\t ]+$/, "", $0)
-			imprimir=0
-
-			if ($0 ~ /^[0-9]+\/[0-9]+\/[0-9]+[\t ]+[0-9]+h[0-9]+/){
-				dadosdojogo=$0
+	$ZZWWWDUMP "$url" |
+	sed -n '/[0-9]h[0-9]/p;/_ X/p' |
+	zztool trim |
+	awk -F "[_]+" '
+		{
+			if ($0 ~ /[0-9]h[0-9]/) {
+				printf "%-40s", $0
 			}
-
-			if ($0 ~ /^[[:alpha:]\t _-]+X[[:alpha:]\t _-]/){
-				gsub( /^[A-Z]+ /, "", $0 )
-				gsub( / [A-Z]+$/, "", $0 )
-				gsub( / [_X ]+ /, "    x    ", $0)
-				jogo=$0
-				imprimir=1
+			else {
+				sub(/^[A-Z]+ /, "")
+				sub(/ [A-Z]+$/, "")
+				printf "%25s x %-25s\n", $1, $3
 			}
-
-			if(imprimir){
-				imprimir=0
-				printf "%-38s %s\n", dadosdojogo, jogo
-			}
-
-		}'
-	)
-
+		}' |
+	sed 's/  x  /x/' |
 	case "$1" in
-		hoje | amanh[aã] | ontem | s[aá]bado | domingo)
-			echo "$listajogos" | grep -e $( zzdata $1 | zzdatafmt -f 'DD/MM/AA' )
+		hoje | amanh[aã] | segunda | ter[cç]a | quarta | quinta | sexta | s[aá]bado | domingo)
+			grep --color=never -e $( zzdata $1 | zzdatafmt -f 'DD/MM/AA' )
 			;;
 		*)
-			echo "$listajogos"
+			grep --color=never -i "${1-.}"
 			;;
 	esac
 }
