bd83f0cf2cc0f4639603cc8f51d58784adb76173
Aurelio Jargas <verde@aurelio.net>
2015-03-04 08:21:10 -0300

    zztradutor: teste novo

--- testador/zztradutor.sh
+++ testador/zztradutor.sh
@@ -4,3 +4,7 @@ $ zztradutor	pt-es	livro	#→ libro
 $ zztradutor	pt-fr	livro	#→ livre
 $ zztradutor	pt-it	livro	#→ libro
 $ zztradutor	pt-de	livro	#→ Buch
+
+# Via STDIN
+
+$ echo livro | zztradutor pt-en #→ book
