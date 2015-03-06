2ca9ada8f0da6b192201fdf9561cac730b9382c5
Aurelio Jargas <verde@aurelio.net>
2015-03-06 17:14:23 -0300

    nanny: Ajustes de alinhamento no teste

--- testador/zzunescape.sh
+++ testador/zzunescape.sh
@@ -8,23 +8,23 @@ $
 
 # Preparativos - XML
 
-$ printf '&#34;&#x22;&quot; '	 > _tmp5
-$ printf '&#38;&#x26;&amp; '	>> _tmp5
-$ printf '&#39;&#x27;&apos; '	>> _tmp5
-$ printf '&#60;&#x3C;&lt; '	>> _tmp5
-$ printf '&#62;&#x3E;&gt;'	>> _tmp5
-$ echo                          >> _tmp5
+$ printf '&#34;&#x22;&quot; '  > _tmp5
+$ printf '&#38;&#x26;&amp; '  >> _tmp5
+$ printf '&#39;&#x27;&apos; ' >> _tmp5
+$ printf '&#60;&#x3C;&lt; '   >> _tmp5
+$ printf '&#62;&#x3E;&gt;'    >> _tmp5
+$ echo                        >> _tmp5
 $
 
 # HTML
 
-$ zzunescape	--html	_tmp2	#→ --file _tmp1
-$ zzunescape	--html	_tmp3	#→ --file _tmp1
-$ zzunescape	--html	_tmp4	#→ --file _tmp1
+$ zzunescape --html _tmp2        #→ --file _tmp1
+$ zzunescape --html _tmp3        #→ --file _tmp1
+$ zzunescape --html _tmp4        #→ --file _tmp1
 
 # XML
 
-$ zzunescape	--xml	_tmp5	#→ """ &&& ''' <<< >>>
+$ zzunescape --xml  _tmp5        #→ """ &&& ''' <<< >>>
 
 # Faxina
 
