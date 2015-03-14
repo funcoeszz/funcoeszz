3365cd358af7e6ca711759c84a2fe6cb94984987
itamarnet <itamarnet@yahoo.com.br>
2015-03-13 22:30:03 -0300

    zzalinhar: Adaptando para uso do zzpad.

--- testador/zzalinhar.sh
+++ testador/zzalinhar.sh
@@ -89,3 +89,17 @@ $ zzalinhar -c zzxml.out.xml | sed 's/$/|/'
        </section>        |
          </xml>          |
 $
+
+# Justificar
+$ sed 's/[<>]/ X /g;s|/| _ |g;' zzxml.in.xml | zztrim -H | zzalinhar -j
+X                                       xml                                       X
+X                                     section                                     X
+X        title        X         Título         X          _         title         X
+X               img               src="foo.png"                 _                 X
+X                                      para                                       X
+Meu parágrafo, com  X strong X negrito X  _ strong X  e  X em X itálico X  _ em X .
+X                          _                         para                         X
+X     escape     X      &quot;&amp;&apos;&lt;&gt;      X       _      escape      X
+X                         _                        section                        X
+X                          _                         xml                          X
+$
--- zz/zzalinhar.sh
+++ zz/zzalinhar.sh
@@ -10,7 +10,7 @@
 # determinam o tamanho da largura como base ao alinhamento.
 # Obs.: Se a largura original for maior é truncada no limite fornecido.
 #
-# Uso: zzalinhar [-l|-r|-c|-j] [-w <largura>] arquivo
+# Uso: zzalinhar [-l|-e|-r|-d|-c|-j] [-w <largura>] arquivo
 # Ex.: zzalinhar arquivo.txt
 #      zzalinhar -c -w 20 arquivo.txt
 #      zzalinhar -j arquivo.txt
@@ -18,22 +18,22 @@
 #
 # Autor: Itamar <itamarnet (a) yahoo com br>
 # Desde: 2014-05-23
-# Versão: 1
+# Versão: 2
 # Licença: GPL
-# Requisitos: zzcolunar zztrim
+# Requisitos: zztrim
 # ----------------------------------------------------------------------------
 zzalinhar ()
 {
 	zzzz -h alinhar "$1" && return
 
-	local alinhamento='l'
+	local alinhamento='r'
 	local largura=0
 
 	while test "${1#-}" != "$1"
 	do
 		case "$1" in
-		-l | --left | -e | --esqueda)  alinhamento='l' ;;
-		-r | --right | -d | --direita) alinhamento='r' ;;
+		-l | --left | -e | --esqueda)  alinhamento='r' ;;
+		-r | --right | -d | --direita) alinhamento='l' ;;
 		-c | --center | --centro)      alinhamento='c' ;;
 		-j | --justify | --justificar) alinhamento='j' ;;
 		-w | --width | --largura)
@@ -46,13 +46,16 @@ zzalinhar ()
 		shift
 	done
 
-	zztool file_stdin "$@" | zztrim |
-	if test $alinhamento != 'j'
+	zztool file_stdin "$@" |
+	if test "$alinhamento" = "j"
 	then
-		zzcolunar -$alinhamento -w $largura 1
+		sed 's/"/\\"/g'
 	else
-		zzcolunar -l -w $largura 1 | zztrim |
-		awk -v larg=$largura '
+		cat -
+	fi |
+	zztrim -H |
+	awk -v larg=$largura -v opt_pad=$alinhamento '
+		# Função para unir os campos e os separadores de campos(" ")
 		function juntar(  str_saida, j) {
 			str_saida=""
 			for ( j=1; j<=length(campos); j++ ) {
@@ -61,34 +64,70 @@ zzalinhar ()
 			sub(/ *$/, "", str_saida)
 			return str_saida
 		}
+
+		# Função que aumenta a quantidade de espaços intermadiários
 		function aumentar() {
 			espacos[pos_atual] = espacos[pos_atual] " "
 			pos_atual--
-			pos_atual = pos_atual == 0 ? qtde : pos_atual
+			pos_atual = (pos_atual == 0 ? qtde : pos_atual)
 		}
 
-		{ linha[NR] = $0; larg = ( length > larg ? length : larg ) }
+		# Função para determinar tamanho da string sem erros com codificação
+		function tam_linha(entrada,  saida, comando) 
+		{
+			comando = ("echo \"" entrada "\" | wc -m")
+			comando | getline saida
+			close(comando)
+			return saida-1
+		}
 
-		END {
-			for (i=1; i<=NR; i++) {
-				if (length(linha[i]) == larg) { print linha[i] }
-				else {
-					split("", campos)
-					split("", espacos)
-					qtde = split(linha[i], campos, " ", espacos)
-					if ( qtde <= 1 ) { print linha[i] }
-					else {
-						pos_atual = qtde - 1
-						saida = juntar()
-						while ( length(saida) < larg ) {
-							aumentar()
-							saida = juntar()
-						}
-						print saida
-					}
-				}
+		{
+			# Guardando as linhas em um array
+			linha[NR] = $0
+
+			# Guardando na variável larg o maior tamanho de linha
+			tam_atual = tam_linha($0)
+			if (larg < tam_atual) {
+				larg = tam_atual
 			}
 		}
-		'
+
+		END {
+			if (opt_pad == "j") {
+				for (i=1; i<=NR; i++) {
+					if (tam_linha(linha[i]) == larg) { print linha[i] }
+					else {
+						split("", campos)
+						split("", espacos)
+						qtde = split(linha[i], campos, " ", espacos)
+						if ( qtde <= 1 ) { print linha[i] }
+						else {
+							pos_atual = qtde - 1
+							saida = juntar()
+							while ( tam_linha(saida) < larg ) {
+								aumentar()
+								saida = juntar()
+							}
+							print saida
+						}
+					}
+				}
+			} else {
+				opt_pad = (opt_pad == "c" ? "a" : opt_pad)
+				command = ("funcoeszz zzpad -" opt_pad " " larg)
+
+				# Usando zzpad para alinhar a partir do array
+				for (num_linha=1; num_linha<=NR; num_linha++) {
+					print linha[num_linha] | command
+				}
+				close(command)
+			}
+		}
+	' |
+	if test "$alinhamento" = "j"
+	then
+		sed 's/\\"/"/g'
+	else
+		cat -
 	fi
 }
