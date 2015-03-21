abbc2a16312070a42da4dfd8961a1aff1e27c32b
Aurelio Jargas <verde@aurelio.net>
2015-03-21 07:14:38 -0300

    zzrastreamento: teste atualizado
    O código de rastreamento anterior parou de funcionar. Imagino que o
    Correio deva remover registros antigos após um tempo.

--- testador/zzrastreamento.sh
+++ testador/zzrastreamento.sh
@@ -1,4 +1,13 @@
-$ zzrastreamento SA488387146BR | sed '2 s|^../../.... ..:.. [^ ].*|DATA HORA TEXTO|; 2q'
-Data             Local                           Situação
-DATA HORA TEXTO
+$ zzrastreamento PE354648030BR
+Data             Local                                             Situação
+16/03/2015 20:32 CEE VILA GUILHERME - Sao Paulo/SP                 Entrega Efetuada
+16/03/2015 11:16 Sao Paulo/SP                                      Saiu para entrega ao destinatário
+16/03/2015 08:24 CTE VILA MARIA - Sao Paulo/SP                     Encaminhado
+                 Encaminhado para CEE VILA GUILHERME - Sao Paulo/SP
+12/03/2015 19:06 CTE FLORIANOPOLIS/GTURN3 (BLUMENAU) - Blumenau/SC Encaminhado
+                 Em trânsito para CTE VILA MARIA - Sao Paulo/SP
+11/03/2015 08:59 AGF SAGUACU - Joinville/SC                        Encaminhado
+                 Em trânsito para CTE FLORIANOPOLIS/GTURN3 (BLUMENAU) - Blumenau/SC
+10/03/2015 16:58 AGF SAGUACU - Joinville/SC                        Postado depois do horário limite da agência
+                 Objeto sujeito a encaminhamento no próximo dia útil
 $
