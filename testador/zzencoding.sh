1db540d0b0c8f6550a85268647baca4902e4c772
Aurelio Jargas <verde@aurelio.net>
2015-03-23 07:26:18 -0300

    Função nova zzencoding: Informa a codificação de um arquivo/texto
    Veja issue #197

---
+++ testador/zzencoding.sh
@@ -0,0 +1,44 @@
+# UTF-8
+
+$ zzencoding zzascii.sh
+utf-8
+$ cat zzascii.sh | zzencoding
+utf-8
+$ echo bênção | zzencoding
+utf-8
+$
+
+# ISO-8859-1
+
+$ zzencoding _iso-8859-1.txt
+iso-8859-1
+$ cat _iso-8859-1.txt | zzencoding
+iso-8859-1
+$
+
+# US-ASCII
+
+$ zzencoding _numeros.txt
+us-ascii
+$ cat _numeros.txt | zzencoding
+us-ascii
+$ echo foo | zzencoding
+us-ascii
+$
+
+# sem \n
+
+$ printf bênção | zzencoding
+utf-8
+$ printf '\341 ' | zzencoding
+iso-8859-1
+$ printf foo | zzencoding
+us-ascii
+$
+
+# vazio
+
+$ zzencoding _vazio.txt
+$ echo | zzencoding
+$ printf '' | zzencoding
+$
---
+++ zz/zzencoding.sh
@@ -0,0 +1,24 @@
+# ----------------------------------------------------------------------------
+# Informa qual a codificação de um arquivo (ou texto via STDIN).
+#
+# Uso: zzencoding [arquivo]
+# Ex.: zzencoding /etc/passwd          # us-ascii
+#      zzencoding index-iso.html       # iso-8859-1
+#      echo FooBar | zzencoding        # us-ascii
+#      echo Bênção | zzencoding        # utf-8
+#
+# Autor: Aurelio Marinho Jargas, www.aurelio.net
+# Desde: 2015-03-21
+# Versão: 1
+# Licença: GPL
+# ----------------------------------------------------------------------------
+zzencoding ()
+{
+	zzzz -h encoding "$1" && return
+
+	zztool file_stdin "$@" |
+		# A opção --mime é portável, -i/-I não
+		# O - pode não ser portável, mas /dev/stdin não funciona
+		file -b --mime - |
+		sed -n 's/.*charset=//p'
+}
