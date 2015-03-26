495eeff77433a362e568aa3e58a358597cd9a492
Aurelio Jargas <verde@aurelio.net>
2015-03-26 10:34:59 -0300

    zzurlencode: suporte a UTF-8, opção nova -t e -n
    Tive que reescrever a função para poder suportar UTF-8. O od é usado
    para descobrir os códigos hexadecimais.
    
    A parte de proteger alguns caracteres da conversão ficou muito mágica,
    mas foi a solução mais robusta que encontrei das várias que tentei.
    Usar loop no shell caractere a caractere deu problemas com alguns
    caracteres especiais para o shell.
    
    Os caracteres a não converter não são mais posicionais, e agora possuem
    sua própria opção: -n.
    
    Testador bem completo, tentando chegar nos limites do algoritmo.
    
    Veja issues #194 e #201

--- zz/zzurlencode.sh
+++ zz/zzurlencode.sh
@@ -1,60 +1,96 @@
 # ----------------------------------------------------------------------------
-# Codifica um texto numa URL.
-# Permite especificar um conjunto de caracteres a não serem codificados.
-# Uso: zzurlencode [string]
-# Ex.: zzurlencode '!@#$_+{}^+abcd'     # %21%40%23%24_%2B%7B%7D%5E%2Babcd
-#      zzurlencode ':/' http://         # http://
-#      zzurlencode http://              # http%3A%2F%2F
+# http://en.wikipedia.org/wiki/Percent-encoding
+# Codifica o texto como %HH, para ser usado numa URL (a/b → a%2Fb)
+# Obs.: Por padrão, letras, números e _.~- não são codificados (RFC 3986)
+#
+# Opções:
+#   -t, --todos  Codifica todos os caracteres, sem exceção
+#   -n STRING    Informa caracteres adicionais que não devem ser codificados
+#
+# Uso: zzurlencode [texto]
+# Ex.: zzurlencode http://www            # http%3A%2F%2Fwww
+#      zzurlencode -n : http://www       # http:%2F%2Fwww
+#      zzurlencode -t http://www         # %68%74%74%70%3A%2F%2F%77%77%77
+#      zzurlencode -t -n w/ http://www   # %68%74%74%70%3A//www
 #
 # Autor: Guilherme Magalhães Gall <gmgall (a) gmail com>
 # Desde: 2013-03-19
-# Versão: 3
+# Versão: 4
 # Licença: GPL
 # ----------------------------------------------------------------------------
 zzurlencode ()
 {
 	zzzz -h urlencode "$1" && return
 
-	local string
-	local caractere
-	local nl=$(printf '\n')
-	local nao_converter='-ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_.'
+	local resultado undo
 
-	# Se foram passados 3 ou mais parâmetros, mostra mensagem de uso.
-	if test $# -ge 3; then
-		zztool -e uso urlencode
-		return 1
-	# Se 2 parâmetros foram passados, o 1º indica um conjunto de
-	# caracteres que não serão codificados e o 2º o texto a codificar.
-	elif test $# -eq 2; then
-		nao_converter="$nao_converter$1"
-		string="$2"
-	# Se apenas 1 parâmetro foi passado, é o texto a codificar.
-	else
-		string="$1"
-	fi
+	# RFC 3986, unreserved - Estes nunca devem ser convertidos (exceto se --all)
+	local nao_converter='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_.~-'
 
-	# Loop a cada caractere. O printf o converte para hexa se o caractere
-	# não constar em nao_converter, senão o imprime
-	printf %s "$string" | while IFS= read -r -n 1 caractere
+	while test -n "$1"
 	do
-		if test "$caractere" = "$nl"
-		then
-			# Exceção para contornar um bug:
-			#   printf %x 'c retorna 0 quando c=\n
-			printf '%%0A'
-		else
-			# Não usei zztool grep_var porque ele considera ?
-			# caractere especial (glob)
-			if printf %s "$nao_converter" | fgrep "$caractere" > /dev/null
-			then
-				printf "$caractere"
-			else
-				printf '%%%02X' "'$caractere"
-			fi
-		fi
+		case "$1" in
+			-t | --todos | -a | --all)
+				nao_converter=''
+				shift
+			;;
+			-n)
+				if test -z "$2"
+				then
+					zztool erro 'Faltou informar o valor da opção -n'
+					return 1
+				fi
+
+				nao_converter="$nao_converter$2"
+				shift
+				shift
+			;;
+			--) shift; break;;
+			-*) zztool erro "Opção inválida: $1"; return 1;;
+			*) break;;
+		esac
 	done
 
-	# Garante a quebra de linha no final do resultado
-	echo
+	# Codifica todos os caracteres, sem exceção
+	# foo → %66%6F%6F
+	resultado=$(
+		if test -n "$1"
+		then printf %s "$*"  # texto via argumentos
+		else cat -           # texto via STDIN
+		fi |
+		# Usa o comando od para descobrir o valor hexa de cada caractere.
+		# É portável e suporta UTF-8, decompondo cada caractere em seus bytes.
+		od -v -A n -t x1 |
+		# Converte os números hexa para o formato %HH, sem espaços
+		tr -d ' \n\t' |
+		sed 's/../%&/g' |
+		zzmaiusculas
+	)
+
+	# Há caracteres protegidos, que não devem ser convertidos?
+	if test -n "$nao_converter"
+	then
+		# Desfaz a conversão de alguns caracteres (usando magia)
+		#
+		# Um sed é aplicado no resultado original "desconvertendo"
+		# alguns dos %HH de volta para caracteres normais. Mas como
+		# fazer isso somente para os caracteres de $nao_converter?
+		#
+		# É usada a própria zzurlencode para codificar a lista dos
+		# protegidos, e um sed formata esse resultado, compondo outro
+		# script sed, que será aplicado no resultado original trocando
+		# os %HH por \xHH.
+		#
+		# $ zzurlencode -t -- "ab" | sed 's/%\(..\)/s,&,\\\\x\1,g; /g'
+		# s,%61,\\x61,g; s,%62,\\x62,g;
+		#
+		# Essa string manipulada será mostrada pelo printf %b, que
+		# expandirá os \xHH tornando-os caracteres normais novamente.
+		# Ufa! :)
+		#
+		undo=$(zzurlencode -t -- "$nao_converter" | sed 's/%\(..\)/s,&,\\\\x\1,g; /g')
+		printf '%b\n' $(echo "$resultado" | sed "$undo")
+	else
+		printf '%s\n' "$resultado"
+	fi
 }
