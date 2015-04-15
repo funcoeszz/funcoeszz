1aa54c50e50f7bddd31bfe3b940250871fba2db1
itamarnet <itamarnet@yahoo.com.br>
2015-04-14 22:29:29 -0300

    zzfutebol: Adicionado recurso de conferir placar de partidas terminadas.

--- zz/zzfutebol.sh
+++ zz/zzfutebol.sh
@@ -1,27 +1,31 @@
 # ----------------------------------------------------------------------------
 # http://esporte.uol.com.br/futebol/agenda-de-jogos
 # Mostra todos os jogos de futebol marcados para os proximos dias.
-# Além de mostrar os times que irão jogar, o script também mostra o dia,
-# o horário e por qual campeonato será o jogo.
+# Ou os resultados de jogos recentes.
+# Além de mostrar os times que jogam, o script também mostra o dia,
+# o horário e por qual campeonato será ou foi o jogo.
 #
-# Suporta um argumento que pode ser:
-#
-# Um dos dias da semana, como:
+# Suporta um argumento que pode ser um dos dias da semana, como:
 #  hoje, amanhã, segunda, terça, quarta, quinta, sexta, sábado, domingo.
 #
-# Um filtro como nome do campeonato ou nome do time.
-# Ou o horário de uma partida.
+# Ou um ou dois argumentos para ver resultados do jogos:
+#   resultado ou placar, que pode ser acompanhado de hoje, ontem, anteontem.
 #
-# Uso: zzfutebol [ argumento ]
+# Um filtro com nome do campeonato, nome do time, ou horário de uma partida.
+#
+# Uso: zzfutebol [resultado | placar ] [ argumento ]
 # Ex.: zzfutebol                 # Todas as partidas nos próximos dias.
 #      zzfutebol hoje            # Partidas que acontecem hoje.
 #      zzfutebol sabado          # Partidas que acontecem no sábado.
 #      zzfutebol libertadores    # Próximas partidas da Libertadores.
 #      zzfutebol 21h             # Partidas que começam entre 21 e 22h.
+#      zzfutebol resultado       # Placar dos jogos já ocorridos.
+#      zzfutebol placar ontem    # Placar dos jogos de ontem.
+#      zzfutebol placar espanhol # Placar dos jogos do Campeonato Espanhol.
 #
 # Autor: Jefferson Fausto Vaz (www.faustovaz.com)
 # Desde: 2014-04-08
-# Versão: 6
+# Versão: 7
 # Licença: GPL
 # Requisitos: zzdata zzdatafmt zztrim zzpad
 # ----------------------------------------------------------------------------
@@ -32,19 +36,29 @@ zzfutebol ()
 	local url="http://esporte.uol.com.br/futebol/central-de-jogos/proximos-jogos"
 	local linha campeonato time1 time2
 
+	case "$1" in
+		resultado | placar) url="http://esporte.uol.com.br/futebol/central-de-jogos/resultados"; shift;;
+		ontem | anteontem)  url="http://esporte.uol.com.br/futebol/central-de-jogos/resultados";;
+	esac
+
 	$ZZWWWDUMP "$url" |
-	sed -n '
-		/[0-9]h[0-9]/p
-		/ __*$/ {s/ [A-Z][A-Z][A-Z]//; s/ __*//; p;}
-		/º / { s/.*\([0-9]\{1,\}º\)/\1/;p }' |
+	sed -n '/[0-9]h[0-9]/{N;N;p;}' |
+	sed '
+		s/[A-Z][A-Z][A-Z] //
+		s/__*//
+		/º / { s/.*\([0-9]\{1,\}º\)/\1/ }' |
 	zztrim |
 	awk '
 		NR % 3 == 1 { campeonato = $0 }
 		NR % 3 == 2 { time1 = $0 }
-		NR % 3 == 0 { print campeonato ":" time1 ":" $0 }
+		# NR % 3 == 0 { print campeonato ":" time1 ":" $0 }
+		NR % 3 == 0 {
+			if ($NF ~ /^[0-9]$/) { reserva=$NF " "; $NF=""; } else { reserva="" }
+			print campeonato ":" time1 ":" reserva $0
+		}
 		' |
 	case "$1" in
-		hoje | amanh[aã] | segunda | ter[cç]a | quarta | quinta | sexta | s[aá]bado | domingo)
+		hoje | amanh[aã] | segunda | ter[cç]a | quarta | quinta | sexta | s[aá]bado | domingo | ontem | anteontem)
 			grep --color=never -e $( zzdata $1 | zzdatafmt -f 'DD/MM/AA' )
 			;;
 		*)
