ee774a04a5222d457e1502d5b7ae459df9412a81
Aurelio Jargas <verde@aurelio.net>
2015-05-28 23:00:42 -0300

    zzglobo: Adicionado teste

---
+++ testador/zzglobo.sh
@@ -0,0 +1,12 @@
+$ zzglobo | sed '1s/\[..-..-....\]/[DATA]/; s/^[0-9][0-9]:[0-9][0-9] - .*/HORA - DESCRIÇÃO/' | head -n 10
+Hoje [DATA]
+
+HORA - DESCRIÇÃO
+HORA - DESCRIÇÃO
+HORA - DESCRIÇÃO
+HORA - DESCRIÇÃO
+HORA - DESCRIÇÃO
+HORA - DESCRIÇÃO
+HORA - DESCRIÇÃO
+HORA - DESCRIÇÃO
+$
