a54fcb07eb25e7b0dece6557f6934923a6bd4a3e
Aurelio Jargas <verde@aurelio.net>
2015-05-28 23:02:06 -0300

    zzglobo: sem brancos inúteis na saída
    - Requer zztrim
    - Removida variável $DATA não utilizada
    - $URL virou $url, para seguir o padrão ZZ

--- zz/zzglobo.sh
+++ zz/zzglobo.sh
@@ -5,18 +5,19 @@
 #
 # Autor: Vinícius Venâncio Leite <vv.leite (a) gmail com>
 # Desde: 2007-11-30
-# Versão: 4
+# Versão: 5
 # Licença: GPL
+# Requisitos: zztrim
 # ----------------------------------------------------------------------------
 zzglobo ()
 {
 	zzzz -h globo "$1" && return
 
-	local DATA=`date +%d | sed 's/^0//'`
-	local URL="http://vejonatv.com.br/programacao/globo-rede.html"
+	local url="http://vejonatv.com.br/programacao/globo-rede.html"
 
-	$ZZWWWDUMP "$URL" |
+	$ZZWWWDUMP "$url" |
 		sed -n "/Hoje \[[0-9]*\-[0-9]*\-[0-9]*\]/,/Amanhã .*/p" |
 		sed '$d' |
-		uniq
+		uniq |
+		zztrim
 }
