d754f1628628280a5b1efad07604be65c394e712
Italo Gonçales <italo.goncales@gmail.com>
2015-10-01 07:34:20 -0400

    Função nova: zzexcuse - Exibe aleatoriamente uma desculpa esfarrapada de programadores ( em ingles )

---
+++ zz/zzexcuse.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source /usr/bin/funcoeszz    # inclui o ambiente ZZ
+ZZPATH=$PWD/excuse.sh        # o PATH desse script
+
+# ----------------------------------------------------------------------------
+# Da uma desculpa comum de desenvolvedor ( em ingles )
+# Fonte de pesquisa: http://programmingexcuses.com/
+# Uso: zzexcuse
+# Ex.: zzexcuse
+#
+# Autor: Italo Gonçales, @goncalesi, <italo.goncales (a) gmail com>
+# Versão: 1
+# Licença: GPL
+# ----------------------------------------------------------------------------
+
+zzexcuse ()
+{
+    zzzz -h excuse "$1" && return
+
+    printf "$( curl -s http://developerexcuses.com/ | 
+           perl -ne '/center.*nofollow.*?>(.*?)<\/a>/ and print "$1"' )\n"
+}
