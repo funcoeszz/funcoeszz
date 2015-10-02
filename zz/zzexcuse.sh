457288b5e85615f463289ce33b07b250e78ba404
Italo Gonçales <italo.goncales@gmail.com>
2015-10-02 10:04:59 -0400

    Challenge accepted: Tirando dependencia de perl para sed e adicionando opção de traduzir

--- zz/zzexcuse.sh
+++ zz/zzexcuse.sh
@@ -1,18 +1,34 @@
 #!/bin/bash
 # ----------------------------------------------------------------------------
-# Da uma desculpa comum de desenvolvedor ( em ingles )
+# Da uma desculpa comum de desenvolvedor ( em ingles ).
 # Fonte de pesquisa: http://programmingexcuses.com/
-# Uso: zzexcuse
-# Ex.: zzexcuse
+# Use a opção -t ou --traduzir para traduzir a desculpa em portugues
+#
+# Uso: zzexcuse [-t|--traduzir]
+# Ex.: zzexcuse         # The user must not know how to use it
+#      zzexcuse -t      # O usuário não deve saber como usá-lo
 #
 # Autor: Italo Gonçales, @goncalesi, <italo.goncales (a) gmail com>
+# Desde: 2015-10-01
 # Versão: 1
 # Licença: GPL
+# Requisitos: zztradutor
 # ----------------------------------------------------------------------------
 zzexcuse ()
 {
     zzzz -h excuse "$1" && return
-
-    printf "$( curl -s http://developerexcuses.com/ | 
-           perl -ne '/center.*nofollow.*?>(.*?)<\/a>/ and print "$1"' )\n"
+    
+    case "$1" in 
+        -t | --traduzir ) # O usuário informou a opção de tradução
+            printf "$( curl -s http://developerexcuses.com/ |
+                sed -rn 's/\s{2}<center.*nofollow.*?>(.*?)<\/a>.*/\1/p' |
+                zztradutor en-pt $2 )\n"
+            return
+            ;;
+        *)
+            printf "$( curl -s http://developerexcuses.com/ |
+                sed -rn 's/\s{2}<center.*nofollow.*?>(.*?)<\/a>.*/\1/p' )\n"
+            return 
+            ;;
+    esac
 }
