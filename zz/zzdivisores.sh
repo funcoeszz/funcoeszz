37cd463c7a8aa89424cf14e19038a081895d6b4f
itamarnet <itamarnet@yahoo.com.br>
2015-10-11 10:05:19 -0300

    zzdivisores: Modificando e tornando o código mais limpo e rápido.

--- zz/zzdivisores.sh
+++ zz/zzdivisores.sh
@@ -6,9 +6,8 @@
 #
 # Autor: Itamar <itamarnet (a) yahoo com br>
 # Desde: 2013-03-25
-# Versão: 3
+# Versão: 4
 # Licença: GPL
-# Requisitos: zzfatorar
 # ----------------------------------------------------------------------------
 zzdivisores ()
 {
@@ -16,38 +15,29 @@ zzdivisores ()
 
 	test -n "$1" || { zztool -e uso divisores; return 1; }
 
-	local fatores fator divisores_temp divisor divisor_atual
-	local divisores="1"
-
 	if zztool testa_numero "$1" && test $1 -ge 2
 	then
-		# Decompõe o número em fatores primos
-		fatores=$(zzfatorar --no-bc $1 | cut -f 2 -d "|" | zztool lines2list)
-
-		# Se for primo informa 1 e ele mesmo
-		zztool grep_var 'primo' "$fatores" && { echo "1 $1"; return; }
-
-		for fator in $fatores
-		do
-			# Para cada fator primo, multiplica-se pelos divisores já conhecidos
-			for divisor in $divisores
-			do
-				divisor_atual=$(($fator * $divisor))
-
-				# Apenas armazenando se divisor não existir
-				echo "$divisores_temp" | zztool list2lines | grep "^${divisor_atual}$" > /dev/null
-				if test $? -eq 1
-				then
-					divisores_temp=$( echo "$divisores_temp $divisor_atual")
-				fi
-			done
-
-			# Reabastece a variável divisores eliminando repetições
-			divisores=$(echo "$divisores $divisores_temp" | zztool list2lines | sort -n | uniq | zztool lines2list)
-		done
-
-		# Elimina-se as repetições e ordena-se os divisores encontrados
-		echo $divisores | zztool list2lines | sort -n | uniq | zztool lines2list | zztool nl_eof
+		# Código adaptado a partir da solução em:
+		# http://stackoverflow.com/questions/11699324/
+		echo "$1" |
+		awk '{
+				i=2
+				limits = sqrt($1)
+				while (i <= limits) {
+					if ($1 % i == 0) {
+						result[i] = i
+						ind = $1 / i
+						if (i != ind ) { result[ind] = ind }
+					}
+					i++
+				}
+				asort(result)
+				printf 1
+				for (j in result) {
+					printf " " result[j]
+				}
+				print " " $1
+		}'
 	else
 		# Se não for um número válido exibe a ajuda
 		zzdivisores -h
