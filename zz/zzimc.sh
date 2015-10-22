d40c4f4923a98c386f6a536c658afaa29381d2a5
Rafael <rafael@localhost.localdomain>
2015-10-21 22:07:39 -0700

    Chamada da função adicionada zzzz corrigida, passando direto o resultado do imc para o awk usando um |, utilizando as funções testa_numero e testa_numero_fracionado para testar os valores de entrada.

--- zz/zzimc.sh
+++ zz/zzimc.sh
@@ -9,35 +9,53 @@
 # Versão: 1
 # Licença: GPL
 # ----------------------------------------------------------------------------
-
 zzimc ()
 {
 
-   zzzz -h chaves "$1" && return
-   
-    local PESO=$1
-    local ALTURA=$2
+    zzzz -h zzimc "$1" && return
+    
+    if zztool testa_numero "$1" || zztool testa_numero_fracionario "$1"
+    then
+        
+        if zztool testa_numero "$2" || zztool testa_numero_fracionario "$2"
+        then
 
-    VALOR_IMC=`echo "scale=2;$PESO / ( $ALTURA^2 )" | bc`
-
-    awk 'BEGIN { 
-
-        if ('$VALOR_IMC' >= 40 ) {print "IMC: '$VALOR_IMC' - OBESIDADE GRAU III"}
-
-        if ('$VALOR_IMC' < 40 && '$VALOR_IMC' >= 35) {print "IMC: '$VALOR_IMC' - OBESIDADE GRAU II"}
-        
-        if ('$VALOR_IMC' < 35 && '$VALOR_IMC' >= 30) {print "IMC: '$VALOR_IMC' - OBESIDADE GRAU I"}
-        
-        if ('$VALOR_IMC' < 30 && '$VALOR_IMC' >= 25) {print "IMC: '$VALOR_IMC' - PRE-OBESIDADE"}
-        
-        if ('$VALOR_IMC' < 25 && '$VALOR_IMC' >= 18.5) {print "IMC: '$VALOR_IMC' - PESO ADEQUADO"}
-        
-        if ('$VALOR_IMC' < 18.5 && '$VALOR_IMC' >= 17) {print "IMC: '$VALOR_IMC' - MAGREZA GRAU I"}
-        
-        if ('$VALOR_IMC' < 17 && '$VALOR_IMC' >= 16) {print "IMC: '$VALOR_IMC' - MAGREZA GRAU II"}
-        
-        if ('$VALOR_IMC' < 16 ) {print "IMC: '$VALOR_IMC' - MAGREZA GRAU III"}
-        
-    }'
-
+            local PESO=$1
+            local ALTURA=$2
+            
+            echo "scale=2;$PESO / ( $ALTURA^2 )" | bc | awk '{ 
+            
+                if ($1 >= 40 ) {print "IMC: "$1" - OBESIDADE GRAU III"}
+            
+                if ($1 < 40 && $1 >= 35) {print "IMC: "$1" - OBESIDADE GRAU II"}
+                
+                if ($1 < 35 && $1 >= 30) {print "IMC: "$1" - OBESIDADE GRAU I"}
+                
+                if ($1 < 30 && $1 >= 25) {print "IMC: "$1" - PRE-OBESIDADE"}
+                
+                if ($1 < 25 && $1 >= 18.5) {print "IMC: "$1" - PESO ADEQUADO"}
+                
+                if ($1 < 18.5 && $1 >= 17) {print "IMC: "$1" - MAGREZA GRAU I"}
+                
+                if ($1 < 17 && $1 >= 16) {print "IMC: "$1" - MAGREZA GRAU II"}
+                
+                if ($1 < 16 ) {print "IMC: "$1" - MAGREZA GRAU III"}
+                
+            }'
+        else
+    
+            echo "Valor inserido inválido, favor verificar!"
+            echo "Ex: "
+            echo "    zzimc 108.5 1.73"
+            echo "    zzimc 85 1.60"
+            echo "    zzimc 125 2"
+        fi
+    else
+    
+        echo "Valor inserido inválido, favor verificar!"
+        echo "Ex: "
+        echo "    zzimc 108.5 1.73"
+        echo "    zzimc 85 1.60"
+        echo "    zzimc 125 2"
+    fi
 }
