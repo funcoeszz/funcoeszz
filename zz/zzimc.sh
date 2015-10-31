46701b091aab663ea13b138633a0c4fbfcdfb202
Rafael <rafael@localhost.localdomain>
2015-10-30 18:37:06 -0700

    Convertendo ',' para '.', ajuste na mensagem de erro e na forma como é gerada

--- zz/zzimc.sh
+++ zz/zzimc.sh
@@ -14,48 +14,43 @@ zzimc ()
 
     zzzz -h zzimc "$1" && return
     
-    if zztool testa_numero "$1" || zztool testa_numero_fracionario "$1"
+    local PESO=`echo $1 | tr "," "."`
+    local ALTURA=`echo $2 | tr "," "."`
+    
+    if ! ( zztool testa_numero "$PESO" || zztool testa_numero_fracionario "$PESO" )
     then
-        
-        if zztool testa_numero "$2" || zztool testa_numero_fracionario "$2"
-        then
-
-            local PESO=$1
-            local ALTURA=$2
-            
-            echo "scale=2;$PESO / ( $ALTURA^2 )" | bc | awk '{ 
-            
-                if ($1 >= 40 ) {print "IMC: "$1" - OBESIDADE GRAU III"}
-            
-                if ($1 < 40 && $1 >= 35) {print "IMC: "$1" - OBESIDADE GRAU II"}
-                
-                if ($1 < 35 && $1 >= 30) {print "IMC: "$1" - OBESIDADE GRAU I"}
-                
-                if ($1 < 30 && $1 >= 25) {print "IMC: "$1" - PRE-OBESIDADE"}
-                
-                if ($1 < 25 && $1 >= 18.5) {print "IMC: "$1" - PESO ADEQUADO"}
-                
-                if ($1 < 18.5 && $1 >= 17) {print "IMC: "$1" - MAGREZA GRAU I"}
-                
-                if ($1 < 17 && $1 >= 16) {print "IMC: "$1" - MAGREZA GRAU II"}
-                
-                if ($1 < 16 ) {print "IMC: "$1" - MAGREZA GRAU III"}
-                
-            }'
-        else
     
-            echo "Valor inserido inválido, favor verificar!"
-            echo "Ex: "
-            echo "    zzimc 108.5 1.73"
-            echo "    zzimc 85 1.60"
-            echo "    zzimc 125 2"
-        fi
-    else
+        zztool erro "Valor inserido para o peso está inválido, favor verificar!"
+        return 1
     
-        echo "Valor inserido inválido, favor verificar!"
-        echo "Ex: "
-        echo "    zzimc 108.5 1.73"
-        echo "    zzimc 85 1.60"
-        echo "    zzimc 125 2"
     fi
+        
+    if ! ( zztool testa_numero "$ALTURA" || zztool testa_numero_fracionario "$ALTURA" )
+    then
+    
+        zztool erro "Valor inserido para a altura está inválido, favor verificar!"
+        return 1
+        
+    fi
+            
+    echo "scale=2;$PESO / ( $ALTURA^2 )" | bc | awk '{ 
+            
+        if ($1 >= 40 ) {print "IMC: "$1" - OBESIDADE GRAU III"}
+            
+        if ($1 < 40 && $1 >= 35) {print "IMC: "$1" - OBESIDADE GRAU II"}
+                
+        if ($1 < 35 && $1 >= 30) {print "IMC: "$1" - OBESIDADE GRAU I"}
+                
+        if ($1 < 30 && $1 >= 25) {print "IMC: "$1" - PRE-OBESIDADE"}
+                
+        if ($1 < 25 && $1 >= 18.5) {print "IMC: "$1" - PESO ADEQUADO"}
+                
+        if ($1 < 18.5 && $1 >= 17) {print "IMC: "$1" - MAGREZA GRAU I"}
+                
+        if ($1 < 17 && $1 >= 16) {print "IMC: "$1" - MAGREZA GRAU II"}
+                
+        if ($1 < 16 ) {print "IMC: "$1" - MAGREZA GRAU III"}
+                
+    }'
+
 }
