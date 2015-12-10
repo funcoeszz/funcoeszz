d696d3ff825cb81ce48cbb7777a68be5f67bf5a5
itamarnet <itamarnet@yahoo.com.br>
2015-12-10 08:13:06 -0200

    zzexcuse: Ajuste no cabeçalho, portabilidade do sed e uso de $ZZWWWDUMP

--- zz/zzexcuse.sh
+++ zz/zzexcuse.sh
@@ -1,34 +1,27 @@
-#!/bin/bash
 # ----------------------------------------------------------------------------
 # Da uma desculpa comum de desenvolvedor ( em ingles ).
-# Fonte de pesquisa: http://programmingexcuses.com/
-# Use a opção -t ou --traduzir para traduzir a desculpa em portugues
+# Com a opção -t ou --traduzir mostra as desculpas traduzidas.
 #
 # Uso: zzexcuse [-t|--traduzir]
-# Ex.: zzexcuse         # The user must not know how to use it
-#      zzexcuse -t      # O usuário não deve saber como usá-lo
+# Ex.: zzexcuse
 #
 # Autor: Italo Gonçales, @goncalesi, <italo.goncales (a) gmail com>
-# Desde: 2015-10-01
-# Versão: 1
+# Desde: 2015-09-26
+# Versão: 2
 # Licença: GPL
-# Requisitos: zztradutor
+# Requisitos: zztrim zztradutor
 # ----------------------------------------------------------------------------
 zzexcuse ()
 {
-    zzzz -h excuse "$1" && return
-    
-    case "$1" in 
-        -t | --traduzir ) # O usuário informou a opção de tradução
-            printf "$( curl -s http://developerexcuses.com/ |
-                sed -rn 's/\s{2}<center.*nofollow.*?>(.*?)<\/a>.*/\1/p' |
-                zztradutor en-pt $2 )\n"
-            return
-            ;;
-        *)
-            printf "$( curl -s http://developerexcuses.com/ |
-                sed -rn 's/\s{2}<center.*nofollow.*?>(.*?)<\/a>.*/\1/p' )\n"
-            return 
-            ;;
-    esac
+	zzzz -h excuse "$1" && return
+
+	local url='http://programmingexcuses.com/'
+
+	$ZZWWWDUMP "$url" |
+	sed '$d' |
+	zztrim |
+	case $1 in
+	-t | --traduzir ) zztradutor en-pt ;;
+	*) cat - ;;
+	esac
 }
