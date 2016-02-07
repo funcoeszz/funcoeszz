214e587dd53efe909554048f824571100e7b199a
itamarnet <itamarnet@yahoo.com.br>
2016-02-07 09:27:32 -0200

    zzpronuncia: Corrigindo urls e filtros, e troca de WAV por MP3.

--- zz/zzpronuncia.sh
+++ zz/zzpronuncia.sh
@@ -1,12 +1,11 @@
 # ----------------------------------------------------------------------------
-# http://www.m-w.com
 # Fala a pronúncia correta de uma palavra em inglês.
 # Uso: zzpronuncia palavra
 # Ex.: zzpronuncia apple
 #
 # Autor: Thobias Salazar Trevisan, www.thobias.org
 # Desde: 2002-04-10
-# Versão: 3
+# Versão: 4
 # Licença: GPL
 # Requisitos: zzplay
 # ----------------------------------------------------------------------------
@@ -14,11 +13,11 @@ zzpronuncia ()
 {
 	zzzz -h pronuncia "$1" && return
 
-	local wav_file wav_dir wav_url
+	local audio_file audio_dir
 	local palavra=$1
-	local cache=$(zztool cache pronuncia "$palavra.wav")
-	local url='http://www.m-w.com/dictionary'
-	local url2='http://cougar.eb.com/soundc11'
+	local cache=$(zztool cache pronuncia "$palavra.mp3")
+	local url='http://www.merriam-webster.com/dictionary'
+	local url2='http://media.merriam-webster.com/audio/prons/en/us/mp3'
 
 	# Verificação dos parâmetros
 	test -n "$1" || { zztool -e uso pronuncia; return 1; }
@@ -30,33 +29,31 @@ zzpronuncia ()
 		return
 	fi
 
-	# Busca o arquivo WAV na Internet caso não esteja no cache
+	# Busca o arquivo MP3 na Internet caso não esteja no cache
 	if ! test -f "$cache"
 	then
 		# Extrai o nome do arquivo no site do dicionário
-		wav_file=$(
+		audio_file=$(
 			$ZZWWWHTML "$url/$palavra" |
-			sed -n "/.*return au('\([a-z0-9]\{1,\}\)'.*/{s//\1/p;q;}")
+			sed -n "/data-file=\"[^\"]*$palavra[^\"]*\"/{s/.*data-file=\"//;s/\".*//;p;}" |
+			uniq)
 
 		# Ops, não extraiu nada
-		if test -z "$wav_file"
+		if test -z "$audio_file"
 		then
 			zztool erro "$palavra: palavra não encontrada"
 			return 1
 		else
-			wav_file="${wav_file}.wav"
+			audio_file="${audio_file}.mp3"
 		fi
 
-		# O nome da pasta é a primeira letra do arquivo (/a/apple001.wav)
-		# Ou "number" se iniciar com um número (/number/9while01.wav)
-		wav_dir=$(echo $wav_file | cut -c1)
-		echo $wav_dir | grep '[0-9]' >/dev/null && wav_dir='number'
+		# O nome da pasta é a primeira letra do arquivo (/a/apple001.mp3)
+		# Ou "number" se iniciar com um número (/number/9while01.mp3)
+		audio_dir=$(echo $audio_file | cut -c1)
+		echo $audio_dir | grep '[0-9]' >/dev/null && audio_dir='number'
 
 		# Compõe a URL do arquivo e salva-o localmente (cache)
-		wav_url="$url2/$wav_dir/$wav_file"
-		echo "URL: $wav_url"
-		$ZZWWWHTML "$wav_url" > "$cache"
-		echo "Gravado o arquivo '$cache'"
+		$ZZWWWHTML "$url2/$audio_dir/$audio_file" > "$cache"
 	fi
 
 	# Fala que eu te escuto
