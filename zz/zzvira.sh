fb1f4391edf825e489f7b9d253f3916bdfa8a6a2
itamarnet <itamarnet@yahoo.com.br>
2016-02-10 19:05:39 -0200

    zzvira: Opção -E para apenas espelhar, sem inverter o texto.

--- zz/zzvira.sh
+++ zz/zzvira.sh
@@ -2,13 +2,14 @@
 # Vira um texto, de trás pra frente (rev) ou de ponta-cabeça.
 # Ideia original de: http://www.revfad.com/flip.html (valeu @andersonrizada)
 #
-# Uso: zzvira [-X] texto
+# Uso: zzvira [-X|-E] texto
 # Ex.: zzvira Inverte tudo             # odut etrevnI
 #      zzvira -X De pernas pro ar      # ɹɐ oɹd sɐuɹǝd ǝp
+#      zzvira -E De pernas pro ar      # pǝ dǝɹuɐs dɹo ɐɹ
 #
 # Autor: Aurelio Marinho Jargas, www.aurelio.net
 # Desde: 2010-05-24
-# Versão: 2
+# Versão: 3
 # Licença: GPL
 # Requisitos: zzsemacento zzminusculas
 # ----------------------------------------------------------------------------
@@ -22,17 +23,26 @@ zzvira ()
 	then
 		rasteira=1
 		shift
+	elif test "$1" = '-E'
+	then
+		rasteira=2
+		shift
 	fi
 
 	# Dados via STDIN ou argumentos
 	zztool multi_stdin "$@" |
 
 	# Vira o texto de trás pra frente (rev)
-	sed '
+	if test -z "$rasteira" || test "$rasteira" -ne 2
+	then
+		sed '
 		/\n/!G
 		s/\(.\)\(.*\n\)/&\2\1/
 		//D
-		s/.//' |
+		s/.//'
+	else
+		cat -
+	fi |
 
 	if test -n "$rasteira"
 	then
