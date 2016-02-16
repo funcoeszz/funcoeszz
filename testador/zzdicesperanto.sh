4d0470291817e3005811f4e82a8f427ecbcbfbb9
itamarnet <itamarnet@yahoo.com.br>
2016-02-16 05:39:03 -0200

    zzdicesperanto: Incluindo testador.

---
+++ testador/zzdicesperanto.sh
@@ -0,0 +1,60 @@
+$ zzdicesperanto -d pt -p eo função
+funkcio
+ofico
+$
+
+$ zzdicesperanto -p en trabalho
+work
+labour
+job
+business
+employment
+occupation
+working
+accomplishment
+achievement
+feat
+drudgery
+labor
+mechanical work
+ado
+effort
+exercise
+operation
+paper
+task
+toil
+$
+
+$ zzdicesperanto -p de novo
+neu
+frisch
+erneut
+ferner
+jung
+kühl
+neuartig
+neue
+neun
+unberührt
+weiter
+$
+
+$ zzdicesperanto -d de Frieden
+paco
+Paco
+pacakordo
+$
+
+$ zzdicesperanto -p de vitória
+Sieg
+Triumph
+$
+
+$ zzdicesperanto saudade
+saŭdado
+sopiro
+$
+
+$ zzdicesperanto esperança    #→ espero
+$ zzdicesperanto -d en never  #→ neniam
