328cea4ed67da5afe90a5c9375a729693ae21201
itamarnet <itamarnet@yahoo.com.br>
2016-04-05 08:34:33 -0300

    zzchavepgp: Atualizando testador.

--- testador/zzchavepgp.sh
+++ testador/zzchavepgp.sh
@@ -2,7 +2,7 @@ $ zzchavepgp
 Uso: zzchavepgp nome|e-mail
 $ zzchavepgp XXNOTFOUNDXX
    No results found: No keys found
-$ zzchavepgp Guido van Rossum
+$ zzchavepgp Guido van Rossum | sed 's/^ *//'
 Type bits/keyID     Date       User ID
 pub  1024D/6F023F0B 2002-10-16 Guido van Rossum <guido@python.org>
 $
