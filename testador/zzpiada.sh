9bd7e5661aecb387130e2cf0f8a0da0524afe924
itamarnet <itamarnet@yahoo.com.br>
2016-04-05 05:21:09 -0300

    zzpiada: Alterando testador.

--- testador/zzpiada.sh
+++ testador/zzpiada.sh
@@ -1,3 +1,12 @@
+$ zzpiada -h
+
+zzpiada
+http://www.xalexandre.com.br/
+Mostra uma piada diferente cada vez que é chamada.
+Uso: zzpiada
+Ex.: zzpiada
+
+$
 #$ zzpiada | sed 's/[A-ZÁÉÓÚ].*/TEXTO/ ; 3q'
 #TEXTO
 #
