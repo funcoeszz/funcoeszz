fb18324ccb977c9b2c6e54ceb76a6477b0702bec
itamarnet <itamarnet@yahoo.com.br>
2016-04-10 22:31:15 -0300

    zzdicbabylon: Corrigindo testador.

--- testador/zzdicbabylon.sh
+++ testador/zzdicbabylon.sh
@@ -2,5 +2,5 @@ $ zzdicbabylon drum
 v. tocar tambor
 s. tambor, caixa, tamboril
 $ zzdicbabylon
-Uso: zzdicbabylon [idioma] palavra   #idiomas: nl fr de he it pt es 
+Uso: zzdicbabylon [idioma] palavra   #idiomas: nl fr de he it pt es
 $
