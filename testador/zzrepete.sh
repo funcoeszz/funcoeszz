51587059bff2abc7ddee5a1e865b319d33ebcccb
itamarnet <itamarnet@yahoo.com.br>
2016-04-12 19:36:56 -0300

    Função nova: zzrepete - Repete um dado texto na quantidade de vezes solicitada.

---
+++ testador/zzrepete.sh
@@ -0,0 +1,28 @@
+$ zzrepete 
+Uso: zzrepete [-l | -linha] <repetições> <texto>
+$ zzrepete abre alas
+Número inválido para repetições: abre
+$ zzrepete -5 Foo baR
+Número inválido para repetições: -5
+$ zzrepete 7 Foo baR
+Foo baRFoo baRFoo baRFoo baRFoo baRFoo baRFoo baR
+$ zzrepete -l 6 Foo" | " baR "_"
+Foo |  baR _
+Foo |  baR _
+Foo |  baR _
+Foo |  baR _
+Foo |  baR _
+Foo |  baR _
+$ zzrepete 10 "Bar    Foo "
+Bar    Foo Bar    Foo Bar    Foo Bar    Foo Bar    Foo Bar    Foo Bar    Foo Bar    Foo Bar    Foo Bar    Foo 
+$ zzrepete -l 8 Bar     Foo
+Bar Foo
+Bar Foo
+Bar Foo
+Bar Foo
+Bar Foo
+Bar Foo
+Bar Foo
+Bar Foo
+$ zzrepete 50 .
+..................................................
---
+++ zz/zzrepete.sh
@@ -0,0 +1,51 @@
+# ----------------------------------------------------------------------------
+# Repete um dado texto na quantidade de vezes solicitada.
+# Com a opção -l ou --linha cada repetição é uma nova linha.
+#
+# Uso: zzrepete [-l | -linha] <repetições> <texto>
+# Ex.: zzrepete 15 Foo     # FooFooFooFooFooFooFooFooFooFooFooFooFooFooFoo
+#
+# Autor: Itamar <itamarnet (a) yahoo com br>
+# Desde: 2016-04-12
+# Versão: 1
+# Licença: GPL
+# Requisitos: zzseq
+# ----------------------------------------------------------------------------
+zzrepete ()
+{
+	zzzz -h repete "$1" && return
+
+	test -n "$1" || { zztool -e uso repete; return 1; }
+
+	# Definindo variáveis
+	local linha i qtde
+
+	# Uma repetição por linha
+	if test "$1" = "-l" -o "$1" = "--linha"
+	then
+		linha='\n'
+		shift
+	fi
+
+	# Ao menos 2 parâmetros: Número de repetições e o resto o que vai ser repetido
+	test $# -ge 2 || { zztool -e uso repete; return 1; }
+
+	# Se preenche os requesitos, vamos em frente.
+	if zztool testa_numero "$1" && test "$1" -gt 0
+	then
+		qtde="$1"
+		shift
+
+		# É aqui que acontece, o código é auto-explicativo :)
+		for i in $(zzseq $qtde)
+		do
+			printf "$*${linha}"
+		done |
+		zztool nl_eof
+
+	else
+		# Ops! Deu algum erro.
+		zztool erro "Número inválido para repetições: $1"
+		return 1
+	fi
+}
