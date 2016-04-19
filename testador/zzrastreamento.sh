4f8bc8e605f004b1658cbe543070f79758d3d0d3
itamarnet <itamarnet@yahoo.com.br>
2016-04-19 20:33:29 -0300

    zzrastreamento: Deixando o testador mais genérico.

--- testador/zzrastreamento.sh
+++ testador/zzrastreamento.sh
@@ -1,44 +1,8 @@
 $ zzrastreamento notfound
-$ zzrastreamento TE123456789BR
-Data               Local                                        Situação
-18/04/2016 11:09   CEE ITAJAI - Itajai/SC                       Reintegrado
-                   Em tratamento, aguarde.
-15/04/2016 16:39   CEE ITAJAI - Itajai/SC                       Entrega Efetuada
-15/04/2016 09:05   Itajai/SC                                    Saiu para entrega ao destinatário
-14/04/2016 17:46   CEE ITAJAI - Itajai/SC                       Entrega Efetuada
-14/04/2016 08:37   Itajai/SC                                    Saiu para entrega ao destinatário
-31/03/2016 16:43   CDD BELFORD ROXO - Belford Roxo/RJ           Área com distribuição sujeita a prazo diferenciado
-                   Restrição Temporária
-29/01/2016 17:58   CDD BRASILIA-ASA NORTE - Brasilia/DF         Cancelada a última informação
-29/01/2016 17:58   Brasilia/DF                                  Saiu para entrega ao destinatário
-22/12/2015 13:51   CDD BRASILIA-ASA NORTE - Brasilia/DF         Endereço com numeração irregular - Em pesquisa
-                   Objeto sujeito a atraso na entrega
-22/12/2015 13:42   Brasilia/DF                                  Saiu para entrega ao destinatário
-12/12/2015 07:36   CDD SAMAMBAIA - Brasilia/DF                  Endereço incorreto
-                   Objeto em devolução ao remetente
-12/12/2015 07:34   Brasilia/DF                                  Saiu para entrega ao destinatário
-27/11/2015 17:45   CDD SAMAMBAIA - Brasilia/DF                  Cancelada a última informação
-27/11/2015 17:44   Brasilia/DF                                  Saiu para entrega ao destinatário
-17/11/2015 16:09   CDD PALMAS - Palmas/TO                       Cancelada a última informação
-17/11/2015 10:52   Palmas/TO                                    Saiu para entrega ao destinatário
-13/11/2015 15:30   CDD PALMAS - Palmas/TO                       Cancelada a última informação
-13/11/2015 15:14   Palmas/TO                                    Saiu para entrega ao destinatário
-13/11/2015 15:11   CDD PALMAS - Palmas/TO                       Cancelada a última informação
-13/11/2015 10:27   Palmas/TO                                    Saiu para entrega ao destinatário
-13/11/2015 10:26   CDD PALMAS - Palmas/TO                       Cancelada a última informação
-13/11/2015 10:02   Palmas/TO                                    Saiu para entrega ao destinatário
-13/11/2015 09:58   CDD PALMAS - Palmas/TO                       Cancelada a última informação
-13/11/2015 09:55   Palmas/TO                                    Saiu para entrega ao destinatário
-13/11/2015 09:53   CDD PALMAS - Palmas/TO                       Cancelada a última informação
-13/11/2015 09:50   Palmas/TO                                    Saiu para entrega ao destinatário
-12/11/2015 16:14   CDD PALMAS - Palmas/TO                       Entrega não efetuada por motivos operacionais
-                   Entrega prevista para o próximo dia útil
-12/11/2015 15:40   Palmas/TO                                    Saiu para entrega ao destinatário
-10/11/2015 10:02   CDD PALMAS - Palmas/TO                       Entrega não efetuada por motivos operacionais
-                   Entrega prevista para o próximo dia útil
-10/11/2015 09:41   Palmas/TO                                    Saiu para entrega ao destinatário
-10/11/2015 09:40   CDD PALMAS - Palmas/TO                       Cancelada a última informação
-10/11/2015 09:25   Palmas/TO                                    Saiu para entrega ao destinatário
-21/10/2015 18:31   CDD BRASILIA-ASA NORTE - Brasilia/DF         Entrega Efetuada
-21/10/2015 11:47   Brasilia/DF                                  Saiu para entrega ao destinatário
+$ zzrastreamento TE123456789BR | sed -n '/[0-9]:[0-9]/ { s|/[A-Z][A-Z]|/EE|; s/[0-9]/N/g; s|:NN\(.*\)/EE|:NN LOCAL/EE|; s|/EE \(.*\)|/EE EXPLICACAO|; p; }' | sed 's|NN/NN/NN*|DATA|; s/NN:NN/HORA/; 5q'
+DATA HORA LOCAL/EE EXPLICACAO
+DATA HORA LOCAL/EE EXPLICACAO
+DATA HORA LOCAL/EE EXPLICACAO
+DATA HORA LOCAL/EE EXPLICACAO
+DATA HORA LOCAL/EE EXPLICACAO
 $
