3758e3bbd4f89bfa61ff29e6301c315efede5653
itamarnet <itamarnet@yahoo.com.br>
2016-05-24 14:30:30 -0300

    zztestar: Melhorando regex no 'mac'.

--- zz/zztestar.sh
+++ zz/zztestar.sh
@@ -184,9 +184,9 @@ zztestar ()
 		mac)
 			# Testa se $2 tem um formato de MAC válido
 			# O MAC poderá ser nos formatos 00:00:00:00:00:00, 00-00-00-00-00-00 ou 0000.0000.0000
-			echo "$2" | grep '\([0-9A-Fa-f]\{2\}-\)\{5\}[0-9A-Fa-f]\{2\}' >/dev/null && return 0
-			echo "$2" | grep '\([0-9A-Fa-f]\{2\}:\)\{5\}[0-9A-Fa-f]\{2\}' >/dev/null && return 0
-			echo "$2" | grep '\([0-9A-Fa-f]\{4\}\.\)\{2\}[0-9A-Fa-f]' >/dev/null && return 0
+			echo "$2" | egrep '^([0-9A-Fa-f]{2}-){5}[0-9A-Fa-f]{2}$' >/dev/null && return 0
+			echo "$2" | egrep '^([0-9A-Fa-f]{2}:){5}[0-9A-Fa-f]{2}$' >/dev/null && return 0
+			echo "$2" | egrep '^([0-9A-Fa-f]{4}\.){2}[0-9A-Fa-f]{4}$' >/dev/null && return 0
 
 			test -n "$erro" && zztool erro "MAC address inválido '$2'"
 			return 1
