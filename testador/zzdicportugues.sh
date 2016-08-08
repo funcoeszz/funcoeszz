bd6851e5ac48171ab89b8a18f51aa3fda699b0a2
itamarnet <itamarnet@yahoo.com.br>
2016-08-07 21:30:40 -0300

    zzdicportugues: Atualizando testador - parte 2.

--- testador/zzdicportugues.sh
+++ testador/zzdicportugues.sh
@@ -42,10 +42,10 @@ $
 
 $ zzdicportugues maçã
 s.f.Nome do fruto produzido pela macieira: adoro comer maçã!
-[Por Extensão]O que se assemelha a uma maçã, em seu formato, aparência ou gosto; feito com esse fruto: caderno em forma de maçã; vinagre de maçã.
+[Por Extensão] O que se assemelha a uma maçã, em seu formato, aparência ou gosto; feito com esse fruto: caderno em forma de maçã; vinagre de maçã.
 Maçaneta; a parte da frente, mais alta, de uma sela.
-[Zoologia]Bezoar; tipo de pedra carnosa que, encontrada no estômago de certos animais, para algumas pessoas tem propriedade terapêutica.
-[Gramática]Diminutivo irregular: maçanilha.
+[Zoologia] Bezoar; tipo de pedra carnosa que, encontrada no estômago de certos animais, para algumas pessoas tem propriedade terapêutica.
+[Gramática] Diminutivo irregular: maçanilha.
 Maçã do rosto. A parte saliente do rosto que, situada abaixo dos olhos, é formada pelo osso malar.
 (Etm. do latim: matiana; mattiana)
 
@@ -60,8 +60,8 @@ $
 $ zzdicportugues louça
 s.f.Produtos e/ou objetos feitos em cerâmica, barro ou porcelana para servir refeições; esses utensílios: serviu o jantar com a louça antiga.
 Produto de cerâmica esmaltada, usado de diversas maneiras, normalmente na confecção de objetos domésticos: bule de louça.
-[Informal]Os utensílios domésticos usados na cozinha: preciso lavar a louça.
-[Por Extensão]Xícara usada em bares e botequins; o café que se serve nessa xícara.
+[Informal] Os utensílios domésticos usados na cozinha: preciso lavar a louça.
+[Por Extensão] Xícara usada em bares e botequins; o café que se serve nessa xícara.
 Desuso. Penico; objeto ou recipiente usado para urinar ou defecar.
 (Etm. de origem questionável)
 
@@ -70,9 +70,9 @@ $
 
 $ zzdicportugues louçã
 adj.Enfeitado; coberto por adornos, enfeites.
-[Figurado]Belo; repleto de brilho; bonito de se ver: pintura louçã.
-[Pejorativo]Janota; que se veste exageradamente elegante.
-Gíria. Doidice; que se comporta loucamente.
+[Figurado] Belo; repleto de brilho; bonito de se ver: pintura louçã.
+[Pejorativo] Janota; que se veste exageradamente elegante.
+[Gíria] Doidice; que se comporta loucamente.
 (Etm. Fem. de loução)
 
 Louçã é sinônimo de: janota, belo
@@ -97,9 +97,12 @@ v.reg.mult.Expressar-se através das palavras; dizer: falou mentiras; falou ment
 Dizer a verdade; revelar: o réu se recusava a falar ao juri.
 Exercer influência: a honra deve falar mais alto que o interesse.
 Iniciar um assunto; contar alguma coisa: falavam sobre o filme.
-[Figurado]Ser expressivo ou compreensível; demonstrar: as ações falam sozinhas; olhos que falam.
-Expressar-se numa outra língua: fala espanhol com perfeição.v.t.i.Falar mal de; criticar: fala sempre da vizinha.v.pron.Permanecer em contato com alguém: os pais não se falam.s.m.Ato de se expressar, de conversar: não ouço o falar do professor.
-[Linguística]Variante de uma língua que depende de sua região; dialeto: o falar mineiro.
+[Figurado] Ser expressivo ou compreensível; demonstrar: as ações falam sozinhas; olhos que falam.
+Expressar-se numa outra língua: fala espanhol com perfeição.
+v.t.i.Falar mal de; criticar: fala sempre da vizinha.
+v.pron.Permanecer em contato com alguém: os pais não se falam.
+s.m.Ato de se expressar, de conversar: não ouço o falar do professor.
+[Linguística] Variante de uma língua que depende de sua região; dialeto: o falar mineiro.
 (Etm. do latim: fabulare)
 
 Falar é sinônimo de: comunicar, conversar, discursar, exprimir, criticar, conversa, dialeto, dizer, revelar
@@ -111,12 +114,15 @@ $ zzdicportugues comer
 v.t.d.Ingerir algum alimento, levando à boca e engolindo: ele não gosta de comer legumes; adorava comer tortas.
 Roubar ou apropriar-se do não lhe pertence: os impostos comiam meu salário.
 Deixar de ver; ocultar ou omitir: durante a leitura, comia palavras.
-[Figurado]Gastar completamente; consumir: comeu a herança da filha.
-[Figurado]Acreditar muito em: o delegado não comeu sua história.
-[Figurado]Vulgar. Possuir sexualmente outra pessoa.v.pron.Consumir-se por: comia-se de raiva!v.i.Alimentar-se habitualmente: não como em restaurantes.
+[Figurado] Gastar completamente; consumir: comeu a herança da filha.
+[Figurado] Acreditar muito em: o delegado não comeu sua história.
+[Figurado] Vulgar. Possuir sexualmente outra pessoa.
+v.pron.Consumir-se por: comia-se de raiva!
+v.i.Alimentar-se habitualmente: não como em restaurantes.
 Provar pela primeira vez; experimentar: comer da maçã proibida.
-[Figurado]Carcomer, roer, consumir: a ferrugem come o ferro.
-[Figurado]Eliminar ou ganhar pedras em jogo de tabuleiro.s.m.Ação de comer; aquilo que se come ou ingere; alimento: o comer não lhe satisfaz.
+[Figurado] Carcomer, roer, consumir: a ferrugem come o ferro.
+[Figurado] Eliminar ou ganhar pedras em jogo de tabuleiro.
+s.m.Ação de comer; aquilo que se come ou ingere; alimento: o comer não lhe satisfaz.
 (Etm. do latim: comedere)
 
 Comer é sinônimo de: consumir, manducar, ingerir, ocultar, alimentar, roer, carcomer, manjar, papar
