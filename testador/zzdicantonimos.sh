1dc9eeb8dd437a62565768663949784b04e70795
itamarnet <itamarnet@yahoo.com.br>
2016-08-07 23:00:31 -0300

    zzdicantonimos: Listagem ordenade e única de antõnimos.

--- testador/zzdicantonimos.sh
+++ testador/zzdicantonimos.sh
@@ -1,19 +1,13 @@
 # Normal
 
 $ zzdicantonimos alto
-1. anão, diminuto, reduzido, raso, pequeno, baixo.
-2. anão, barato, pequeno, caído, flácido, fraco.
-3. módico, fraco, vulgar, leve, insignificante, modesto.
-4. torpe, sórdido, pior, rasteiro, indigno, acessível, razoável, lento, vil, inferior.
-5. compreensível, cristalino, descomplicado, acessível, razoável.
-6. falda, base, sopé.
+acessível anão baixo barato base caído compreensível cristalino descomplicado diminuto falda flácido fraco indigno inferior insignificante lento leve modesto módico ordinário pequeno pior raso rasteiro razoável reduzido sopé sórdido torpe vil vulgar
 $
 
 # Palavra com acentos
 
 $ zzdicantonimos folgazão
-1. introvertido, sério, infeliz, tristonho.
-2. rancoroso.
+infeliz introvertido rancoroso sério tristonho
 $
 
 # Palavra não encontrada
