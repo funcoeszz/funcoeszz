312ede8637aa501175383166c5484c0a227920a5
itamarnet <itamarnet@yahoo.com.br>
2016-08-20 18:14:00 -0300

    zzchavepgp: Removendo espaço inicial no testador.

--- testador/zzchavepgp.sh
+++ testador/zzchavepgp.sh
@@ -2,10 +2,10 @@ $ zzchavepgp
 Uso: zzchavepgp nome|e-mail
 $ zzchavepgp XXNOTFOUNDXX
    No results found: No keys found
-$ zzchavepgp Guido van Rossum | sed -n '1p; $ {s/^ *//;p;}'
+$ zzchavepgp Guido van Rossum | sed -n '1{s/^ *//;p;}; ${s/^ *//;p;}'
 Type bits/keyID     Date       User ID
 pub  1024D/6F023F0B 2002-10-16 Guido van Rossum <guido@python.org>
-$ zzchavepgp Linus Torvalds | sed -n '1p; $ {s/^ *//;p;}'
+$ zzchavepgp Linus Torvalds | sed -n '1{s/^ *//;p;}; ${s/^ *//;p;}'
 Type bits/keyID     Date       User ID
 pub  1024R/A86B35C5 1996-06-08 Linus Torvalds <Linus.Torvalds@Helsinki.FI>
 $
