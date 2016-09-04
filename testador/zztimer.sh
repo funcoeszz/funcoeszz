8c387c8b717c88a3b3c51adf616b3425b9ef55e8
itamarnet <itamarnet@yahoo.com.br>
2016-09-04 09:19:59 -0300

    zztimer: Alterando código para viabilizar testes e o testador.

---
+++ testador/zztimer.sh
@@ -0,0 +1,178 @@
+# Usar no teste a opção --teste para ter retorno visual.
+$ zztimer -c 4:32	#→ 272
+$ zztimer --teste 15
+00:00:15
+00:00:14
+00:00:13
+00:00:12
+00:00:11
+00:00:10
+00:00:09
+00:00:08
+00:00:07
+00:00:06
+00:00:05
+00:00:04
+00:00:03
+00:00:02
+00:00:01
+00:00:00
+$ zztimer --teste -n 10
+ 0000   0000          0000   0000          111    0000 
+0    0 0    0   #    0    0 0    0   #      11   0    0
+0    0 0    0        0    0 0    0          11   0    0
+0    0 0    0   #    0    0 0    0   #      11   0    0
+ 0000   0000          0000   0000         111111  0000 
+ 0000   0000          0000   0000          0000   9999 
+0    0 0    0   #    0    0 0    0   #    0    0 9    9
+0    0 0    0        0    0 0    0        0    0  99999
+0    0 0    0   #    0    0 0    0   #    0    0      9
+ 0000   0000          0000   0000          0000   9999 
+ 0000   0000          0000   0000          0000   8888 
+0    0 0    0   #    0    0 0    0   #    0    0 8    8
+0    0 0    0        0    0 0    0        0    0  8888 
+0    0 0    0   #    0    0 0    0   #    0    0 8    8
+ 0000   0000          0000   0000          0000   8888 
+ 0000   0000          0000   0000          0000  777777
+0    0 0    0   #    0    0 0    0   #    0    0     7 
+0    0 0    0        0    0 0    0        0    0    7  
+0    0 0    0   #    0    0 0    0   #    0    0   7   
+ 0000   0000          0000   0000          0000   7    
+ 0000   0000          0000   0000          0000   6666 
+0    0 0    0   #    0    0 0    0   #    0    0 6     
+0    0 0    0        0    0 0    0        0    0 66666 
+0    0 0    0   #    0    0 0    0   #    0    0 6    6
+ 0000   0000          0000   0000          0000   6666 
+ 0000   0000          0000   0000          0000  55555 
+0    0 0    0   #    0    0 0    0   #    0    0 5     
+0    0 0    0        0    0 0    0        0    0 55555 
+0    0 0    0   #    0    0 0    0   #    0    0      5
+ 0000   0000          0000   0000          0000   5555 
+ 0000   0000          0000   0000          0000     44 
+0    0 0    0   #    0    0 0    0   #    0    0   4 4 
+0    0 0    0        0    0 0    0        0    0  4  4 
+0    0 0    0   #    0    0 0    0   #    0    0 444444
+ 0000   0000          0000   0000          0000      4 
+ 0000   0000          0000   0000          0000  33333 
+0    0 0    0   #    0    0 0    0   #    0    0      3
+0    0 0    0        0    0 0    0        0    0  3333 
+0    0 0    0   #    0    0 0    0   #    0    0      3
+ 0000   0000          0000   0000          0000  33333 
+ 0000   0000          0000   0000          0000   2222 
+0    0 0    0   #    0    0 0    0   #    0    0     22
+0    0 0    0        0    0 0    0        0    0    22 
+0    0 0    0   #    0    0 0    0   #    0    0  22   
+ 0000   0000          0000   0000          0000  222222
+ 0000   0000          0000   0000          0000   111  
+0    0 0    0   #    0    0 0    0   #    0    0   11  
+0    0 0    0        0    0 0    0        0    0   11  
+0    0 0    0   #    0    0 0    0   #    0    0   11  
+ 0000   0000          0000   0000          0000  111111
+ 0000   0000          0000   0000          0000   0000 
+0    0 0    0   #    0    0 0    0   #    0    0 0    0
+0    0 0    0        0    0 0    0        0    0 0    0
+0    0 0    0   #    0    0 0    0   #    0    0 0    0
+ 0000   0000          0000   0000          0000   0000 
+$ zztimer --teste -x Z 9
+ ZZZZ   ZZZZ          ZZZZ   ZZZZ          ZZZZ   ZZZZ 
+Z    Z Z    Z   Z    Z    Z Z    Z   Z    Z    Z Z    Z
+Z    Z Z    Z        Z    Z Z    Z        Z    Z  ZZZZZ
+Z    Z Z    Z   Z    Z    Z Z    Z   Z    Z    Z      Z
+ ZZZZ   ZZZZ          ZZZZ   ZZZZ          ZZZZ   ZZZZ 
+ ZZZZ   ZZZZ          ZZZZ   ZZZZ          ZZZZ   ZZZZ 
+Z    Z Z    Z   Z    Z    Z Z    Z   Z    Z    Z Z    Z
+Z    Z Z    Z        Z    Z Z    Z        Z    Z  ZZZZ 
+Z    Z Z    Z   Z    Z    Z Z    Z   Z    Z    Z Z    Z
+ ZZZZ   ZZZZ          ZZZZ   ZZZZ          ZZZZ   ZZZZ 
+ ZZZZ   ZZZZ          ZZZZ   ZZZZ          ZZZZ  ZZZZZZ
+Z    Z Z    Z   Z    Z    Z Z    Z   Z    Z    Z     Z 
+Z    Z Z    Z        Z    Z Z    Z        Z    Z    Z  
+Z    Z Z    Z   Z    Z    Z Z    Z   Z    Z    Z   Z   
+ ZZZZ   ZZZZ          ZZZZ   ZZZZ          ZZZZ   Z    
+ ZZZZ   ZZZZ          ZZZZ   ZZZZ          ZZZZ   ZZZZ 
+Z    Z Z    Z   Z    Z    Z Z    Z   Z    Z    Z Z     
+Z    Z Z    Z        Z    Z Z    Z        Z    Z ZZZZZ 
+Z    Z Z    Z   Z    Z    Z Z    Z   Z    Z    Z Z    Z
+ ZZZZ   ZZZZ          ZZZZ   ZZZZ          ZZZZ   ZZZZ 
+ ZZZZ   ZZZZ          ZZZZ   ZZZZ          ZZZZ  ZZZZZ 
+Z    Z Z    Z   Z    Z    Z Z    Z   Z    Z    Z Z     
+Z    Z Z    Z        Z    Z Z    Z        Z    Z ZZZZZ 
+Z    Z Z    Z   Z    Z    Z Z    Z   Z    Z    Z      Z
+ ZZZZ   ZZZZ          ZZZZ   ZZZZ          ZZZZ   ZZZZ 
+ ZZZZ   ZZZZ          ZZZZ   ZZZZ          ZZZZ     ZZ 
+Z    Z Z    Z   Z    Z    Z Z    Z   Z    Z    Z   Z Z 
+Z    Z Z    Z        Z    Z Z    Z        Z    Z  Z  Z 
+Z    Z Z    Z   Z    Z    Z Z    Z   Z    Z    Z ZZZZZZ
+ ZZZZ   ZZZZ          ZZZZ   ZZZZ          ZZZZ      Z 
+ ZZZZ   ZZZZ          ZZZZ   ZZZZ          ZZZZ  ZZZZZ 
+Z    Z Z    Z   Z    Z    Z Z    Z   Z    Z    Z      Z
+Z    Z Z    Z        Z    Z Z    Z        Z    Z  ZZZZ 
+Z    Z Z    Z   Z    Z    Z Z    Z   Z    Z    Z      Z
+ ZZZZ   ZZZZ          ZZZZ   ZZZZ          ZZZZ  ZZZZZ 
+ ZZZZ   ZZZZ          ZZZZ   ZZZZ          ZZZZ   ZZZZ 
+Z    Z Z    Z   Z    Z    Z Z    Z   Z    Z    Z     ZZ
+Z    Z Z    Z        Z    Z Z    Z        Z    Z    ZZ 
+Z    Z Z    Z   Z    Z    Z Z    Z   Z    Z    Z  ZZ   
+ ZZZZ   ZZZZ          ZZZZ   ZZZZ          ZZZZ  ZZZZZZ
+ ZZZZ   ZZZZ          ZZZZ   ZZZZ          ZZZZ   ZZZ  
+Z    Z Z    Z   Z    Z    Z Z    Z   Z    Z    Z   ZZ  
+Z    Z Z    Z        Z    Z Z    Z        Z    Z   ZZ  
+Z    Z Z    Z   Z    Z    Z Z    Z   Z    Z    Z   ZZ  
+ ZZZZ   ZZZZ          ZZZZ   ZZZZ          ZZZZ  ZZZZZZ
+ ZZZZ   ZZZZ          ZZZZ   ZZZZ          ZZZZ   ZZZZ 
+Z    Z Z    Z   Z    Z    Z Z    Z   Z    Z    Z Z    Z
+Z    Z Z    Z        Z    Z Z    Z        Z    Z Z    Z
+Z    Z Z    Z   Z    Z    Z Z    Z   Z    Z    Z Z    Z
+ ZZZZ   ZZZZ          ZZZZ   ZZZZ          ZZZZ   ZZZZ 
+$ zztimer --teste -y 0123456789# OABCDEFGHIJ 9
+ OOOO   OOOO          OOOO   OOOO          OOOO   IIII 
+O    O O    O   J    O    O O    O   J    O    O I    I
+O    O O    O        O    O O    O        O    O  IIIII
+O    O O    O   J    O    O O    O   J    O    O      I
+ OOOO   OOOO          OOOO   OOOO          OOOO   IIII 
+ OOOO   OOOO          OOOO   OOOO          OOOO   HHHH 
+O    O O    O   J    O    O O    O   J    O    O H    H
+O    O O    O        O    O O    O        O    O  HHHH 
+O    O O    O   J    O    O O    O   J    O    O H    H
+ OOOO   OOOO          OOOO   OOOO          OOOO   HHHH 
+ OOOO   OOOO          OOOO   OOOO          OOOO  GGGGGG
+O    O O    O   J    O    O O    O   J    O    O     G 
+O    O O    O        O    O O    O        O    O    G  
+O    O O    O   J    O    O O    O   J    O    O   G   
+ OOOO   OOOO          OOOO   OOOO          OOOO   G    
+ OOOO   OOOO          OOOO   OOOO          OOOO   FFFF 
+O    O O    O   J    O    O O    O   J    O    O F     
+O    O O    O        O    O O    O        O    O FFFFF 
+O    O O    O   J    O    O O    O   J    O    O F    F
+ OOOO   OOOO          OOOO   OOOO          OOOO   FFFF 
+ OOOO   OOOO          OOOO   OOOO          OOOO  EEEEE 
+O    O O    O   J    O    O O    O   J    O    O E     
+O    O O    O        O    O O    O        O    O EEEEE 
+O    O O    O   J    O    O O    O   J    O    O      E
+ OOOO   OOOO          OOOO   OOOO          OOOO   EEEE 
+ OOOO   OOOO          OOOO   OOOO          OOOO     DD 
+O    O O    O   J    O    O O    O   J    O    O   D D 
+O    O O    O        O    O O    O        O    O  D  D 
+O    O O    O   J    O    O O    O   J    O    O DDDDDD
+ OOOO   OOOO          OOOO   OOOO          OOOO      D 
+ OOOO   OOOO          OOOO   OOOO          OOOO  CCCCC 
+O    O O    O   J    O    O O    O   J    O    O      C
+O    O O    O        O    O O    O        O    O  CCCC 
+O    O O    O   J    O    O O    O   J    O    O      C
+ OOOO   OOOO          OOOO   OOOO          OOOO  CCCCC 
+ OOOO   OOOO          OOOO   OOOO          OOOO   BBBB 
+O    O O    O   J    O    O O    O   J    O    O     BB
+O    O O    O        O    O O    O        O    O    BB 
+O    O O    O   J    O    O O    O   J    O    O  BB   
+ OOOO   OOOO          OOOO   OOOO          OOOO  BBBBBB
+ OOOO   OOOO          OOOO   OOOO          OOOO   AAA  
+O    O O    O   J    O    O O    O   J    O    O   AA  
+O    O O    O        O    O O    O        O    O   AA  
+O    O O    O   J    O    O O    O   J    O    O   AA  
+ OOOO   OOOO          OOOO   OOOO          OOOO  AAAAAA
+ OOOO   OOOO          OOOO   OOOO          OOOO   OOOO 
+O    O O    O   J    O    O O    O   J    O    O O    O
+O    O O    O        O    O O    O        O    O O    O
+O    O O    O   J    O    O O    O   J    O    O O    O
+ OOOO   OOOO          OOOO   OOOO          OOOO   OOOO 
+$
