db5e446a072fe4002c2745a476dbea38e56fc1e8
itamarnet <itamarnet@yahoo.com.br>
2016-09-11 13:09:44 -0300

    zzcinemais e zzcinemark: Novos testadores.

---
+++ testador/zzcinemais.sh
@@ -0,0 +1,71 @@
+$ zzcinemais 
+9 - Uberaba  - MG
+11 - Patos de Minas - MG
+21 - Guaratinguetá - SP
+32 - Anápolis - GO
+34 - Montes Claros - MG
+35 - Shop. Alameda - Juiz de Fora - MG
+36 - Ituiutaba - MG
+37 - Araxá - MG
+38 - Lorena - SP
+39 - Jardim Norte - Juiz de Fora - MG
+$ zzcinemais 9 | sed '/ - [A-Z][A-Z]$/s/.*/CIDADE/;/\/[0-9]\{4\}$/s/.*/DATA/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/_/;/[^A-Z_][^A-Z]/{s/.*/FILME/;}' | sort | uniq
+_
+CIDADE
+DATA
+FILME
+HORARIO
+$ zzcinemais 11 | sed '/ - [A-Z][A-Z]$/s/.*/CIDADE/;/\/[0-9]\{4\}$/s/.*/DATA/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/_/;/[^A-Z_][^A-Z]/{s/.*/FILME/;}' | sort | uniq
+_
+CIDADE
+DATA
+FILME
+HORARIO
+$ zzcinemais 21 | sed '/ - [A-Z][A-Z]$/s/.*/CIDADE/;/\/[0-9]\{4\}$/s/.*/DATA/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/_/;/[^A-Z_][^A-Z]/{s/.*/FILME/;}' | sort | uniq
+_
+CIDADE
+DATA
+FILME
+HORARIO
+$ zzcinemais 32 | sed '/ - [A-Z][A-Z]$/s/.*/CIDADE/;/\/[0-9]\{4\}$/s/.*/DATA/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/_/;/[^A-Z_][^A-Z]/{s/.*/FILME/;}' | sort | uniq
+_
+CIDADE
+DATA
+FILME
+HORARIO
+$ zzcinemais 34 | sed '/ - [A-Z][A-Z]$/s/.*/CIDADE/;/\/[0-9]\{4\}$/s/.*/DATA/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/_/;/[^A-Z_][^A-Z]/{s/.*/FILME/;}' | sort | uniq
+_
+CIDADE
+DATA
+FILME
+HORARIO
+$ zzcinemais 35 | sed '/ - [A-Z][A-Z]$/s/.*/CIDADE/;/\/[0-9]\{4\}$/s/.*/DATA/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/_/;/[^A-Z_][^A-Z]/{s/.*/FILME/;}' | sort | uniq
+_
+CIDADE
+DATA
+FILME
+HORARIO
+$ zzcinemais 36 | sed '/ - [A-Z][A-Z]$/s/.*/CIDADE/;/\/[0-9]\{4\}$/s/.*/DATA/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/_/;/[^A-Z_][^A-Z]/{s/.*/FILME/;}' | sort | uniq
+_
+CIDADE
+DATA
+FILME
+HORARIO
+$ zzcinemais 37 | sed '/ - [A-Z][A-Z]$/s/.*/CIDADE/;/\/[0-9]\{4\}$/s/.*/DATA/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/_/;/[^A-Z_][^A-Z]/{s/.*/FILME/;}' | sort | uniq
+_
+CIDADE
+DATA
+FILME
+HORARIO
+$ zzcinemais 38 | sed '/ - [A-Z][A-Z]$/s/.*/CIDADE/;/\/[0-9]\{4\}$/s/.*/DATA/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/_/;/[^A-Z_][^A-Z]/{s/.*/FILME/;}' | sort | uniq
+_
+CIDADE
+DATA
+FILME
+HORARIO
+$ zzcinemais 39 | sed '/ - [A-Z][A-Z]$/s/.*/CIDADE/;/\/[0-9]\{4\}$/s/.*/DATA/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/_/;/[^A-Z_][^A-Z]/{s/.*/FILME/;}' | sort | uniq
+_
+CIDADE
+DATA
+FILME
+HORARIO
