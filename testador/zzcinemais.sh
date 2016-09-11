3af0b8d943a0e3ee039c61695cab6df9f878818d
itamarnet <itamarnet@yahoo.com.br>
2016-09-11 15:12:39 -0300

    zzcinemais, zzcinemark e zzcinepolis: Ajustando sintaxe do 'sed' testadores.

--- testador/zzcinemais.sh
+++ testador/zzcinemais.sh
@@ -9,62 +9,62 @@ $ zzcinemais
 37 - Araxá - MG
 38 - Lorena - SP
 39 - Jardim Norte - Juiz de Fora - MG
-$ zzcinemais 9 | sed '/ - [A-Z][A-Z]$/s/.*/CIDADE/;/\/[0-9]\{4\}$/s/.*/DATA/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/_/;/[^A-Z_][^A-Z]/{s/.*/FILME/;}' | sort | uniq
-_
+$ zzcinemais 9 | sed '/ - [A-Z][A-Z]$/s/.*/CIDADE/;/\/[0-9]\{4\}$/s/.*/DATA/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/AA/;/[^A-Z[^A-Z]/{s/.*/FILME/;}' | sort | uniq
+AA
 CIDADE
 DATA
 FILME
 HORARIO
-$ zzcinemais 11 | sed '/ - [A-Z][A-Z]$/s/.*/CIDADE/;/\/[0-9]\{4\}$/s/.*/DATA/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/_/;/[^A-Z_][^A-Z]/{s/.*/FILME/;}' | sort | uniq
-_
+$ zzcinemais 11 | sed '/ - [A-Z][A-Z]$/s/.*/CIDADE/;/\/[0-9]\{4\}$/s/.*/DATA/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/AA/;/[^A-Z[^A-Z]/{s/.*/FILME/;}' | sort | uniq
+AA
 CIDADE
 DATA
 FILME
 HORARIO
-$ zzcinemais 21 | sed '/ - [A-Z][A-Z]$/s/.*/CIDADE/;/\/[0-9]\{4\}$/s/.*/DATA/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/_/;/[^A-Z_][^A-Z]/{s/.*/FILME/;}' | sort | uniq
-_
+$ zzcinemais 21 | sed '/ - [A-Z][A-Z]$/s/.*/CIDADE/;/\/[0-9]\{4\}$/s/.*/DATA/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/AA/;/[^A-Z[^A-Z]/{s/.*/FILME/;}' | sort | uniq
+AA
 CIDADE
 DATA
 FILME
 HORARIO
-$ zzcinemais 32 | sed '/ - [A-Z][A-Z]$/s/.*/CIDADE/;/\/[0-9]\{4\}$/s/.*/DATA/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/_/;/[^A-Z_][^A-Z]/{s/.*/FILME/;}' | sort | uniq
-_
+$ zzcinemais 32 | sed '/ - [A-Z][A-Z]$/s/.*/CIDADE/;/\/[0-9]\{4\}$/s/.*/DATA/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/AA/;/[^A-Z[^A-Z]/{s/.*/FILME/;}' | sort | uniq
+AA
 CIDADE
 DATA
 FILME
 HORARIO
-$ zzcinemais 34 | sed '/ - [A-Z][A-Z]$/s/.*/CIDADE/;/\/[0-9]\{4\}$/s/.*/DATA/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/_/;/[^A-Z_][^A-Z]/{s/.*/FILME/;}' | sort | uniq
-_
+$ zzcinemais 34 | sed '/ - [A-Z][A-Z]$/s/.*/CIDADE/;/\/[0-9]\{4\}$/s/.*/DATA/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/AA/;/[^A-Z[^A-Z]/{s/.*/FILME/;}' | sort | uniq
+AA
 CIDADE
 DATA
 FILME
 HORARIO
-$ zzcinemais 35 | sed '/ - [A-Z][A-Z]$/s/.*/CIDADE/;/\/[0-9]\{4\}$/s/.*/DATA/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/_/;/[^A-Z_][^A-Z]/{s/.*/FILME/;}' | sort | uniq
-_
+$ zzcinemais 35 | sed '/ - [A-Z][A-Z]$/s/.*/CIDADE/;/\/[0-9]\{4\}$/s/.*/DATA/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/AA/;/[^A-Z[^A-Z]/{s/.*/FILME/;}' | sort | uniq
+AA
 CIDADE
 DATA
 FILME
 HORARIO
-$ zzcinemais 36 | sed '/ - [A-Z][A-Z]$/s/.*/CIDADE/;/\/[0-9]\{4\}$/s/.*/DATA/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/_/;/[^A-Z_][^A-Z]/{s/.*/FILME/;}' | sort | uniq
-_
+$ zzcinemais 36 | sed '/ - [A-Z][A-Z]$/s/.*/CIDADE/;/\/[0-9]\{4\}$/s/.*/DATA/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/AA/;/[^A-Z[^A-Z]/{s/.*/FILME/;}' | sort | uniq
+AA
 CIDADE
 DATA
 FILME
 HORARIO
-$ zzcinemais 37 | sed '/ - [A-Z][A-Z]$/s/.*/CIDADE/;/\/[0-9]\{4\}$/s/.*/DATA/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/_/;/[^A-Z_][^A-Z]/{s/.*/FILME/;}' | sort | uniq
-_
+$ zzcinemais 37 | sed '/ - [A-Z][A-Z]$/s/.*/CIDADE/;/\/[0-9]\{4\}$/s/.*/DATA/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/AA/;/[^A-Z[^A-Z]/{s/.*/FILME/;}' | sort | uniq
+AA
 CIDADE
 DATA
 FILME
 HORARIO
-$ zzcinemais 38 | sed '/ - [A-Z][A-Z]$/s/.*/CIDADE/;/\/[0-9]\{4\}$/s/.*/DATA/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/_/;/[^A-Z_][^A-Z]/{s/.*/FILME/;}' | sort | uniq
-_
+$ zzcinemais 38 | sed '/ - [A-Z][A-Z]$/s/.*/CIDADE/;/\/[0-9]\{4\}$/s/.*/DATA/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/AA/;/[^A-Z[^A-Z]/{s/.*/FILME/;}' | sort | uniq
+AA
 CIDADE
 DATA
 FILME
 HORARIO
-$ zzcinemais 39 | sed '/ - [A-Z][A-Z]$/s/.*/CIDADE/;/\/[0-9]\{4\}$/s/.*/DATA/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/_/;/[^A-Z_][^A-Z]/{s/.*/FILME/;}' | sort | uniq
-_
+$ zzcinemais 39 | sed '/ - [A-Z][A-Z]$/s/.*/CIDADE/;/\/[0-9]\{4\}$/s/.*/DATA/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/AA/;/[^A-Z[^A-Z]/{s/.*/FILME/;}' | sort | uniq
+AA
 CIDADE
 DATA
 FILME
--- testador/zzcinemark.sh
+++ testador/zzcinemark.sh
@@ -1,75 +1,75 @@
-$ zzcinemark 687 | sed '1s/.*/CINEMA/;/^Dia: [0-3][0-9]-[0-1][0-9]-[0-9]\{4\}$/s/.*/DATA/;/  \(Livre\|[0-1][0-9] Anos\)$/s/.*/DETALHE/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/_/;/[^A-Z_][^A-Z]/{s/.*/FILME/;}' | sort | uniq
-_
+$ zzcinemark 687 | sed '1s/.*/CINEMA/;/^Dia: [0-3][0-9]-[0-1][0-9]-[0-9]\{4\}$/s/.*/DATA/;/ \(Livre\|[0-1][0-9] Anos\)$/s/.*/DETALHE/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/AA/;/[^A-Z[^A-Z]/{s/.*/FILME/;}' | sort | uniq
+AA
 CINEMA
 DATA
 DETALHE
 FILME
 HORARIO
-$ zzcinemark 713 | sed '1s/.*/CINEMA/;/^Dia: [0-3][0-9]-[0-1][0-9]-[0-9]\{4\}$/s/.*/DATA/;/  \(Livre\|[0-1][0-9] Anos\)$/s/.*/DETALHE/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/_/;/[^A-Z_][^A-Z]/{s/.*/FILME/;}' | sort | uniq
-_
+$ zzcinemark 713 | sed '1s/.*/CINEMA/;/^Dia: [0-3][0-9]-[0-1][0-9]-[0-9]\{4\}$/s/.*/DATA/;/ \(Livre\|[0-1][0-9] Anos\)$/s/.*/DETALHE/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/AA/;/[^A-Z[^A-Z]/{s/.*/FILME/;}' | sort | uniq
+AA
 CINEMA
 DATA
 DETALHE
 FILME
 HORARIO
-$ zzcinemark 709 | sed '1s/.*/CINEMA/;/^Dia: [0-3][0-9]-[0-1][0-9]-[0-9]\{4\}$/s/.*/DATA/;/  \(Livre\|[0-1][0-9] Anos\)$/s/.*/DETALHE/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/_/;/[^A-Z_][^A-Z]/{s/.*/FILME/;}' | sort | uniq
-_
+$ zzcinemark 709 | sed '1s/.*/CINEMA/;/^Dia: [0-3][0-9]-[0-1][0-9]-[0-9]\{4\}$/s/.*/DATA/;/ \(Livre\|[0-1][0-9] Anos\)$/s/.*/DETALHE/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/AA/;/[^A-Z[^A-Z]/{s/.*/FILME/;}' | sort | uniq
+AA
 CINEMA
 DATA
 DETALHE
 FILME
 HORARIO
-$ zzcinemark 687 | sed '1s/.*/CINEMA/;/^Dia: [0-3][0-9]-[0-1][0-9]-[0-9]\{4\}$/s/.*/DATA/;/  \(Livre\|[0-1][0-9] Anos\)$/s/.*/DETALHE/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/_/;/[^A-Z_][^A-Z]/{s/.*/FILME/;}' | sort | uniq
-_
+$ zzcinemark 687 | sed '1s/.*/CINEMA/;/^Dia: [0-3][0-9]-[0-1][0-9]-[0-9]\{4\}$/s/.*/DATA/;/ \(Livre\|[0-1][0-9] Anos\)$/s/.*/DETALHE/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/AA/;/[^A-Z[^A-Z]/{s/.*/FILME/;}' | sort | uniq
+AA
 CINEMA
 DATA
 DETALHE
 FILME
 HORARIO
-$ zzcinemark 2118 | sed '1s/.*/CINEMA/;/^Dia: [0-3][0-9]-[0-1][0-9]-[0-9]\{4\}$/s/.*/DATA/;/  \(Livre\|[0-1][0-9] Anos\)$/s/.*/DETALHE/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/_/;/[^A-Z_][^A-Z]/{s/.*/FILME/;}' | sort | uniq
-_
+$ zzcinemark 2118 | sed '1s/.*/CINEMA/;/^Dia: [0-3][0-9]-[0-1][0-9]-[0-9]\{4\}$/s/.*/DATA/;/ \(Livre\|[0-1][0-9] Anos\)$/s/.*/DETALHE/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/AA/;/[^A-Z[^A-Z]/{s/.*/FILME/;}' | sort | uniq
+AA
 CINEMA
 DATA
 DETALHE
 FILME
 HORARIO
-$ zzcinemark 768 | sed '1s/.*/CINEMA/;/^Dia: [0-3][0-9]-[0-1][0-9]-[0-9]\{4\}$/s/.*/DATA/;/  \(Livre\|[0-1][0-9] Anos\)$/s/.*/DETALHE/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/_/;/[^A-Z_][^A-Z]/{s/.*/FILME/;}' | sort | uniq
-_
+$ zzcinemark 768 | sed '1s/.*/CINEMA/;/^Dia: [0-3][0-9]-[0-1][0-9]-[0-9]\{4\}$/s/.*/DATA/;/ \(Livre\|[0-1][0-9] Anos\)$/s/.*/DETALHE/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/AA/;/[^A-Z[^A-Z]/{s/.*/FILME/;}' | sort | uniq
+AA
 CINEMA
 DATA
 DETALHE
 FILME
 HORARIO
-$ zzcinemark 697 | sed '1s/.*/CINEMA/;/^Dia: [0-3][0-9]-[0-1][0-9]-[0-9]\{4\}$/s/.*/DATA/;/  \(Livre\|[0-1][0-9] Anos\)$/s/.*/DETALHE/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/_/;/[^A-Z_][^A-Z]/{s/.*/FILME/;}' | sort | uniq
-_
+$ zzcinemark 697 | sed '1s/.*/CINEMA/;/^Dia: [0-3][0-9]-[0-1][0-9]-[0-9]\{4\}$/s/.*/DATA/;/ \(Livre\|[0-1][0-9] Anos\)$/s/.*/DETALHE/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/AA/;/[^A-Z[^A-Z]/{s/.*/FILME/;}' | sort | uniq
+AA
 CINEMA
 DATA
 DETALHE
 FILME
 HORARIO
-$ zzcinemark 703 | sed '1s/.*/CINEMA/;/^Dia: [0-3][0-9]-[0-1][0-9]-[0-9]\{4\}$/s/.*/DATA/;/  \(Livre\|[0-1][0-9] Anos\)$/s/.*/DETALHE/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/_/;/[^A-Z_][^A-Z]/{s/.*/FILME/;}' | sort | uniq
-_
+$ zzcinemark 703 | sed '1s/.*/CINEMA/;/^Dia: [0-3][0-9]-[0-1][0-9]-[0-9]\{4\}$/s/.*/DATA/;/ \(Livre\|[0-1][0-9] Anos\)$/s/.*/DETALHE/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/AA/;/[^A-Z[^A-Z]/{s/.*/FILME/;}' | sort | uniq
+AA
 CINEMA
 DATA
 DETALHE
 FILME
 HORARIO
-$ zzcinemark 2108 | sed '1s/.*/CINEMA/;/^Dia: [0-3][0-9]-[0-1][0-9]-[0-9]\{4\}$/s/.*/DATA/;/  \(Livre\|[0-1][0-9] Anos\)$/s/.*/DETALHE/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/_/;/[^A-Z_][^A-Z]/{s/.*/FILME/;}' | sort | uniq
-_
+$ zzcinemark 2108 | sed '1s/.*/CINEMA/;/^Dia: [0-3][0-9]-[0-1][0-9]-[0-9]\{4\}$/s/.*/DATA/;/ \(Livre\|[0-1][0-9] Anos\)$/s/.*/DETALHE/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/AA/;/[^A-Z[^A-Z]/{s/.*/FILME/;}' | sort | uniq
+AA
 CINEMA
 DATA
 DETALHE
 FILME
 HORARIO
-$ zzcinemark 2117 | sed '1s/.*/CINEMA/;/^Dia: [0-3][0-9]-[0-1][0-9]-[0-9]\{4\}$/s/.*/DATA/;/  \(Livre\|[0-1][0-9] Anos\)$/s/.*/DETALHE/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/_/;/[^A-Z_][^A-Z]/{s/.*/FILME/;}' | sort | uniq
-_
+$ zzcinemark 2117 | sed '1s/.*/CINEMA/;/^Dia: [0-3][0-9]-[0-1][0-9]-[0-9]\{4\}$/s/.*/DATA/;/ \(Livre\|[0-1][0-9] Anos\)$/s/.*/DETALHE/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/AA/;/[^A-Z[^A-Z]/{s/.*/FILME/;}' | sort | uniq
+AA
 CINEMA
 DATA
 DETALHE
 FILME
 HORARIO
-$ zzcinemark 2127 | sed '1s/.*/CINEMA/;/^Dia: [0-3][0-9]-[0-1][0-9]-[0-9]\{4\}$/s/.*/DATA/;/  \(Livre\|[0-1][0-9] Anos\)$/s/.*/DETALHE/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/_/;/[^A-Z_][^A-Z]/{s/.*/FILME/;}' | sort | uniq
-_
+$ zzcinemark 2127 | sed '1s/.*/CINEMA/;/^Dia: [0-3][0-9]-[0-1][0-9]-[0-9]\{4\}$/s/.*/DATA/;/ \(Livre\|[0-1][0-9] Anos\)$/s/.*/DETALHE/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/AA/;/[^A-Z[^A-Z]/{s/.*/FILME/;}' | sort | uniq
+AA
 CINEMA
 DATA
 DETALHE
