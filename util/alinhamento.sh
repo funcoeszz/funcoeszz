bf28b04fc72975b4b8714840598f4b484b579042
itamarnet <itamarnet@yahoo.com.br>
2016-09-24 09:20:23 -0300

    alinhamento.sh: retirando função inexistente de exceção, e garantindo funcionamento caso a 'funcoeszz' não esteja no PATH.

--- util/alinhamento.sh
+++ util/alinhamento.sh
@@ -10,18 +10,18 @@ cd $(dirname "$0") || exit 1
 
 cd ../zz
 
-funcoeszz tool eco "Linha que inicia com um espaço"
+../funcoeszz tool eco "Linha que inicia com um espaço"
 grep '^ ' * |
-	grep -v -E '^zz(google|palpite|loteria2)'  # caso válido, sed multilinha
+	grep -v -E '^zz(google|palpite)'  # caso válido, sed multilinha
 
-funcoeszz tool eco "Linha com Tab e espaço misturados"
+../funcoeszz tool eco "Linha com Tab e espaço misturados"
 grep '	 ' * |
 	# [\t ]: Dentro de colchetes, é regex
 	fgrep -v '[	 ]'
 
-funcoeszz tool eco "Linha com Tabs ou espaços inúteis no final"
+../funcoeszz tool eco "Linha com Tabs ou espaços inúteis no final"
 grep '[^ 	][ 	]\{1,\}$' * |
 	grep -v '^zzxml.sh:.*Foo $'  # exceção, usado num comentário
 
-funcoeszz tool eco "Linhas vazias, mas com brancos"
+../funcoeszz tool eco "Linhas vazias, mas com brancos"
 egrep '^[	 ]+$' *
