1ad28bdb3b9633e36fdf0304d1e446f8eb375390
itamarnet <itamarnet@yahoo.com.br>
2016-10-27 20:51:24 -0200

    zzdicantonimos: Atualizando testador.

--- testador/zzdicantonimos.sh
+++ testador/zzdicantonimos.sh
@@ -1,7 +1,7 @@
 # Normal
 
 $ zzdicantonimos alto
-acessível anão baixo barato base caído compreensível cristalino descomplicado diminuto falda flácido fraco indigno inferior insignificante lento leve modesto módico ordinário pequeno pior raso rasteiro razoável reduzido sopé sórdido torpe vil vulgar
+acessível anão baixo base caído compreensível cristalino descomplicado diminuto falda flácido fraco indigno inferior insignificante leve modesto módico pequeno pior raso rasteiro reduzido sopé sórdido torpe vil
 $
 
 # Palavra com acentos
