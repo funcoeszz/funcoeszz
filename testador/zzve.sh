1e8abcd47422e370bea187bf8886007026f600e1
itamarnet <itamarnet@yahoo.com.br>
2016-11-14 22:00:31 -0200

    zzve: Ajustando filtro no testador para valor negativo inesperado.

--- testador/zzve.sh
+++ testador/zzve.sh
@@ -19,7 +19,7 @@ $ zzve custo | awk '/ ao (ano|mês)/ { sub(/.*ao (ano|mês)\)* */,""); print NF 
 $ zzve aplicacoes | sed -n '/[0-9],[0-9]/{/para o mês de/d;s/ *IBrX 50 *//;s/.*[a-zA-Z)] *//g;p;}' | awk '{ print NF }' | uniq	#→ 8
 $ zzve ima  | awk '/I[RM].-/ { gsub(/\*/,""); print NF }' | uniq	#→ 6
 $ zzve mercado | awk '/[0-9],[0-9]/ { sub(/.*[)*] */,""); print NF }' | uniq	#→ 6
-$ zzve insper | sed -n '/[0-9],/{s/.*dias//;s/^[^0-9]*//;s/[0-9.]*,[0-9]*/9,9/g;s/  */ /g;s/ $//;p;}'
+$ zzve insper | sed -n '/[0-9],/{s/.*dias//;s/^[^0-9]*//;s/[-0-9.]*,[0-9]*/9,9/g;s/  */ /g;s/ $//;p;}'
 9,9 9,9 9,9 9,9 9,9
 9,9% 9,9% 9,9% 9,9% 9,9%
 9,9% 9,9% 9,9% 9,9% 9,9%
