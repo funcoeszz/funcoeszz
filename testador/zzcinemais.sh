e63ad5bf485b85124c728f3a234987de8e842dc7
Aurelio Jargas <verde@aurelio.net>
2016-11-17 17:01:10 -0200

    zzcinemais: testes melhorados, trata código inválido
    Veja #397

--- testador/zzcinemais.sh
+++ testador/zzcinemais.sh
@@ -1,8 +1,16 @@
-# Preparação filtro
-$ filtro='/ - [A-Z][A-Z]$/s/.*/CIDADE/;/\/[0-9]\{4\}$/s/.*/DATA/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/AA/;/[^A-Z][^A-Z]/{s/.*/FILME/;}'
+# Uso incorreto
+
+$ zzcinemais foo    ;echo $?
+Uso: zzcinemais [código cidade]
+1
+$ zzcinemais 999    ;echo $?
+Não encontrei o cinema 999
+1
 $
 
-$ zzcinemais 
+# Sem argumentos mostra a lista de cidades/cinemas
+
+$ zzcinemais
 9 - Uberaba  - MG
 11 - Patos de Minas - MG
 21 - Guaratinguetá - SP
@@ -13,63 +21,15 @@ $ zzcinemais
 37 - Araxá - MG
 38 - Lorena - SP
 39 - Jardim Norte - Juiz de Fora - MG
+$
+
+# Uso normal, informando o número
+
+$ filtro='/ - [A-Z][A-Z]$/s/.*/CIDADE/;/\/[0-9]\{4\}$/s/.*/DATA/;/[0-2][0-9]h[0-5][0-9]/s/.*/HORARIO/;/^$/s/^/AA/;/[^A-Z][^A-Z]/{s/.*/FILME/;}'
 $ zzcinemais 9 | sed "$filtro" | sort | uniq
 AA
 CIDADE
 DATA
 FILME
 HORARIO
-$ zzcinemais 11 | sed "$filtro" | sort | uniq
-AA
-CIDADE
-DATA
-FILME
-HORARIO
-$ zzcinemais 21 | sed "$filtro" | sort | uniq
-AA
-CIDADE
-DATA
-FILME
-HORARIO
-$ zzcinemais 32 | sed "$filtro" | sort | uniq
-AA
-CIDADE
-DATA
-FILME
-HORARIO
-$ zzcinemais 34 | sed "$filtro" | sort | uniq
-AA
-CIDADE
-DATA
-FILME
-HORARIO
-$ zzcinemais 35 | sed "$filtro" | sort | uniq
-AA
-CIDADE
-DATA
-FILME
-HORARIO
-$ zzcinemais 36 | sed "$filtro" | sort | uniq
-AA
-CIDADE
-DATA
-FILME
-HORARIO
-$ zzcinemais 37 | sed "$filtro" | sort | uniq
-AA
-CIDADE
-DATA
-FILME
-HORARIO
-$ zzcinemais 38 | sed "$filtro" | sort | uniq
-AA
-CIDADE
-DATA
-FILME
-HORARIO
-$ zzcinemais 39 | sed "$filtro" | sort | uniq
-AA
-CIDADE
-DATA
-FILME
-HORARIO
+$
