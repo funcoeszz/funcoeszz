85c3bae72ed6a786b6607a9482532a3f754dd717
itamarnet <itamarnet@yahoo.com.br>
2018-01-07 20:03:05 -0200

    zztempo: Nanny e portabilidade.

--- testador/zztempo.sh
+++ testador/zztempo.sh
@@ -5,10 +5,10 @@
 #   ― (   ) ―   ← 2 km/h       
 #      `-’      10 km          
 #     /   \     0.0 mm   
-$zztempo -0 -m -l pt curitiba | sed '1 s/Curitiba.*$/Curitiba/g; 2,3 d; s/[0-9][0-9]*/9/g; s/\.9\+//g; s/^ .\{14\}//g; s/[↖|↗|↘|↙|←|↑|→|↓]/↑/g'
+$ zztempo -0 -m -l pt curitiba | sed '1 s/Curitiba.*$/Curitiba/g; 2,3 d; s/[0-9][0-9]*/9/g; s/\.9\+//g; s/^ .\{14\}//g; s/[↖|↗|↘|↙|←|↑|→|↓]/↑/g; s/ *$//'
 Previsão do tempo para: Curitiba
-9 °C          
-↑ 9 km/h       
-9 km          
-9 mm 
+9 °C
+↑ 9 km/h
+9 km
+9 mm
 $
