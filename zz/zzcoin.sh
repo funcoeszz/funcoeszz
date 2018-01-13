0ab4f12d8e8758a4244d112011d95a44316aeaf3
itamarnet <itamarnet@yahoo.com.br>
2018-01-12 22:20:34 -0200

    zzcoin: Ajustando filtros e urls, e incluindo a moeda BCash.

--- zz/zzcoin.sh
+++ zz/zzcoin.sh
@@ -1,18 +1,19 @@
 # ----------------------------------------------------------------------------
-# Retorna a cotação de criptomoedas em Reais (bitcoin e litecoins).
-# Opções: btc ou bitecoin / ltc ou litecoin.
+# Retorna a cotação de criptomoedas em Reais (Bitcoin, Litecoins ou BCash).
+# Opções: btc ou bitecoin / ltc ou litecoin / bch ou bcash.
 # Com as opções -a ou --all, várias criptomoedas cotadas em dólar.
-# Uso: zzcoin [btc|bitcoin|ltc|litecoin|-a|--all]
+# Uso: zzcoin [btc|bitcoin|ltc|litecoin|bch|bcash|-a|--all]
 # Ex.: zzcoin
 #      zzcoin btc
 #      zzcoin litecoin
+#      zzcoin bch
 #      zzcoin -a
 #
 # Autor: Tárcio Zemel <tarciozemel (a) gmail com>
 # Desde: 2014-03-24
-# Versão: 4
+# Versão: 5
 # Licença: GPL
-# Requisitos: zzminusculas zzsemacento zznumero
+# Requisitos: zzjuntalinhas zzminusculas zznumero zzsemacento zzsqueeze zztrim zzxml
 # ----------------------------------------------------------------------------
 zzcoin ()
 {
@@ -20,72 +21,52 @@ zzcoin ()
 
 	# Variáveis gerais
 	local moeda_informada=$(echo "${1:--a}" | zzminusculas | zzsemacento)
-	local url="https://www.mercadobitcoin.com.br/api"
+	local url="https://www.mercadobitcoin.net/api"
 
 	# Se não informou moeda válida, termina
 	case "$moeda_informada" in
-		btc | bitcoin  )
+		btc | bitcoin )
 			# Monta URL a ser consultada
-			url="${url}/ticker/"
-			zztool source "$url" |
-			sed 's/.*"last"://;s/,"buy.*//' |
+			url="${url}/BTC/ticker/"
+			zztool dump "$url" |
+			sed 's/.*"last": *"//;s/", *"buy.*//' |
 			zznumero -m
 		;;
-		ltc | litecoin  )
+		ltc | litecoin )
 			# Monta URL a ser consultada
-			url="${url}/ticker_litecoin/"
-			zztool source "$url" |
-			sed 's/.*"last"://;s/,"buy.*//' |
+			url="${url}/LTC/ticker/"
+			zztool dump "$url" |
+			sed 's/.*"last": *"//;s/", *"buy.*//' |
+			zznumero -m
+		;;
+		bch | bcash )
+			# Monta URL a ser consultada
+			url="${url}/BCH/ticker/"
+			zztool dump "$url" |
+			sed 's/.*"last": *"//;s/", *"buy.*//' |
 			zznumero -m
 		;;
 		-a | --all )
-			url="http://coinmarketcap.com/mineable.html"
-			zztool dump "$url" |
-			sed -n '/#/,/Last updated/{
-				/^ *\*/d
-				/^ *$/d
-				s/Total Market Cap/Valor Total de Mercado/
-				s/Last updated/Última atualização/
-				s/ %//
-				s/\$ //g
-				s/  Name /Nome /
-				s/ Market Cap/Valor Mercado/
-				s/     Price/Preço/
-				s/Total Supply/Total Oferta/
-				s/ (24h)/(24h)/g
-				s/Change(24h)/%Var(24h)/
-				s/ Market Cap Graph (7d)//
-				s/ Price Graph (7d)//
-				s/ *sparkline *//
-				/______/d
-				/------/d
-				p
-				}' |
-			awk '
-				function espacos(  tamanho, saida, i) {
-					for(i=1;i<=tamanho;i++)
-						saida = saida " "
-					return saida
-				}
-				NR==1 {print}
-				NR>=2 {
-					if($2 == $3 || $3 ~ /\.\.\.$/) {
-						atual = $2 " " $3
-						novo = $2 " " espacos(length($3))
-						sub(atual, novo)
-						print
-					}
-					else if(($2 == $4 && $3 == $5) || $5 ~ /\.\.\.$/) {
-						gsub(/\)/,"_"); gsub(/\(/,"_")
-						atual = $2 " " $3 " " $4 " " $5
-						novo = $2 " " $3 " " espacos(length($4)+length($5)+1)
-						sub(atual, novo)
-						gsub(/_/," "); gsub(/_/," ")
-						print
-					}
-					else { print }
-				}'
-			return
+			url="http://coinmarketcap.com/"
+			zztool source http://coinmarketcap.com/ |
+			sed -n '/<table class="table" id="currencies" >/,/<\/table>/p' |
+			zzjuntalinhas -i '<td ' -f '</td>' |
+			zzxml --untag |
+			zztrim |
+			zzsqueeze |
+			sed 's/\.\.\./…/' |
+			awk '{
+				printf "%-4s", $0
+				getline; printf "%-20s", $0
+				getline;getline; printf "%17s", $0
+				getline; printf "%12s", $0
+				getline; printf "%17s", $0
+				getline; printf "  %-23s", $0
+				getline; if(NR==8) printf "%10s", $0
+				getline; if(NR!=9) printf "%12s", $0
+				getline; printf "\n"
+			}' |
+			tr '.,' ',.'
 		;;
 		* ) return 1;;
 	esac
