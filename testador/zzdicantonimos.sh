aef970ecabb7eb9f7b5fe884abb3b300f0c43817
itamarnet <itamarnet@yahoo.com.br>
2018-01-13 14:01:14 -0200

    zzdicantonimos: Ajustes na formatação.

--- testador/zzdicantonimos.sh
+++ testador/zzdicantonimos.sh
@@ -1,13 +1,47 @@
 # Normal
 
 $ zzdicantonimos alto
-acessível anão baixo base caído compreensível cristalino descomplicado diminuto falda flácido fraco indigno inferior insignificante leve modesto módico pequeno pior raso rasteiro reduzido sopé sórdido torpe vil
+Contrário de grande:
+ baixo pequeno raso diminuto anão miúdo nanico tacanho
+
+Contrário de superior:
+ inferior último ínfero pior péssimo
+
+Contrário de eminente:
+ indigno torpe vil sórdido reles ordinário ignóbil rasteiro tratante
+
+Contrário de excessivo:
+ módico modesto reduzido razoável econômico
+
+Contrário de sério:
+ insignificante desimportante irrelevante irrisório leve desprezível
+
+Contrário de erguido:
+ caído flácido murcho mole
+
+Contrário de incompreensível:
+ acessível compreensível fácil simples inteligível descomplicado perceptível cognoscível claro cristalino
+
+Contrário de arrojado:
+ medroso covarde temeroso frouxo
+
+Contrário de agudo:
+ grave fraco abafado
+
+Contrário de afastado:
+ próximo perto aproximado junto pegado vizinho
+
+Contrário de cimo:
+ base falda sopé pé aba socairo
+
+Contrário de bêbado:
+ sóbrio abstêmico abstinente
 $
 
 # Palavra com acentos
 
 $ zzdicantonimos folgazão
-infeliz introvertido rancoroso sério tristonho
+ introvertido sério infeliz tristonho rancoroso
 $
 
 # Palavra não encontrada
