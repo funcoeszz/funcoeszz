efadee1a5f557201975d88d1b56df5eb1fd174ea
itamarnet <itamarnet@yahoo.com.br>
2018-01-14 10:30:46 -0200

    zzglobo: Adequando testador.

--- testador/zzglobo.sh
+++ testador/zzglobo.sh
@@ -1,5 +1,5 @@
-$ zzglobo | sed '1s/\[..[\/-]..[\/-]....\]/[DATA]/; s/^[0-9][0-9]:[0-9][0-9] - .*/HORA - DESCRIÇÃO/' | head -n 10
-Hoje [DATA]
+$ zzglobo | sed '1{s/.*,/Sem,/;s/, .*/ DATA/;};s/[0-2][0-9]:[0-5][0-9]:/HORA:/;s/: .*/ - DESCRIÇÃO/' | head -n 10
+Sem DATA
 
 HORA - DESCRIÇÃO
 HORA - DESCRIÇÃO
