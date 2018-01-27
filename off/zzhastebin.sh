a531add9906cd7fe75fa0f18186a04019dc26d30
itamarnet <itamarnet@yahoo.com.br>
2018-01-27 12:33:47 -0200

    zzhastebin: Desativada - curl não funciona, é preciso ter o programa haste (ruby e xcode) instalados.

---
+++ off/zzhastebin.sh
@@ -0,0 +1,33 @@
+# ----------------------------------------------------------------------------
+# http://hastebin.com/
+# Gera link para arquivos de texto em geral.
+#
+# Uso: zzhastebin [arquivo]
+# Ex.: zzhastebin helloworld.sh
+#
+# Autor: Jones Dias <diasjones07 (a) gmail.com>
+# Desde: 2015-02-12
+# Versão: 1
+# Licença: GPL
+# ----------------------------------------------------------------------------
+# DESATIVADA: 2018-01-27 curl falha, precisa do haste (ruby e xcode)
+zzhastebin ()
+{
+
+	zzzz -h hastebin "$1" && return
+
+	test -n "$1" || { zztool -e uso hastebin; return 1; }
+
+	# Verifica o parametro da função
+	zztool arquivo_legivel "$1" || { zztool -e uso hastebin; return 1; }
+
+	local hst="http://hastebin.com/"
+	local uri
+	local ext=$(basename $1 | cut -d\. -f2)
+
+	# Retorna o ID
+	uri="$(curl -s --data-binary @$1 ${hst}documents | cut -d\" -f 4)"
+
+	# Imprime link
+	echo "$hst$uri.$ext"
+}
