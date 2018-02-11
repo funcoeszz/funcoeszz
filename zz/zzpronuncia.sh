6cec79f581ea75d25b470387e72d566ad39722ac
itamarnet <itamarnet@yahoo.com.br>
2018-02-10 22:55:22 -0200

    zzpronuncia: Corrigindo extração do link do áudio.

--- zz/zzpronuncia.sh
+++ zz/zzpronuncia.sh
@@ -5,20 +5,20 @@
 #
 # Autor: Thobias Salazar Trevisan, www.thobias.org
 # Desde: 2002-04-10
-# Versão: 4
+# Versão: 5
 # Licença: GPL
-# Requisitos: zzplay
+# Requisitos: zzplay zzunescape
 # Nota: opcional say
 # ----------------------------------------------------------------------------
 zzpronuncia ()
 {
 	zzzz -h pronuncia "$1" && return
 
-	local audio_file audio_dir
+	local audio_file
 	local palavra=$1
 	local cache=$(zztool cache pronuncia "$palavra.mp3")
 	local url='http://www.merriam-webster.com/dictionary'
-	local url2='http://media.merriam-webster.com/audio/prons/en/us/mp3'
+	local url2='http://media.merriam-webster.com/audio/prons'
 
 	# Verificação dos parâmetros
 	test -n "$1" || { zztool -e uso pronuncia; return 1; }
@@ -36,25 +36,20 @@ zzpronuncia ()
 		# Extrai o nome do arquivo no site do dicionário
 		audio_file=$(
 			zztool source "$url/$palavra" |
-			sed -n "/data-file=\"[^\"]*$palavra[^\"]*\"/{s/.*data-file=\"//;s/\".*//;p;}" |
-			uniq)
+			sed -n '/data-file=/{s/.*href="//;s/".*//;p;q;}' |
+			zzunescape --html |
+			awk -F '[=_&]' '{print $3 "/" $4 "/mp3/" $6 "/" $8 ".mp3"}'
+		)
 
 		# Ops, não extraiu nada
 		if test -z "$audio_file"
 		then
 			zztool erro "$palavra: palavra não encontrada"
 			return 1
-		else
-			audio_file="${audio_file}.mp3"
 		fi
 
-		# O nome da pasta é a primeira letra do arquivo (/a/apple001.mp3)
-		# Ou "number" se iniciar com um número (/number/9while01.mp3)
-		audio_dir=$(echo $audio_file | cut -c1)
-		echo $audio_dir | grep '[0-9]' >/dev/null && audio_dir='number'
-
 		# Compõe a URL do arquivo e salva-o localmente (cache)
-		zztool source "$url2/$audio_dir/$audio_file" > "$cache"
+		zztool source "$url2/$audio_file" > "$cache"
 	fi
 
 	# Fala que eu te escuto
