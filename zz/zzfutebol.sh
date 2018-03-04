42595eacc47c992498005e7bbbf32c0919acc478
itamarnet <itamarnet@yahoo.com.br>
2018-03-04 14:27:47 -0300

    zzfutebol: Usando 'zztool source' e mudando os filtros.

--- zz/zzfutebol.sh
+++ zz/zzfutebol.sh
@@ -11,6 +11,8 @@
 # Ou um ou dois argumentos para ver resultados do jogos:
 #   resultado ou placar, que pode ser acompanhado de hoje, ontem, anteontem.
 #
+# Nos casos dos dias, podem ser usadas datas no formato DD/MM/AAAA.
+#
 # Um filtro com nome do campeonato, nome do time, ou horário de uma partida.
 #
 # Uso: zzfutebol [resultado | placar ] [ argumento ]
@@ -25,57 +27,41 @@
 #
 # Autor: Jefferson Fausto Vaz (www.faustovaz.com)
 # Desde: 2014-04-08
-# Versão: 9
+# Versão: 10
 # Licença: GPL
-# Requisitos: zzdata zzdatafmt zztrim zzpad
+# Requisitos: zzcut zzdatafmt zzjuntalinhas zzpad zztrim zzxml
 # ----------------------------------------------------------------------------
 zzfutebol ()
 {
 
 	zzzz -h futebol "$1" && return
-	local url="http://esporte.uol.com.br/futebol/central-de-jogos/proximos-jogos"
+	local url="http://esporte.uol.com.br/futebol/central-de-jogos"
+	local pagina='proximos-jogos'
 	local linha campeonato time1 time2
 
 	case "$1" in
-		resultado | placar) url="http://esporte.uol.com.br/futebol/central-de-jogos/resultados"; shift;;
-		ontem | anteontem)  url="http://esporte.uol.com.br/futebol/central-de-jogos/resultados";;
+		resultado | placar) pagina='resultados'; shift;;
+		ontem | anteontem)  pagina='resultados';;
 	esac
 
-	zztool dump "$url" |
-	sed -n '/[0-9]h[0-9]/{N;N;p;}' |
-	sed '
-		s/^ *__* *$/XXX/
-		s/Amistoso.*/Amistoso/
-		s/ [A-Z][A-Z][A-Z]$//
-		s/__*//' |
+	zztool source "${url}/${pagina}" |
+	zzxml --tidy |
+	zzjuntalinhas -i 'td class="league"' -f '</td>' |
+	if test "$pagina" = 'proximos-jogos'
+	then
+		sed -n '/class="league"/p;/<span class="\(data\|hora\)">/,/<\/span>/p;;/<meta itemprop="\(name\|location\)"/{/content=""/d;s/">//;s/.*"//;p;}'
+	else
+		sed -n '/class="league"/p;/<span class="\(data\|hora\)">/,/<\/span>/p;/abbr title=/{s/">//;s/.*"//;p;};/<label class="gols">/,/label>/p'
+	fi |
+	zzxml --untag |
 	zztrim |
-	awk '
-		NR % 3 == 1 { campeonato = $0 }
-		NR % 3 ~ /^[02]$/ {
-			if ($1 ~ /^[0-9-]{1,}$/ && $2 ~ /^[0-9-]{1,}$/) {
-				penais[NR % 3]=$1; placar[NR % 3]=$2; $1=""; $2=""
-			}
-			else if ($1 ~ /^[0-9-]{1,}$/ && $2 !~ /^[0-9-]{1,}$/) {
-				penais[NR % 3]=""; placar[NR % 3]=$1; $1=""
-			}
-			sub(/^ */,"");sub(/ *$/,"")
-			time[NR % 3]=" " $0 " "
-		}
-		NR % 3 == 0 {
-			if (length(penais[0])>0 && length(penais[2])>0) {
-				placar[2] = placar[2] " ( " penais[2]
-				placar[0] = penais[0] " ) " placar[0]
-			}
-			else {
-				penais[0]=""; penais[2]=""
-			}
-			sub(/  *$/,""); print campeonato ":" time[2] placar[2] ":" placar[0] time[0]
-			placar[0]="";placar[2]=""
-		}
-		' |
+	awk -v pag="$pagina" '
+		BEGIN { lim = (pag=="resultados"?7:4) }
+		/[0-3][0-9]\// {printf $0; for(i=1;i<lim;i++){getline;printf ":" $0};print ""}
+	' |
 	case "$1" in
-		hoje | amanh[aã] | segunda | ter[cç]a | quarta | quinta | sexta | s[aá]bado | domingo | ontem | anteontem)
-			grep --color=never -e $( zzdata $1 | zzdatafmt -f 'DD/MM/AA' )
+		hoje | amanh[aã] | segunda | ter[cç]a | quarta | quinta | sexta | s[aá]bado | domingo | ontem | anteontem | [0-3][0-9]/[01][0-9]/20[1-9][0-9])
+			grep --color=never -e $(zzdatafmt -f 'DD/MM/AA' $1)
 			;;
 		*)
 			grep --color=never -i "${1:-.}"
@@ -83,9 +69,15 @@ zzfutebol ()
 	esac |
 	while read linha
 	do
-		campeonato=$(echo $linha | cut -d":" -f 1)
-		time1=$(echo $linha | cut -d":" -f 2)
-		time2=$(echo $linha | cut -d":" -f 3 | zztrim)
+		campeonato=$(echo $linha | zzcut -d : -D ' ' -f1-3)
+		if test "$pagina" = 'proximos-jogos'
+		then
+			time1=$(echo $linha | zzcut -d : -f 4 | zzcut -d ' x ' -f 1)
+			time2=$(echo $linha | zzcut -d : -f 4 | zzcut -d ' x ' -f 2)
+		else
+			time1=$(echo $linha | zzcut -d : -f 5,4 -D ' ')
+			time2=$(echo $linha | zzcut -d : -f 6,7 -D ' ')
+		fi
 		echo "$(zzpad -r 45 $campeonato) $(zzpad -l 25 $time1) x $time2"
 	done
 }
