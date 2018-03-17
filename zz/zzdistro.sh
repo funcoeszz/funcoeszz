257293d8038fc4968cd9b67a0eb145a3a203d0ce
itamarnet <itamarnet@yahoo.com.br>
2018-03-17 10:16:23 -0300

    zzdistro: Testando primeiro sed

--- zz/zzdistro.sh
+++ zz/zzdistro.sh
@@ -38,19 +38,19 @@ zzdistro ()
 	test -n "$1" && { zztool -e uso distro; return 1; }
 
 	zztool source "$url" |
-	sed '1,/>Rank</d' |
-	awk -F '"' '
-		/phr1/ || /<th class="News">[0-9][0-9]?[0-9]?<\/th>/ {
-			printf "%s\t", $3
-			getline
-			printf "%s\thttp://distrowatch.com/%s\n", $5, $4
-		}
-	' |
-	sed 's/<[^>]*>//g;s/>//g' |
-	if test $lista -eq 1
-	then
-		expand -t 4,18 | zzcolunar -w 60 2
-	else
-		sed 's/ *http.*//' | expand -t 4 | zzcolunar 4
-	fi
+	sed '1,/>Rank</d' #|
+#	awk -F '"' '
+#		/phr1/ || /<th class="News">[0-9][0-9]?[0-9]?<\/th>/ {
+#			printf "%s\t", $3
+#			getline
+#			printf "%s\thttp://distrowatch.com/%s\n", $5, $4
+#		}
+#	' |
+#	sed 's/<[^>]*>//g;s/>//g' |
+#	if test $lista -eq 1
+#	then
+#		expand -t 4,18 | zzcolunar -w 60 2
+#	else
+#		sed 's/ *http.*//' | expand -t 4 | zzcolunar 4
+#	fi
 }
