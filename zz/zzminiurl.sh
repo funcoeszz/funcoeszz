f8859a4ddc6220f93d4e10f150e7109c7d81925b
Vinicius Leite <vv.leite@gmail.com>
2018-04-13 18:04:00 +0200

    Alteração do serviço para encurtar URL
    No dia 30 de Março de 2018 0 Google informou que irá desativar o serviço Google URL Shortener, conforme postagem em seu blog oficial (https://developers.googleblog.com/2018/03/transitioning-google-url-shortener.html). Seguindo a recomendação do próprio Google, efetuei a migração para o serviço do Bitly (https://bitly.is/yournewhome).

--- zz/zzminiurl.sh
+++ zz/zzminiurl.sh
@@ -1,42 +1,37 @@
 # ----------------------------------------------------------------------------
-# Encurta uma URL utilizando o google ("https://goo.gl/").
+# Encurta uma URL utilizando o bit.ly ("https://bit.ly/").
 # Caso a URL já seja encurtada, será exibida a URL completa.
 # Obs.: Se a URL não tiver protocolo no início, será colocado http://
 # Uso: zzminiurl URL
 # Ex.: zzminiurl http://www.funcoeszz.net
 #      zzminiurl www.funcoeszz.net         # O http:// no início é opcional
-#      zzminiurl https://goo.gl/yz4cb9
+#      zzminiurl http://bit.ly/2qysTH4
 #
 # Autor: Vinícius Venâncio Leite <vv.leite (a) gmail com>
 # Desde: 2010-04-26
-# Versão: 6
+# Versão: 7
 # Licença: GPL
 # ----------------------------------------------------------------------------
 zzminiurl ()
 {
 	zzzz -h miniurl "$1" && return
 
-	test -n "$1" || { zztool -e uso miniurl; return 1; }
+	test -n "$1" || { zztool -e uso miniurl2; return 1; }
 
 	local url="$1"
 	local prefixo='http://'
-	local urlencurtador='https://www.googleapis.com/urlshortener/v1/url?key=AIzaSyAutIDVbN_3CmtxpunVnXruLYYAXs5e9Sw'
-	local urlexpansor="https://www.googleapis.com/urlshortener/v1/url?key=AIzaSyAutIDVbN_3CmtxpunVnXruLYYAXs5e9Sw&shortUrl"
-	local contenttype='Content-Type: application/json'
-	local parametro="{\"longUrl\": \"$url\"}"
-	local urlcurta
-	local urlcompara
+	local urlEncurtador='https://api-ssl.bitly.com/v3/shorten?access_token=f158450fee1fd865e57a85cb466d217f2e355fb9&longUrl'
+	local urlExpansor='https://api-ssl.bitly.com/v3/expand?access_token=f158450fee1fd865e57a85cb466d217f2e355fb9&shortUrl'
+	local urlCompara=$(echo "$url" | sed 's/\(.*\:\/\/\)\(bit\.ly\).*/\2/')
 
-	# Se o usuário não informou o protocolo, adiciona o padrão
+	# Se o usuário não informou o protocolo, adiciona o padrão.
 	echo "$url" | egrep '^(https?|ftp|mms)://' >/dev/null || url="$prefixo$url"
 
-	urlcompara=$(echo "$url" | sed 's/\(.*\:\/\/\)\(goo\.gl\).*/\2/')
-	urlcurta=$(curl -s "$urlencurtador" -H "$contenttype" -d "$parametro" 2>/dev/null)
-
-	if test "$urlcompara" == 'goo.gl'
+    	# Testa se a URL já é encurtada, se sim, expande, senão, encurta.
+    	if test "$urlCompara" == 'bit.ly'
 	then
-		curl -s "$urlexpansor=$url" | sed -n '/"longUrl"/ {s/.*\(http[^"]*\)".*/\1/g; p; }'
+        	curl -s ${urlExpansor}=${url} | sed -n '/"long_url"/ {s/.*\(http[^"]*\)".*/\1/g; p; }' && echo
 	else
-		echo "$urlcurta" | sed -n '/"id"/ {s/.*\(http[^"]*\)".*/\1/g; p;}'
-	fi
+        	curl -s ${urlEncurtador}=${url} | sed -e 's/.*"url":"\(.*\)","h.*/\1/' && echo
+    	fi
 }
