c5a33cc88140294a822546ba7350ca25eb8469a4
itamarnet <itamarnet@yahoo.com.br>
2018-04-25 23:00:39 -0300

    zzcotacao: Trocando 'zztool dump' por 'zztool source' e reajustando filtros e testador.

--- testador/zzcotacao.sh
+++ testador/zzcotacao.sh
@@ -1,12 +1,12 @@
 $ zzcotacao | sed 's/[0-9],[0-9]\{3\}/9.999/g;s|  n/d|9.999|g;s/[+_]/ /g;s/-/ /g;s/[0-9],[0-9]\{2\}/9.99/g;5q'
 Infomoney
                     Compra   Venda   Var(%)
-Dolar Comercial      9.999   9.999    9.99
-Dolar Turismo        9.999   9.999    9.99
-Dolar PTAX800        9.999   9.999    9.99
+Dólar Comercial      9.999   9.999    9.99
+Dólar Turismo        9.999   9.999    9.99
+Dólar PTAX800        9.999   9.999    9.99
 $
 
-$ zzcotacao | sed -n '/UOL /,$ {s|[0-3][0-9]/[0-1][0-9]/[0-9]\{4\}|DATA|;s/[0-2][0-9]h[0-5][0-9]/HORA/ ;s/[0-9],[0-9]\{4\}/9.9999/g;s/[+_]/ /g;s/-/ /g;s/[0-9],[0-9]\{2\}/9.99/g;s/ 0,0/9.99/;p;}'
+$ zzcotacao | sed -n '/UOL /,$ {s|[0-3][0-9]/[0-1][0-9]/[0-9]\{4\}|DATA|;s/[0-2][0-9]h[0-5][0-9]/HORA/ ;s/[0-9],[0-9]\{4\}/9.9999/g;s/[+_]/ /g;s/-/ /g;s/  *[0-9],[0-9]\{1,\}%/   9.99%/g;p;}'
 UOL   Economia
 DATA HORA
                     Compra   Venda   Var(%)
