dd5bf313b6fe7653d6e88d4744ea129b94a0cc51
itamarnet <itamarnet@yahoo.com.br>
2018-05-13 14:23:18 -0300

    Funcão nova: zzclassnum - Função para classificar um número.

---
+++ testador/zzclassnum.sh
@@ -0,0 +1,317 @@
+$ zzclassnum 0
+Par
+Jacobsthal
+$
+
+$ zzclassnum 1
+Ímpar
+Jacobsthal
+Mersenne
+Triangular
+Quadrado
+Pentagonal
+Hexagonal
+Hexagonal Centrado
+Estrela
+Heptagonal
+Dodecagonal
+Palíndromo
+$
+
+$ zzclassnum 2
+Par
+Primo
+Oblongo
+Palíndromo
+$
+
+$ zzclassnum 3
+Ímpar
+Jacobsthal
+Primo
+Wagstaff
+Fermat
+Mersenne
+Triangular
+Palíndromo
+$
+
+$ zzclassnum 7
+Ímpar
+Primo
+Mersenne
+Hexagonal Centrado
+Heptagonal
+Palíndromo
+$
+
+$ zzclassnum 12
+Par
+Excessivo
+Oblongo
+Pentagonal
+Dodecagonal
+$
+
+$ zzclassnum 13
+Ímpar
+Primo
+Estrela
+$
+
+$ zzclassnum 18
+Par
+Excessivo
+Heptagonal
+$
+
+$ zzclassnum 21
+Ímpar
+Jacobsthal
+Defectivo
+Triangular
+$
+
+$ zzclassnum 22
+Par
+Defectivo
+Pentagonal
+Palíndromo
+$
+
+$ zzclassnum 28
+Par
+Perfeito
+Triangular
+Hexagonal
+$
+
+$ zzclassnum 34
+Par
+Defectivo
+Heptagonal
+$
+
+$ zzclassnum 35
+Ímpar
+Defectivo
+Pentagonal
+$
+
+$ zzclassnum 36
+Par
+Excessivo
+Triangular
+Quadrado
+$
+
+$ zzclassnum 37
+Ímpar
+Primo
+Hexagonal Centrado
+Estrela
+$
+
+$ zzclassnum 55
+Ímpar
+Defectivo
+Triangular
+Heptagonal
+Palíndromo
+$
+
+$ zzclassnum 81
+Ímpar
+Defectivo
+Quadrado
+Heptagonal
+$
+
+$ zzclassnum 91
+Ímpar
+Defectivo
+Triangular
+Hexagonal
+Hexagonal Centrado
+$
+
+$ zzclassnum 120
+Par
+Excessivo
+Triangular
+Hexagonal
+$
+
+$ zzclassnum 121
+Ímpar
+Defectivo
+Quadrado
+Estrela
+Palíndromo
+$
+
+$ zzclassnum 127
+Ímpar
+Primo
+Mersenne
+Hexagonal Centrado
+$
+
+$ zzclassnum 171
+Ímpar
+Jacobsthal
+Defectivo
+Triangular
+Palíndromo
+$
+
+
+$ zzclassnum 253
+Ímpar
+Defectivo
+Triangular
+Estrela
+$
+
+$ zzclassnum 380
+Par
+Excessivo
+Oblongo
+$
+
+$ zzclassnum 469
+Ímpar
+Defectivo
+Hexagonal Centrado
+Heptagonal
+$
+
+$ zzclassnum 496
+Par
+Perfeito
+Triangular
+Hexagonal
+$
+
+$ zzclassnum 547
+Ímpar
+Primo
+Hexagonal Centrado
+$
+
+$ zzclassnum 561
+Ímpar
+Defectivo
+Triangular
+Hexagonal
+Dodecagonal
+$
+
+$ zzclassnum 616
+Par
+Excessivo
+Heptagonal
+Palíndromo
+$
+
+$ zzclassnum 721
+Ímpar
+Defectivo
+Hexagonal Centrado
+$
+
+$ zzclassnum 1001
+Ímpar
+Defectivo
+Pentagonal
+Palíndromo
+$
+
+$ zzclassnum 1162
+Par
+Defectivo
+Pentagonal
+$
+
+$ zzclassnum 1404
+Par
+Excessivo
+Heptagonal
+$
+
+$ zzclassnum 1657
+Ímpar
+Primo
+Hexagonal Centrado
+$
+
+$ zzclassnum 3025
+Ímpar
+Defectivo
+Quadrado
+Dodecagonal
+$
+
+$ zzclassnum 6336
+Par
+Excessivo
+Dodecagonal
+Palíndromo
+$
+
+$ zzclassnum 8128
+Par
+Perfeito
+Triangular
+Hexagonal
+$
+
+$ zzclassnum 8893
+Ímpar
+Primo
+Estrela
+$
+
+$ zzclassnum 9504
+Par
+Excessivo
+Dodecagonal
+$
+
+$ zzclassnum 65537
+Ímpar
+Primo
+Fermat
+$
+
+$ zzclassnum 174763
+Ímpar
+Jacobsthal
+Primo
+Wagstaff
+$
+
+$ zzclassnum 524287
+Ímpar
+Primo
+Mersenne
+$
+
+$ zzclassnum 2863311531
+Ímpar
+Jacobsthal
+Defectivo
+$
+
+$ zzclassnum 4294967297
+Ímpar
+Defectivo
+Fermat
+$
+
+$ zzclassnum 2932031007403
+Ímpar
+Jacobsthal
+Primo
+Wagstaff
+$
