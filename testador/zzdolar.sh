0d5fd1cb14494b7c2940dd69f7cb4bfe540d4b74
itamarnet <itamarnet@yahoo.com.br>
2020-12-21 06:49:58 -0300

    zzdolar: Usando zzcotacao como base.

--- testador/zzdolar.sh
+++ testador/zzdolar.sh
@@ -1,5 +1,5 @@
-$ zzdolar | sed 's/[0-9]/9/g;s/[+-]//g;s/99h99//;s/99\/99\/9999/DATA/g;s/9,9*\%/PORCENTAGEM/g;s/9,999*/DINHEIRO/g' | sed 's/[ 	]*$//'
-DATA
-		Compra	Venda	Variação
-Comercial	DINHEIRO	DINHEIRO	PORCENTAGEM
-Turismo		DINHEIRO	DINHEIRO	PORCENTAGEM
+$ zzdolar | sed 's/[0-9]/9/g;s/[+-]//g;;s/9,999*/NNNNN/g'
+              Compra   Venda   Var(%)
+Comercial      NNNNN   NNNNN  NNNNN
+Turismo        NNNNN   NNNNN  NNNNN
+$
--- zz/zzdolar.sh
+++ zz/zzdolar.sh
@@ -6,24 +6,15 @@
 #
 # Autor: Aurelio Marinho Jargas, www.aurelio.net
 # Desde: 2000-02-22
-# Versão: 8
+# Versão: 9
 # Licença: GPL
-# Requisitos: zzjuntalinhas zzsqueeze zztrim zzxml
+# Requisitos: zzcotacao
 # Tags: internet, consulta
 # ----------------------------------------------------------------------------
 zzdolar ()
 {
 	zzzz -h dolar "$1" && return
 
-	# Faz a consulta e filtra o resultado
-	zztool source 'http://economia.uol.com.br/cotacoes' |
-	zzxml --tidy |
-	sed -n '/<table class="borda mod-grafico-wide quatro-colunas">/,/table>/p' |
-	zzjuntalinhas -i '<tr>' -f '</tr>' |
-	zzjuntalinhas -i '<caption' -f 'caption>' |
-	zzxml --untag |
-	zzsqueeze |
-	zztrim |
-	sed -n '/Dólar /{s///;s/comercial - //;s/com\./Comercial/;s/tur\./Turismo/;s/ /	/g;p;}' |
-	sed '$s/	/		/;1a \		Compra	Venda	Variação'
+	zzcotacao |
+		sed -n '2{s/      //;p;};s/Dólar //;/Comercial/p;/Turismo/p'
 }
