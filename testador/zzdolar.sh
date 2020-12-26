b7f699a5fff8e64ee47cf75d4ef790d7959b4d66
Aurelio Jargas <verde@aurelio.net>
2020-12-26 09:58:55 +0100

    zzdolar: Atualiza testes
    Em alguns casos, vem os centavos com apenas um dígito. Exemplo:
    
        Comercial      NNNNN     9,9  NNNNN[201~

--- testador/zzdolar.sh
+++ testador/zzdolar.sh
@@ -1,4 +1,4 @@
-$ zzdolar | sed 's/[0-9]/9/g;s/[+-]//g;;s/9,999*/NNNNN/g'
+$ zzdolar | sed 's/[0-9]/9/g;s/[+-]//g;;s/9,99*/NNNNN/g'
               Compra   Venda   Var(%)
 Comercial      NNNNN   NNNNN  NNNNN
 Turismo        NNNNN   NNNNN  NNNNN
