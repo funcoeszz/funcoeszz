939a115eb1aefb1e25c60495e00583d7901b977b
itamarnet <itamarnet@yahoo.com.br>
2021-01-03 07:46:04 -0300

    zzwikipedia: Retirando limitação artificial do teste.

--- testador/zzwikipedia.sh
+++ testador/zzwikipedia.sh
@@ -1,4 +1,4 @@
-$ zzwikipedia -pt PDP-7 | sed '/^ *$/d; s/^ *//;/^Refer/q'
+$ zzwikipedia -pt PDP-7 | sed '/^ *$/d; s/^ *//'
 PDP-7
 PDP-7 modificado sendo restaurado
 O DEC PDP-7 é um minicomputador produzido pela Digital Equipment Corporation. Introduzido em 1965,^[1] foi o primeiro a usar a tecnologia Flip Chip daquela companhia.^[2] Com um custo de apenas 72 mil dólares, era um computador muito barato mas bastante poderoso. Tinha uma arquitectura de
@@ -6,6 +6,10 @@ O DEC PDP-7 é um minicomputador produzido pela Digital Equipment Corporation. I
 Em 1969, Ken Thompson escreveu o primeiro sistema UNIX em assembly num PDP-7, então chamado Unics, o que era uma brincadeira algo traiçoeira com o Multics - o sistema operativo do Space Travel, um jogo que precisava de gráficos para ilustrar os movimentos dos planetas.^[3]
 Ainda restam alguns PDP-7 funcionais, e decorre em Oslo, na Noruega, um projecto de restauro.^[4]
 Referências
+1. ↑ «Digital Equipment Corporation - 1957 to the present» (PDF)
+2. ↑ Tore Sinding Bekkedal (2009). «Digital Equipment Corporation PDP-7». soemtron.org
+3. ↑ Ritchie, Dennis M. «The Development of the C Language»
+4. ↑ «Archived copy». Consultado em 24 de junho de 2005. Arquivado do original em 15 de julho de 2012  PDP-7 restoration project located in Oslo, Norway.
 $
 $ zzwikipedia
 Uso: zzwikipedia [-idioma] palavra(s)
