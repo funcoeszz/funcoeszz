5aef4bbca6806f50e8bca29542dc7adfb5ff2463
itamarnet <itamarnet@yahoo.com.br>
2021-01-04 20:08:54 -0300

    Função nova: zzconstantes - Lista constantes matemáticas e físicas e unidades do SI.

---
+++ testador/zzconstantes.sh
@@ -0,0 +1,8 @@
+$ zzconstantes -z
+Uso: zzconstantes [-m|-f|-s|-a|-p]
+$ zzconstantes -m	#=> --lines 23
+$ zzconstantes -f	#=> --lines 24
+$ zzconstantes -s	#=> --lines 43
+$ zzconstantes -a	#=> --lines 18
+$ zzconstantes -p	#=> --lines 15
+$ zzconstantes		#=> --lines 121
