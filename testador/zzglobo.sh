d0f36683303dc36c373bcc62d0556b6bbe309a0d
itamarnet <itamarnet@yahoo.com.br>
2021-01-09 12:27:38 -0300

    zzglobo: Usando a zztv para pegar a programação.

--- testador/zzglobo.sh
+++ testador/zzglobo.sh
@@ -1,7 +1,4 @@
-$ zzglobo | sed '1d;/^ *$/d;s/[0-2][0-9]:[0-5][0-9]:/HORA:/;s/: .*/ - DESCRIÇÃO/' | head -n 5
-HORA - DESCRIÇÃO
-HORA - DESCRIÇÃO
-HORA - DESCRIÇÃO
-HORA - DESCRIÇÃO
-HORA - DESCRIÇÃO
+$ zzglobo | sed '2d;/^ *$/d;s/[0-2][0-9]∶[0-5][0-9]/HORA/;1!s/ .*/ DESCRIÇÃO/' | head -n 2
+TV Globo
+HORA DESCRIÇÃO
 $
