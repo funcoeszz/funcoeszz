83ba07d40c277e91051ec855305eec36aabae6a0
Aurelio Jargas <verde@aurelio.net>
2021-01-24 08:31:49 +0100

    testador/missing.sh: Simplifica
    Não precisava daquela firula toda quando só queremos comparar arquivos
    em duas pastas diferentes.

--- testador/missing.sh
+++ testador/missing.sh
@@ -4,14 +4,8 @@
 # Go to tests folder
 cd $(dirname "$0")
 
-# List of all functions
-all=$(ls -1 ../zz/ | sed 's/\.sh$//; s|.*/||' | sort)
-
-# List of already tested functions
-tested=$(ls -1 zz*.sh | sed 's/\.sh$//' | sort)
-
-# Who's not tested?
-for zz_func in $all
+# All zz functions have a counterpart in the test directory?
+for zz_func in ../zz/*
 do
-	echo "$tested" | grep "^$zz_func$" > /dev/null || echo "$zz_func"
+	test -f $(basename $zz_func) || echo "$zz_func"
 done
