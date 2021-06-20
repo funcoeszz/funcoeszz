49320ec6a766f90d22c40a9124a53efc630796c7
Martin Meier <29928669+martinbra@users.noreply.github.com>
2021-06-20 16:13:57 -0300

    Novo parâmetro de IPv6 externo (-6) para zzipinternet.

--- zz/zzipinternet.sh
+++ zz/zzipinternet.sh
@@ -1,11 +1,14 @@
 # ----------------------------------------------------------------------------
 # Mostra o seu número IP (externo) na Internet.
-# Uso: zzipinternet
+# Com argumento '-6' mostra o IPv6 externo.
+# Uso: zzipinternet [-6]
 # Ex.: zzipinternet
+#      zzipinternet -6
+#
 #
 # Autor: Thobias Salazar Trevisan, www.thobias.org
 # Desde: 2005-09-01
-# Versão: 6
+# Versão: 7
 # Requisitos: zzzz zztool
 # Tags: internet, consulta
 # ----------------------------------------------------------------------------
@@ -13,6 +16,15 @@ zzipinternet ()
 {
 	zzzz -h ipinternet "$1" && return
 
-	# O resultado já vem pronto!
-	zztool source 'http://api.ipaddress.com/myip' | zztool nl_eof
+	# Verifica parametro para ipv6
+	if test '-6' = "$1" 
+	then
+		# Retorna IPv6
+		zztool source 'https://api64.ipify.org' | zztool nl_eof
+	else
+		# Retorna IPv4
+		# O resultado já vem pronto!
+		zztool source 'http://api.ipaddress.com/myip' | zztool nl_eof
+	fi
 }
+
