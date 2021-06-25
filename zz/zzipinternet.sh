0f1c8d7d80096eb226bf548e728dbe90a866f822
Martin Meier <29928669+martinbra@users.noreply.github.com>
2021-06-25 00:18:27 -0300

    voltando para parametros simples, sem fallback

--- zz/zzipinternet.sh
+++ zz/zzipinternet.sh
@@ -1,12 +1,10 @@
 # ----------------------------------------------------------------------------
 # Mostra o seu número IP (externo) na Internet.
-# Aceita os argumentos '--v6', '--v4' (ou ambos) para exibir o IPv6 ou IPv4.
-# Com '--v64' retorna o IPv6 externo se existir, senão retorna o IPv4.
-# Uso: zzipinternet [--v6|--v4|--v64]
+# Retorna IPv4 por padrão ou com o argumento '-4'. Com '-6' retorna o IPv6.
+# Uso: zzipinternet [-4|-6]
 # Ex.: zzipinternet
-#      zzipinternet --v6
-#      zzipinternet --v64
-#      zzipinternet --v6 --v4
+#      zzipinternet -4
+#      zzipinternet -6
 #
 # Autor: Thobias Salazar Trevisan, www.thobias.org
 # Desde: 2005-09-01
@@ -21,28 +19,24 @@ zzipinternet ()
 	# Sem parâmetros seta IPv4 :
 	if test $# -eq 0
 	then
-		set -- --v4
+		set -- -4
+	elif test $# -gt 1
+	then
+		zztool erro "Utilizar no máximo 1 argumento."
+		return 1
 	fi
 
-	#leitura dos parametros de entrada
-	while test $# -gt 0
-	do
-		case "$1" in
-		--v6)
-			# se houver, IPv6, senão vazio:
-			zztool source https://api6.ipify.org | zztool nl_eof;
-			shift;;
-		--v64)
-			# se houver, IPv6, senão IPv4:
-			zztool source https://api64.ipify.org | zztool nl_eof;
-			shift;;
-		--v4)
-			# se houver, IPv4, senão vazio:
-			zztool source https://api4.ipify.org | zztool nl_eof;
-			shift;;
-		*) 
-			# parametro inválido:
-			zztool erro "Opção inválida: $1"; return 1 ;;
-		esac
-	done
+	# Verifica parametro de entrada 
+	case "$1" in 
+	-6)
+		# Retorna IPv6 se houver, senão vazio.
+		zztool source 'https://api6.ipify.org' | zztool nl_eof;;
+	-4)
+		# Retorna IPv4 se houver, senão vazio.
+		zztool source 'https://api4.ipify.org' | zztool nl_eof;;
+	*)
+		# parametro inválido:
+		zztool erro "Opção inválida: $1"; 
+		return 2;;
+	esac
 }
