893402668f282b9ab8da2af89da7181847873ddf
Nilo César Teixeira <nilo.teixeira@gmail.com>
2021-06-29 21:55:44 -0300

    Funções auxiliares aninhadas, novo número da versão, código de zzhora_minutos_auxiliar mantido para testes passarem, correção de espaços em branco em comentários.

--- zz/zzhora.sh
+++ zz/zzhora.sh
@@ -14,87 +14,86 @@
 #
 # Autor: Aurelio Marinho Jargas, www.aurelio.net
 # Desde: 2000-02-22
-# Versão: 5.1
+# Versão: 6
 # Requisitos: zzzz zztool
 # Tags: tempo, cálculo
 # ----------------------------------------------------------------------------
-
-# Função auxiliar que normaliza parâmetro de hora e transforma em minutos
-zzhora_minutos_auxiliar() {
-	local hhmm="${1:-0}"
-	local hhmm_orig="$hhmm"
-	local neg=0
-
-	# Remove possíveis sinais de negativo do início
-	hhmm="${hhmm#-}"
-
-	# Guarda a informação de se era negativo no início
-	test "$hhmm" != "$hhmm_orig" && neg=1
-
-	# Atalhos bacanas para a hora atual
-	test "$hhmm" = 'agora' -o "$hhmm" = 'now' && hhmm=$(date +%H:%M)
-
-	# Se as horas não foram informadas, coloca zero
-	test "${hhmm#*:}" = "$hhmm" && hhmm="0:$hhmm"
-
-	# Extrai horas e minutos para variáveis separadas
-	local hh="${hhmm%:*}"
-	local mm="${hhmm#*:}"
-
-	# Retira o zero das horas e minutos menores que 10
-	hh="${hh#0}"
-	mm="${mm#0}"
-
-	# Se tiver algo faltando, salva como zero
-	hh="${hh:-0}"
-	mm="${mm:-0}"
-
-	# Validação dos dados
-	if ! (zztool testa_numero "$hh" && zztool testa_numero "$mm")
-	then
-		zztool erro "Horário inválido '$hhmm_orig', deve ser HH:MM"
-		return 1
-	fi
-
-	# Os cálculos são feitos utilizando apenas minutos.
-	local n=$((hh*60 + mm))
-
-	# Restaura o sinal para as horas negativas
-	test $neg -eq 1 && n="-$n"
-
-	echo "$n"
-}
-
-# Função auxiliar que faz a operação de soma/subtração de dois horários,
-# considerando que o primeiro parâmetro é expresso em minutos,
-# e retorna o valor da operação em minutos.
-zzhora_soma_auxiliar() {
-	local mm1="$1"
-	local operacao="${2:-+}"
-	local mm2=$(zzhora_minutos_auxiliar "$3")
-	# Se não conseguiu converter parâmetro $3 em minutos, há um erro.
-	if test -z $mm2; then
-		return 1 
-	fi
-
-	# Somente adição e subtração são permitidas
-	if test '-' != "$operacao" -a '+' != "$operacao"
-	then
-		zztool erro "Operação inválida '$operacao'. Deve ser + ou -."
-		return 1
-	fi
-
-	# Tudo certo, hora de fazer o cálculo
-	echo $(($mm1 $operacao $mm2))
-}
-
 zzhora ()
 {
 	zzzz -h hora "$1" && return
 
-	local parcial resultado negativo 
+	local parcial resultado negativo
 	local horas minutos dias horas_do_dia hh mm hh_dia extra
 	local relativo=0
+
+	# Função auxiliar que normaliza parâmetro de hora e transforma em minutos
+	zzhora_minutos_auxiliar() {
+		local hhmm="${1:-0}"
+		local hhmm_orig="$hhmm"
+		local neg=0
+
+		# Remove possíveis sinais de negativo do início
+		hhmm="${hhmm#-}"
+
+		# Guarda a informação de se era negativo no início
+		test "$hhmm" != "$hhmm_orig" && neg=1
+
+		# Atalhos bacanas para a hora atual
+		test "$hhmm" = 'agora' -o "$hhmm" = 'now' && hhmm=$(date +%H:%M)
+
+		# Se as horas não foram informadas, coloca zero
+		test "${hhmm#*:}" = "$hhmm" && hhmm="0:$hhmm"
+
+		# Extrai horas e minutos para variáveis separadas
+		local hh="${hhmm%:*}"
+		local mm="${hhmm#*:}"
+
+		# Retira o zero das horas e minutos menores que 10
+		hh="${hh#0}"
+		mm="${mm#0}"
+
+		# Se tiver algo faltando, salva como zero
+		hh="${hh:-0}"
+		mm="${mm:-0}"
+
+		# Validação dos dados
+		if ! (zztool testa_numero "$hh" && zztool testa_numero "$mm")
+		then
+			zztool erro "Horário inválido '$hhmm_orig', deve ser HH:MM"
+			return 1
+		fi
+
+		# Os cálculos são feitos utilizando apenas minutos.
+		local n=$((hh*60 + mm))
+
+		# Restaura o sinal para as horas negativas
+		test $neg -eq 1 && n="-$n"
+
+		echo "$n"
+	}
+
+	# Função auxiliar que faz a operação de soma/subtração de dois horários,
+	# considerando que o primeiro parâmetro é expresso em minutos,
+	# e retorna o valor da operação em minutos.
+	zzhora_soma_auxiliar() {
+		local mm1="$1"
+		local operacao="${2:-+}"
+		local mm2=$(zzhora_minutos_auxiliar "$3")
+		# Se não conseguiu converter parâmetro $3 em minutos, há um erro.
+		if test -z $mm2; then
+			return 1 
+		fi
+
+		# Somente adição e subtração são permitidas
+		if test '-' != "$operacao" -a '+' != "$operacao"
+		then
+			zztool erro "Operação inválida '$operacao'. Deve ser + ou -."
+			return 1
+		fi
+	
+		# Tudo certo, hora de fazer o cálculo
+		echo $(($mm1 $operacao $mm2))
+	}
 
 	# Opções de linha de comando
 	if test '-r' = "$1"
@@ -111,7 +110,7 @@ zzhora ()
 	# o total parcial de minutos, a operação, e um horário.
 	# (O parcial inicial é diferente: apenas os minutos do primeiro horário)
 	#
-	# Início	: parcial = zzhora_minutos_auxiliar $1
+	# Início  : parcial = zzhora_minutos_auxiliar $1
 	# Rodada 1: parcial = zzhora_soma_auxiliar $parcial $2 $3
 	# Rodada 2: parcial = zzhora_soma_auxiliar $parcial $4 $5
 	# Rodada 3: parcial = zzhora_soma_auxiliar $parcial $6 $7
@@ -149,12 +148,12 @@ zzhora ()
 	# Decide como mostrar o resultado para o usuário.
 	#
 	# Relativo:
-	#   $ zzhora -r 10:00 + 48:00						 $ zzhora -r 12:00 - 13:00
-	#   10:00 (2 dias: dd/mm/yyyy)					 23:00 (ontem: dd/mm/yyyy)
+	#   $ zzhora -r 10:00 + 48:00                    $ zzhora -r 12:00 - 13:00
+	#   10:00 (2 dias: dd/mm/yyyy)                   23:00 (ontem: dd/mm/yyyy)
 	#
 	# Normal:
-	#   $ zzhora 10:00 + 48:00							 $ zzhora 12:00 - 13:00
-	#   58:00 (2d 10h 0m)										 -01:00 (0d 1h 0m)
+	#   $ zzhora 10:00 + 48:00                       $ zzhora 12:00 - 13:00
+	#   58:00 (2d 10h 0m)                            -01:00 (0d 1h 0m)
 	#
 	if test $relativo -eq 1
 	then
