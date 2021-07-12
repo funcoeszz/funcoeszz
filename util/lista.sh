0b21c55adb0a49b33d4775df1cf8c67581f41e71
Aurelio Jargas <verde@aurelio.net>
2021-07-12 02:02:24 +0200

    Novo util/lista.sh, lista as funções disponíveis
    Novo script cuja única função é listar as funções disponíveis na pasta
    zz/, uma por linha.
    
    Conforme os argumentos que recebe, filtra a lista de funções para
    mostrar somente aquelas que acessa m à internet, ou somente as que não
    precisam de internet, etc.
    
    A ideia é que outros scripts que precisam dessas informações, usem
    este script.

---
+++ util/lista.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+# 2021-07-12
+# Aurelio Jargas
+#
+# Lista as funções disponíveis, mostrando uma por linha.
+# Conforme o argumento recebido, filtra a lista.
+#
+# Exemplos:
+#    lista.sh            # lista todas as funções
+#    lista.sh internet   # lista somente as que usam internet
+#    lista.sh local      # lista somente as que nãousam internet
+
+cd "$(dirname "$0")/.." || exit 1  # go to repo root
+
+function zzname_from_path() {
+    while read path
+    do
+        basename $path .sh
+    done
+}
+
+case "$1" in
+    all | todas | "")
+        ls -1 zz/zz*.sh |
+            zzname_from_path |
+            sort
+    ;;
+
+    internet)
+        ./util/metadata.sh tags |
+            grep '^zz/[^ ]* .*internet' |
+            cut -d ' ' -f 1 |
+            zzname_from_path |
+            sort
+    ;;
+
+    no-internet | local | sem-internet)
+        ./util/lista.sh all |
+            grep -F -v -x -f <(./util/lista.sh internet)
+    ;;
+
+    travis)
+        ./util/lista.sh internet |
+            # Why? https://github.com/funcoeszz/funcoeszz/issues/355
+            grep -E -v '^zz(distro|linux|chavepgp)$'
+    ;;
+
+    # Just to make sure we're doing it right
+    self-test)
+        all=$(./util/lista.sh all | wc -l)
+        internet=$(./util/lista.sh internet | wc -l)
+        no_internet=$(./util/lista.sh no-internet | wc -l)
+
+        echo $internet + $no_internet = $all
+        test "$((internet + no_internet))" -eq "$all" || {
+            echo FAIL
+            exit 1
+        }
+    ;;
+
+    *)
+        echo "Invalid argument: $1"
+        exit 1
+    ;;
+esac
