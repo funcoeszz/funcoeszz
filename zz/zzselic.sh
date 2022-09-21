b395a5880867e90221ca037f5a7ccf18f726caf2
Aurelio Jargas <verde@aurelio.net>
2022-09-21 15:29:35 +0200

    zzselic: Adiciona requisitos que faltavam
    Antigamente não era necessário, mas agora Ãzzzz e zztool são funções
    normais (não estão mais no core) e devem ser listadas como requisitos.
    
    Resolve este problema:
    
        $ make lint
        ./util/alinhamento.sh
        ./util/requisitos.sh
        zzselic.sh: # Requisitos: zztool
        zzselic.sh: # Requisitos: zzzz
        make: *** [Makefile:8: lint] Error 1
        $

--- zz/zzselic.sh
+++ zz/zzselic.sh
@@ -16,7 +16,7 @@
 # Autor: Guilherme Magalhães Gall <gmgall (a) gmail com>
 # Desde: 2015-06-07
 # Versão: 3
-# Requisitos: zzdata zzdiadasemana zzdos2unix zzferiado zzurlencode zzutf8 zzpad
+# Requisitos: zzzz zztool zzdata zzdiadasemana zzdos2unix zzferiado zzurlencode zzutf8 zzpad
 # Tags: internet
 # ----------------------------------------------------------------------------
 zzselic ()
