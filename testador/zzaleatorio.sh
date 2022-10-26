2d77d1eb8dbfbe4d360851bab786d9dba9e37e78
Aurelio Jargas <verde@aurelio.net>
2022-10-26 21:32:56 +0000

    testador: zzaleatorio: testa se o número muda a cada chamada
    Pois caso contrário, se vier sempre o mesmo número, o teste iria passar.
    
    Vide https://github.com/funcoeszz/funcoeszz/issues/754

--- testador/zzaleatorio.sh
+++ testador/zzaleatorio.sh
@@ -2,5 +2,15 @@ $ zzaleatorio                           #=> --regex ^\d+$
 $ zzaleatorio 9                         #=> --regex ^\d$
 $ zzaleatorio 2300 2359                 #=> --regex ^23[0-5][0-9]$
 
+# Os números devem mudar a cada execução
+$ zzaleatorio > _tmp1
+$ zzaleatorio > _tmp2
+$ zzaleatorio > _tmp3
+$ diff -q _tmp1 _tmp2 > /dev/null; echo $?	#=> 1
+$ diff -q _tmp1 _tmp3 > /dev/null; echo $?	#=> 1
+$ diff -q _tmp2 _tmp3 > /dev/null; echo $?	#=> 1
+$ rm -f _tmp[123]
+$
+
 # Detectando erro
 $ zzaleatorio texto; echo $?            #=> 1
